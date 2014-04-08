f3073e6e50a71eee89be3e9ce29f72bf1ebebb1c
suraj <suraj_gillespie@yahoo.co.in>
2014-04-09 03:20:54 +0530

    Removing unnecessary code

--- activities/ChatPrototype.activity/js/activity.js
+++ activities/ChatPrototype.activity/js/activity.js
@@ -50,10 +50,6 @@ define(function (require) {
             myElem.innerHTML = authorElem + msg.data;
             myElem.style.color = msg.colour.stroke;
 
-
-            //messagesList.innerHTML += '<li class="received" style = "background-color:"' + userInfo[1].fill +'><span>' + msg.author + '</span>' + msg.data + '</li>';
-            //console.log(messagesList.scrollTop);
-            //console.log(messageField.scrollList);
             messagesList.appendChild(myElem);
             messageContent.scrollTop = messageContent.scrollHeight;
         });
@@ -61,8 +57,7 @@ define(function (require) {
         presenceObject.onServerMessage(function (msg) {
 
             messagesList.innerHTML += '<li class="received" style = "color:blue">' + msg.data + '</li>';
-            connectedUsers.disabled = false;
-            closeBtn.disabled = false;
+
         });
 
         // Show a disconnected message when the WebSocket is closed.
@@ -70,39 +65,6 @@ define(function (require) {
             socketStatus.innerHTML = 'Disconnected from WebSocket.';
             socketStatus.className = 'closed';
         });
-
-        /*
-            sendBtn.onclick = function (e) {
-
-                // Retrieve the message from the textarea.
-                var message = messageField.value;
-                //sendBtn.innerHTML = "Send Message/Ping";
-
-                presenceObject.sendMessage("SampleGroupId", message);
-                // Send the message through the WebSocket.
-
-                messageField.placeholder = "Write your message here...";
-                // Clear out the message field.
-                messageField.value = "";
-        
-            };*/
-        // Send a message when the form is submitted.
-
-        // Close the WebSocket connection when the close button is clicked.
-        /*closeBtn.onclick = function (e) {
-            presenceObject.leaveNetwork();
-            console.log(sendBtn)
-            sendBtn.innerHTML = 'Connect'
-            // Close the WebSocket.
-            return false;
-        };*/
-
-        /*connectedUsers.onclick = function (e) {
-            presenceObject.listUsers("SampleGroupId", function (list) {
-                messagesList.innerHTML += '<li class="received" style = "color:blue">' + list.data + '</li>';
-                messageContent.scrollTop = messageContent.scrollHeight;
-            });
-        }*/
 
         messageField.onkeydown = function (e) {
             if (e.keyCode === 13) {
