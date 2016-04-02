bb7d55356f530bd13693ade30d2b6f9cd5c8a4e1
crusher95 <dhawanutkarsh@gmail.com>
2016-04-03 02:10:03 +0530

    ScreenSaver

---
+++ js/screenSaver.js
@@ -0,0 +1,35 @@
+var timelimit=60000;//1 minute
+function screenSaver () {
+	Timer=setTimeout(activate,timelimit);
+	document.addEventListener('click',deactivate,false);
+	document.addEventListener('mousemove',deactivate,false);
+	document.addEventListener('keypress',deactivate,false);
+}
+function deactivate(){
+	clearTimeout(Timer);
+	var parent=document.getElementById("body");
+	var child=document.getElementById("screen");
+	child.style.display="none";
+	Timer=setTimeout(resume,timelimit);
+}
+function resume(){
+	var image=document.getElementById("screen");
+	image.style.display="block";
+}
+function activate(){
+	var image = document.createElement("img");
+	image.src="js/screensaver.jpg";
+	image.style.width="100%";
+	image.style.height="100%";
+	image.setAttribute("id","screen");
+	image.style.position="absolute";
+	image.style.top="0px";
+	image.style.right="0px";
+	image.style.left="0px";
+	var element = document.getElementById("body");
+	element.appendChild(image);
+	document.addEventListener('click',deactivate,false);
+	document.addEventListener('mousemove',deactivate,false);
+	document.addEventListener('keypress',deactivate,false);
+}
+	
