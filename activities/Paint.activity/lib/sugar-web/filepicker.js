3039803c68d48ca95d301bae0cc648b7c012c440
Lupin <mail@lupin.me>
2016-05-05 22:22:49 +0200

    finished gui

--- activities/Paint.activity/lib/sugar-web/filepicker.js
+++ activities/Paint.activity/lib/sugar-web/filepicker.js
@@ -1,19 +1,53 @@
 define(["sugar-web/datastore"], function(){
     var datastore = require("sugar-web/datastore")
     var filePicker = {};
+    filePicker.hidden = true;
+
+    filePicker.close = function(){
+	var modal = document.getElementById("filepicker-modal");
+
+	if (modal === undefined)
+	    console.log("No filepicker-modal found to display the files");
+	else
+	    modal.style.visibility = "hidden";
+	filePicker.hidden = true;
+    }
     
-    filePicker.browse = function(){
-	if (datastore.localStorage !== undefined){
-	    var files = datastore.localStorage.getAll();
-	    
-	    console.log(files);
+    filePicker.browseImages = function(){
+	var images = filePicker.getImages();
+	var modal = document.getElementById("filepicker-modal");
+
+	if (filePicker.hidden == true){
+	    if (modal === undefined)
+		console.log("No filepicker-modal found to display the files");
+	    else
+	    {
+		modal.style.visibility = "visible";
+		modal.innerHTML = "";
+		modal.innerHTML += "<div class='filepicker-toolbar'>"+
+		    "<button class='filepicker-button-close' id='filepicker-close'/></div"
+		for (var i = 0; i < images.length; i++){
+		    modal.innerHTML += "<div class='filepicker-image'>"+images[i].outerHTML+"</div>";
+		}
+	    }
+	    document.getElementById("filepicker-close").onclick = filePicker.close;
+	    filePicker.hidden = false;
+	}
+    };
+    
+    filePicker.getImages = function(){
+	var images = [];
+	
+	if (datastore.localStorage !== undefined){    
+	    var files = filePicker.getFiles();
 	    for (var key in files) {
 		if (files.hasOwnProperty(key)) {
 		    if (typeof JSON.parse(files[key]) == "string"){
-			img = new Image();
-			img.src = JSON.parse(files[key]);
-			console.log(img.outerHTML);
-			document.body.innerHTML = img.outerHTML;
+			if (files[key].indexOf("data:image") > -1){
+			    img = new Image();
+			    img.src = JSON.parse(files[key]);
+			    images.push(img);
+			}
 		    }
 		}
 	    }
@@ -21,6 +55,11 @@ define(["sugar-web/datastore"], function(){
 	else {
 	    console.log("Localstorage is empty");
 	}
+	return images;
+    };
+
+    filePicker.getImagesWithCallBack = function(callback){
+	callback(filePicker.getImages())
     }
 
     filePicker.getFiles = function(){
