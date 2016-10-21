745a1799f151e0f94b3c63d2ac4b8755b7a9fa1d
Michael Ohayon <mohayon@xebia.fr>
2016-10-21 13:47:54 +0200

    Added docker-compose run files and doc

--- server/settings.js
+++ server/settings.js
@@ -1,11 +1,15 @@
 // Load Sugarizer Settings
 
 var	fs = require('fs'),
-	ini = require('ini');
+ini = require('ini');
 
 // Load and parse sugarizer.ini file
 exports.load = function(callback) {
-	fs.readFile("sugarizer.ini", 'utf-8', function(err, content) {
+	var confFile = "sugarizer.ini"
+	if (process.argv.length >= 3) {
+		confFile = process.argv[2];
+	}
+	fs.readFile(confFile, 'utf-8', function(err, content) {
 		if (err) throw err;
 		callback(ini.parse(content));
 	});
