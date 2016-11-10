dcf39bca58fb7dbcd009256afdad00e2da1d55b7
llaske <llaske@c2s.fr>
2016-11-10 22:06:33 +0100

    Fix Gridpaint zoom computation

--- activities/Gridpaint.activity/js/events.js
+++ activities/Gridpaint.activity/js/events.js
@@ -26,80 +26,50 @@
 
 var onStart, onMove, onEnd;
 var zoom;
-var shiftTop;
 
 function eventInit(){
-	document.getElementById("canvas").onmousedown = evMousedown;	
-	frame.onmousemove = evMousemove;	
+	document.getElementById("canvas").onmousedown = evMousedown;
+	frame.onmousemove = evMousemove;
 	frame.onmouseup = evMouseup;
-	var wsize = document.body.clientWidth;
-	var wtop = 55;
-	if (wsize <= 480) {
-		zoom = 0.353;
-		wtop = 150;
-		shiftTop = -85;
-	} else if (wsize <= 640) {
-		zoom = 0.501;
-		wtop = 100;
-		shiftTop = -40;
-	} else if (wsize <= 854) {
-		zoom = 0.565;
-		wtop = 90;
-		shiftTop = -40;
-	} else if (wsize <= 960) {
-		zoom = 0.645;
-		wtop = 90;
-		shiftTop = -32;
-	} else if (wsize <= 1024) {
-		zoom = 0.95;
-		wtop = 60;
-		shiftTop = 0;
-	} else {
-		zoom = 1.10;
-		wtop = 55;	
-		shiftTop = 0;
-	}
+	var wsize = document.body.clientHeight-55;
+	zoom = wsize/748;
 	document.getElementById("canvas").style.zoom = zoom;
 	var useragent = navigator.userAgent.toLowerCase();
 	if (useragent.indexOf('chrome') == -1) {
 		document.getElementById("canvas").style.MozTransform = "scale("+zoom+")";
 		document.getElementById("canvas").style.MozTransformOrigin = "0 0";
 		document.getElementById("canvas").style.width = "1024px";
-		document.getElementById("canvas").style.height = "748px";	
-	} else {
-		document.getElementById("canvas").style.top = wtop + "px";
-		shiftTop = 0;
+		document.getElementById("canvas").style.height = "748px";
 	}
 }
 
 function evMousedown(e){
-	e.preventDefault(); 
+	e.preventDefault();
 	var x=localx(e.clientX), y=localy(e.clientY);
 	onStart(x,y);
 	// HACK: Force refresh on Android
 	if (/Android/i.test(navigator.userAgent) && document.location.protocol.substr(0,4) != "http") {
 		cnv.style.display='none';
 		cnv.offsetHeight;
-		cnv.style.display='block';	
+		cnv.style.display='block';
 	}
 }
 
 
 function evMousemove(e){
-	e.preventDefault(); 
+	e.preventDefault();
 	if(!onMove) return;
 	var x=localx(e.clientX), y=localy(e.clientY);
 	onMove(x,y);
 }
 
 function evMouseup(e){
-	e.preventDefault(); 
+	e.preventDefault();
 	if(!onEnd) return;
 	onEnd();
 }
 
 function localx(gx){return (gx/zoom-frame.getBoundingClientRect().left);}
 function localy(gy){
-	return (gy/zoom+shiftTop-frame.getBoundingClientRect().top);
+	return (gy/zoom-frame.getBoundingClientRect().top);
 }
-
