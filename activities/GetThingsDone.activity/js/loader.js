b78b89862f54b50323052a315d96390bf4d8a9f3
kiridix <matimar1414@gmail.com>
2016-12-04 14:06:04 -0300

    Update loader.js

--- activities/GetThingsDone.activity/js/loader.js
+++ activities/GetThingsDone.activity/js/loader.js
@@ -6,3 +6,11 @@ requirejs.config({
 });
 
 requirejs(["activity/activity"]);
+
+requirejs(["webL10n","sugar-web/env","moment-with-locales.min"], function(l10n, env, moment) {
+   l10n_s = l10n; //global declaration of translate interface
+
+   getSettings(function(settings) { //globally setting language from sugar settings
+			l10n_s.language.code = settings.language;
+			moment.locale(settings.language);
+	});
