7aa436479ec823a00674cc5b8fa4ef7042ec12f3
kiridix <matimar1414@gmail.com>
2016-12-06 17:01:37 -0300

    Update loader.js

--- activities/GetThingsDone.activity/js/loader.js
+++ activities/GetThingsDone.activity/js/loader.js
@@ -6,3 +6,32 @@ requirejs.config({
 });
 
 requirejs(["activity/activity"]);
+
+requirejs(["webL10n","sugar-web/env","moment-with-locales.min"], function(l10n, env, moment) {
+   l10n_s = l10n; //global declaration of translate interface
+
+   getSettings(function(settings) { //globally setting language from sugar settings
+			l10n_s.language.code = settings.language;
+			moment.locale(settings.language);
+	});
+
+   function getSettings(callback) {
+		var defaultSettings = {
+			name: "",
+			language: navigator.language
+		};
+		if (!env.isSugarizer()) {
+			callback(defaultSettings);
+			return;
+		}
+		if (typeof chrome != 'undefined' && chrome.app && chrome.app.runtime) {
+			var loadedSettings = JSON.parse(values.sugar_settings);
+			chrome.storage.local.get('sugar_settings', function(values) {
+				callback(loadedSettings);
+			});
+		} else {
+			var loadedSettings = JSON.parse(localStorage.sugar_settings);
+			callback(loadedSettings);
+		}
+	}
+});
