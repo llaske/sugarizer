c8163c6f907a5901723fb275519d074fa5e8a123
llaske <llaske@c2s.fr>
2016-12-15 22:01:10 +0100

    Fix issues on Markdown localization

--- activities/Markdown.activity/js/activity.js
+++ activities/Markdown.activity/js/activity.js
@@ -1,96 +1,95 @@
 define(["sugar-web/activity/activity","webL10n","sugar-web/datastore"], function (activity, webL10n, datastore) {
 
-    // Manipulate the DOM only when it is ready.
-    require(['domReady!'], function (doc) {
+	// Manipulate the DOM only when it is ready.
+	require(['domReady!'], function (doc) {
 
-        // Initialize the activity.
-        activity.setup();
-	var buttons = ["wmd-bold-button-second", "wmd-italic-button-second", "wmd-heading-button", "wmd-hr-button",
-			       "wmd-olist-button", "wmd-ulist-button", "wmd-code-button", "wmd-quote-button", "wmd-link-button",
+		// Initialize the activity.
+		activity.setup();
+		var buttons = ["insertText", "wmd-bold-button-second", "wmd-italic-button-second", "wmd-heading-button", "wmd-hr-button",
+				"wmd-olist-button", "wmd-ulist-button", "wmd-code-button", "wmd-quote-button", "wmd-link-button",
 				"wmd-undo-button", "wmd-redo-button"];
-        inputTextContent = document.getElementById("wmd-input-second");
-        inputTextContent.value = l10n_s.get("sample-input");
-	//to save and resume the contents from datastore.
+		inputTextContent = document.getElementById("wmd-input-second");
+		inputTextContent.value = "#"+l10n_s.get("sample-input");
 
-        var datastoreObject = activity.getDatastoreObject();
+		//to save and resume the contents from datastore.
+		var datastoreObject = activity.getDatastoreObject();
 
-        inputTextContent.onblur = function () {
+		inputTextContent.onblur = function () {
 
-            var jsonData = JSON.stringify((inputTextContent.value).toString());
-            datastoreObject.setDataAsText(jsonData);
-            datastoreObject.save(function () {});
-        };
-        markdownParsing(); //to load for the first time
-        datastoreObject.loadAsText(function (error, metadata, data) {
-            markdowntext = JSON.parse(data);
-            inputTextContent.value = markdowntext;
-            markdownParsing(); //to load again when there is a datastore entry
-        });
-	for (i = 0; i < buttons.length; i++) {
-		document.getElementById(buttons[i]).title = l10n_s.get(buttons[i]);
-		var journal = document.getElementById(buttons[i]);
+			var jsonData = JSON.stringify((inputTextContent.value).toString());
+			datastoreObject.setDataAsText(jsonData);
+			datastoreObject.save(function () {});
+		};
+		markdownParsing(); //to load for the first time
+		datastoreObject.loadAsText(function (error, metadata, data) {
+			markdowntext = JSON.parse(data);
+			inputTextContent.value = markdowntext;
+			markdownParsing(); //to load again when there is a datastore entry
+		});
 
-		    journal.onclick = function () {
-		        activity.showObjectChooser(function (error, result) {
-		            //result1 = result.toString();
-		            var datastoreObject = new datastore.DatastoreObject(result);
-		            datastoreObject.loadAsText(function (error, metadata, data) {
+		for (i = 0; i < buttons.length; i++) {
+			document.getElementById(buttons[i]).title = l10n_s.get(buttons[i]);
+		}
+		var journal = document.getElementById("insertText");
 
-		                try {
-		                    textdata = JSON.parse(data);
-		                } catch (e) {
-		                    textdata = data;
-		                }
+		journal.onclick = function () {
+			activity.showObjectChooser(function (error, result) {
+				//result1 = result.toString();
+				var datastoreObject2 = new datastore.DatastoreObject(result);
+				datastoreObject2.loadAsText(function (error, metadata, data) {
+					try {
+						textdata = JSON.parse(data);
+					} catch (e) {
+						textdata = data;
+					}
 
-		                var inputTextContent = document.getElementById("wmd-input-second");
-		                //inputTextContent.value += textdata;
-		                insertAtCursor(inputTextContent, textdata);
-		            });
+					var inputTextContent = document.getElementById("wmd-input-second");
+					//inputTextContent.value += textdata;
+					insertAtCursor(inputTextContent, textdata);
+				});
+			});
+		};
 
-		        });
-		    };
+		function insertAtCursor(myField, myValue) {
+			//IE support
+			if (document.selection) {
+				myField.focus();
+				sel = document.selection.createRange();
+				sel.text = myValue;
+			}
+			//MOZILLA and others
+			else if (myField.selectionStart || myField.selectionStart == '0') {
+				var startPos = myField.selectionStart;
+				var endPos = myField.selectionEnd;
+				myField.value = myField.value.substring(0, startPos)
+					+ myValue
+					+ myField.value.substring(endPos, myField.value.length);
+			} else {
+				myField.value += myValue;
+			}
+			//markdownParsing();
 		}
 
-        function insertAtCursor(myField, myValue) {
-            //IE support
-            if (document.selection) {
-                myField.focus();
-                sel = document.selection.createRange();
-                sel.text = myValue;
-            }
-            //MOZILLA and others
-            else if (myField.selectionStart || myField.selectionStart == '0') {
-                var startPos = myField.selectionStart;
-                var endPos = myField.selectionEnd;
-                myField.value = myField.value.substring(0, startPos)
-                    + myValue
-                    + myField.value.substring(endPos, myField.value.length);
-            } else {
-                myField.value += myValue;
-            }
-            //markdownParsing();
-        }
+		function markdownParsing() {
+			var converter2 = new Markdown.Converter();
 
-        function markdownParsing() {
-            var converter2 = new Markdown.Converter();
+			var help = function () {
+				alert(l10n_s.get("need-help"));
+			}
+			var options = {
+				helpButton: {
+					handler: help
+				},
+				strings: {
+					quoteexample: l10n_s.get("put-it-right-here")
+				}
+			};
 
-            var help = function () {
-                alert(l10n_s.get("need-help"));
-            }
-            var options = {
-                helpButton: {
-                    handler: help
-                },
-                strings: {
-                    quoteexample: l10n_s.get("put-it-right-here")
-                }
-            };
+			var editor2 = new Markdown.Editor(converter2, "-second", options);
 
-            var editor2 = new Markdown.Editor(converter2, "-second", options);
+			editor2.run();
+		}
 
-            editor2.run();
-        }
-
-    });
+	});
 
 });
