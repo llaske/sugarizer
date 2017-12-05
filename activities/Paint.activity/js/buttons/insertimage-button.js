2c605b05925c2ced2a6266780ffcb73feab91c20
kiridix <matimar1414@gmail.com>
2017-12-04 18:18:54 -0800

    Added preview of image

--- activities/Paint.activity/js/buttons/insertimage-button.js
+++ activities/Paint.activity/js/buttons/insertimage-button.js
@@ -1,78 +1,83 @@
 // Insert image button display
 define(['sugar-web/graphics/journalchooser','sugar-web/datastore'], function(chooser, datastore) {
 
-	function initGui() {
-		var insertImageButton = document.getElementById('insertimage-button');
-		PaintApp.elements.insertImageButton = insertImageButton;
-		insertImageButton.addEventListener('click', onInsertImageClick);
-	}
-	var printImage = false;
-	// onClick display dialog with journal content
-	function onInsertImageClick() {
-		// Display journal dialog popup
-		chooser.show(function(entry) {
-			// No selection
-			if (!entry) {
-				return;
-			}
+  function initGui() {
+    var insertImageButton = document.getElementById('insertimage-button');
+    PaintApp.elements.insertImageButton = insertImageButton;
+    insertImageButton.addEventListener('click', onInsertImageClick);
+  }
+  var printImage = false;
+  // onClick display dialog with journal content
+  function onInsertImageClick() {
+    // Display journal dialog popup
+    chooser.show(function(entry) {
+      // No selection
+      if (!entry) {
+        return;
+      }
 
-			// Get object content
-			var dataentry = new datastore.DatastoreObject(entry.objectId);
-			dataentry.loadAsText(function(err, metadata, text) {
-				//We load the drawing inside an image element
-				var element = document.createElement('img');
-				element.src = text;
-				element.onload = function() {
-					printImage = true;
-					//We draw the drawing to the canvas
-					var ctx = PaintApp.elements.canvas.getContext('2d');
-					var width;
-					var height;
-					var x;
-					var y;
-					
-					//Set the start position of the image
-					addEventListener("mousedown", function(event){
-						if(printImage){
-						width = event.clientX;
-						height = event.clientY;
-						x = width;
-						y = height;
-					}
-					});
-					//Calculate the size and print the image
-					addEventListener("mouseup", function(event){
-						if(printImage){
-						width = event.clientX - width;
-						height = event.clientY - height;
-						ctx.drawImage(element, x , y - 55, width, height)
-						printImage = false;
-					}
-					});
-					
-					PaintApp.saveCanvas();
+      // Get object content
+      var dataentry = new datastore.DatastoreObject(entry.objectId);
+      dataentry.loadAsText(function(err, metadata, text) {
+        //We load the drawing inside an image element
+        var element = document.createElement('img');
+        element.src = text;
+        element.onload = function() {
+          printImage = true;
+          //We draw the drawing to the canvas
+          var ctx = PaintApp.elements.canvas.getContext('2d');
+          var width;
+          var height;
+          var x;
+          var y;
+          var imagedata;
+          //Set the start position of the image
+          addEventListener("mousedown", function(event){
+            if(printImage){
+            width = event.clientX;
+            height = event.clientY;
+            x = width;
+            y = height;
+            imagedata = ctx.getImageData(x , y - 55, 60, 70);
+            ctx.drawImage(element, x , y - 55, 60, 70);
+          }
+          });
+          //Calculate the size and print the image
+          addEventListener("mouseup", function(event){
+            if(printImage){
+            ctx.putImageData(imagedata,x , y -55);
+            width = event.clientX - width;
+            height = event.clientY - height;
 
-					/* If the activity is shared we send the element to everyone */
-					if (PaintApp.data.isShared) {
-						try {
-							PaintApp.collaboration.sendMessage({
-								action: 'toDataURL',
-								data: {
-									width: PaintApp.elements.canvas.width / window.devicePixelRatio,
-									height: PaintApp.elements.canvas.height / window.devicePixelRatio,
-									src: PaintApp.collaboration.compress(PaintApp.elements.canvas.toDataURL())
-								}
-							});
-						} catch (e) {}
-			 		}
-				}
-			});
-		}, {mimetype: 'image/png'}, {mimetype: 'image/jpeg'});
-	}
+            ctx.drawImage(element, x , y - 55, width, height);
+            printImage = false;
+          }
+          })
 
-	var insertImageButton = {
-		initGui: initGui
-	};
 
-	return insertImageButton;
+          PaintApp.saveCanvas();
+
+          /* If the activity is shared we send the element to everyone */
+          if (PaintApp.data.isShared) {
+            try {
+              PaintApp.collaboration.sendMessage({
+                action: 'toDataURL',
+                data: {
+                  width: PaintApp.elements.canvas.width / window.devicePixelRatio,
+                  height: PaintApp.elements.canvas.height / window.devicePixelRatio,
+                  src: PaintApp.collaboration.compress(PaintApp.elements.canvas.toDataURL())
+                }
+              });
+            } catch (e) {}
+          }
+        }
+      });
+    }, {mimetype: 'image/png'}, {mimetype: 'image/jpeg'});
+  }
+
+  var insertImageButton = {
+    initGui: initGui
+  };
+
+  return insertImageButton;
 });
