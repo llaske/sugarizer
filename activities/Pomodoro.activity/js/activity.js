9ec4b63ab44fa938b31450dc2f7bea5564877ccc
sanatankumar <sanatan.iit@gmail.com>
2017-12-06 17:25:38 +0530

    added javascript related to  progress, stopwatch

--- activities/Pomodoro.activity/js/activity.js
+++ activities/Pomodoro.activity/js/activity.js
@@ -1,11 +1,15 @@
-define(["sugar-web/activity/activity"], function (activity) {
+define(['sugar-web/activity/activity', 'activity/progress', 'activity/stopwatch'], function (activity, progress, stopwatch) {
 
 	// Manipulate the DOM only when it is ready.
 	require(['domReady!'], function (doc) {
 
 		// Initialize the activity.
 		activity.setup();
-
+		var pomodoroContainer = document.getElementById('pomodoro-container')
+		console.log(stopwatch)
+		var pomodoro = new progress.Progress(50, '#444', 1, pomodoroContainer)
+		pomodoro.draw()
+		pomodoro.update(0.9)
+		// console.log(progress.Progress)
 	});
-
 });
