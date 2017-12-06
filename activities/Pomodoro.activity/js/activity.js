512d052895ea45a0aa0bb6abe892b1e6945c6e4f
sanatankumar <sanatan.iit@gmail.com>
2017-12-06 20:21:41 +0530

    added javascript to pause/play button

--- activities/Pomodoro.activity/js/activity.js
+++ activities/Pomodoro.activity/js/activity.js
@@ -1,14 +1,36 @@
 define(['sugar-web/activity/activity', 'activity/progress', 'activity/stopwatch'], function (activity, Progress, stopwatch) {
 
-	// Manipulate the DOM only when it is ready.
-	require(['domReady!'], function (doc) {
+  // Manipulate the DOM only when it is ready.
+  require(['domReady!'], function (doc) {
 
-		// Initialize the activity.
-		activity.setup();
-		var pomodoroContainer = document.getElementById('pomodoro-container')
-		console.log(stopwatch)
-		var pomodoro = new Progress(50, '#444', 1, pomodoroContainer)
-		pomodoro.draw()
-		pomodoro.update(0.9)
-	});
+    // Initialize the activity.
+    activity.setup();
+    var pomodoroContainer = document.getElementById('pomodoro-container')
+    console.log(stopwatch)
+    var pomodoro = new Progress(50, '#444', 1, pomodoroContainer)
+    pomodoro.draw()
+    pomodoro.update(0.9)
+
+    var playPauseButton = document.getElementById('play-button')
+    playPauseButton.addEventListener('click', function() {
+      if (playPauseButton.classList.contains('play')) {
+        handlePomodoroPause()
+      } else {
+        handlePomodoroPlay()
+      }
+    })
+
+  });
 });
+
+function handlePomodoroPlay() {
+  var playPauseButton = document.getElementById('play-button')
+  playPauseButton.classList.remove('pause')
+  playPauseButton.classList.add('play')
+}
+
+function handlePomodoroPause() {
+  var playPauseButton = document.getElementById('play-button')
+  playPauseButton.classList.remove('play')
+  playPauseButton.classList.add('pause')
+}
