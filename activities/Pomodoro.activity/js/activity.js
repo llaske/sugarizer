e24c6e1b60d08400779ea28b91b22a0bfb2e19a6
sanatankumar <sanatan.iit@gmail.com>
2017-12-07 11:04:37 +0530

    coverted main logic into a factory function

--- activities/Pomodoro.activity/js/activity.js
+++ activities/Pomodoro.activity/js/activity.js
@@ -4,32 +4,69 @@ define(['sugar-web/activity/activity', 'activity/progress', 'activity/stopwatch'
   require(['domReady!'], function (doc) {
 
     // Initialize the activity.
-    activity.setup();
-    var pomodoroContainer = document.getElementById('pomodoro-container')
-    var pomodoro = new Progress(280, '#F90052', 1, pomodoroContainer)
-    pomodoro.draw()
-    pomodoro.update(0.4)
+    activity.setup()
+    main(activity, Progress, stopwatch)
+  })
+})
 
+function main(activity, progress, stopwatch) {
+  handlePausePlay()
+
+  function mapRange(obj, num) {
+    return (((num - obj.from[0]) * (obj.to[1] - obj.to[0])) / (obj.from[1] - obj.from[0])) + obj.to[0]
+  }
+
+  function parseMs(ms) {
+    if (typeof ms !== 'number') {
+      throw new TypeError('Expected a number')
+    }
+
+    var roundTowardZero = ms > 0 ? Math.floor : Math.ceil
+
+    return {
+      days: roundTowardZero(ms / 86400000),
+      hours: roundTowardZero(ms / 3600000) % 24,
+      minutes: roundTowardZero(ms / 60000) % 60,
+      seconds: roundTowardZero(ms / 1000) % 60,
+      milliseconds: roundTowardZero(ms) % 1000
+    }
+  }
+
+  function convertReadableMS(timeInMs) {
+    var parsedTime = parseMs(timeInMs)
+    var timeStr = parsedTime.hours
+      ? `${parsedTime.hours + parsedTime.days * 24}:${parsedTime.minutes}:${parsedTime.seconds}`
+      : `${parsedTime.minutes}:${parsedTime.seconds}`
+    return timeStr
+      .split(':')
+      .map((num) => `${num}`.padStart(2, '0'))
+      .join(':')
+  }
+
+  function handlePausePlay() {
     var playPauseButton = document.getElementById('play-button')
+
     playPauseButton.addEventListener('click', function() {
       if (playPauseButton.classList.contains('play')) {
-        handlePomodoroPause()
+        playPauseButton.classList.remove('play')
+        playPauseButton.classList.add('pause')
       } else {
-        handlePomodoroPlay()
+        playPauseButton.classList.remove('pause')
+        playPauseButton.classList.add('play')
       }
     })
+  }
 
-  });
-});
+  function startWork() {
 
-function handlePomodoroPlay() {
-  var playPauseButton = document.getElementById('play-button')
-  playPauseButton.classList.remove('pause')
-  playPauseButton.classList.add('play')
-}
+  }
 
-function handlePomodoroPause() {
-  var playPauseButton = document.getElementById('play-button')
-  playPauseButton.classList.remove('play')
-  playPauseButton.classList.add('pause')
+  function startBreak() {
+
+  }
+
+  function stopPomodoro() {
+
+  }
+
 }
