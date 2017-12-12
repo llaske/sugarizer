6eac04b4036352a9aba89b1ff5aee8d053785bab
sanatankumar <sanatan.iit@gmail.com>
2017-12-12 08:04:25 +0530

    added pattern.js for patterns

---
+++ activities/GameOfLife.activity/js/patterns.js
@@ -0,0 +1,22 @@
+const generateRandomBoardState = () => (
+  new Array(30).fill(0)
+    .map(() => {
+      const row = new Array(50).fill(0)
+      return row.map(() => Math.floor(Math.random() * 2))
+    })
+)
+
+const glider = () => {
+  const pattern = generateRandomBoardState()
+  pattern[5][7] = 1
+  pattern[6][5] = 1
+  pattern[6][6] = 1
+  pattern[7][6] = 1
+  pattern[7][7] = 1
+  return glider()
+}
+const patterns = [generateRandomBoardState, pattern]
+
+define(() => {
+  return patterns
+})
