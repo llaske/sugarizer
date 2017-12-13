b9e8bc7bcca8613e8cd61bacce7c6da367e05e70
Abdulazeez Abdulazeez Adeshina <31009679+Youngestdev@users.noreply.github.com>
2017-12-13 13:03:06 +0100

    deleted notif.js

--- activities/QRCode.activity/js/notif.js
+++
@@ -1,7 +0,0 @@
-humane.baseCls = 'humane-original'
-         var png-button = document.getElementsById('png-button')
-         for (var i = 0; i < png-button.length; i++) { (function(el){
-            el.onclick = function () {
-               eval(el.innerHTML)
-            }
-         }(png-button[i])) }
