dbe2a709f667e3e414a7146e19d13cb854024a4b
llaske <llaske@c2s.fr>
2018-06-03 10:51:44 +0200

    Add some books and a library description

---
+++ activities/EbookReader.activity/js/constant.js
@@ -0,0 +1,37 @@
+
+// Constants
+var constant = {};
+
+constant.library = {
+	information: {
+		name: "contesfr",
+		title: "Contes célèbres",
+		image: "books/images/library.png",
+	},
+	database: [
+		{
+			title: "Les fables de La Fontaine",
+			author: "La Fontaine",
+			image: "books/images/fontaine1.png",
+			file: "books/LaFontaine_FablesI.epub",
+		},
+		{
+			title: "Alice au pays des merveilles",
+			author: "Lewis Carroll",
+			image: "books/images/alice.png",
+			file: "books/Carroll_Alice.epub",
+		},
+		{
+			title: "Les lettres de mon Moulin",
+			author: "Alphonse Daudet",
+			image: "books/images/moulin.png",
+			file: "books/pg36780-images.epub",
+		},
+		{
+			title: "Contes merveilleux - Tome I",
+			author: "Jacob Ludwig Karl Grimm",
+			image: "books/images/grimmI.png",
+			file: "books/Grimm_ContesI.epub",
+		},
+	]
+];
