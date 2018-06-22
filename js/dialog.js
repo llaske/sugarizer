b12a1df768c5f2656501969cbc0b66246c2c2ffd
llaske <llaske@c2s.fr>
2018-06-22 14:14:13 +0200

    Fix another sync issue when joining server from app, sync is now default

--- js/dialog.js
+++ js/dialog.js
@@ -1001,7 +1001,8 @@ enyo.kind({
 					color: preferences.getColor(),
 					language: preferences.getLanguage(),
 					role: "student",
-					password: this.$.password.getPassword()
+					password: this.$.password.getPassword(),
+					options: { sync: preferences.getOptions("sync"), stats: preferences.getOptions("stats") }
 				},
 				function(inSender, inResponse) {
 					that.login();
