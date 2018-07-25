4ef3b1a0f35d3eb22ac9f8349608c1e94814a5d1
llaske <llaske@c2s.fr>
2018-07-25 22:34:09 +0200

    Fix content URL access in VideoViewer and EbookReader in HTTPS

--- activities/VideoViewer.activity/constant.js
+++ activities/VideoViewer.activity/constant.js
@@ -4,5 +4,5 @@
 var constant = {};
 
 constant.pageCount = 4;
-constant.librariesUrl = "http://sugarizer.org/content/videos.json";
+constant.librariesUrl = (document.location.protocol.substr(0,5)=="https"?"https":"http") + "://sugarizer.org/content/videos.json";
 constant.videoType = "mp4";
