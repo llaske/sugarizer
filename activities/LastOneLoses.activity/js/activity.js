ed4b1ddf44ec226080c508d8f893ae797cb671fc
Mayank Kuthar <43777451+Kuthar8055@users.noreply.github.com>
2018-11-25 10:15:27 +0530

    Update activity.js

--- activities/LastOneLoses.activity/js/activity.js
+++ activities/LastOneLoses.activity/js/activity.js
@@ -1,7 +1,22 @@
 
 define(["sugar-web/activity/activity", "sugar-web/env", "sugar-web/graphics/icon", "webL10n","sugar-web/graphics/presencepalette"], function (activity, env, icon, webL10n, presencepalette) {
 	// Link presence palette
-        var palette = new presencepalette.PresencePalette(document.getElementById("network-button"), undefined);
+	var presence = null;
+	var palette = new presencepalette.PresencePalette(document.getElementById("network-button"), undefined);
+	palette.addEventListener('shared', function() {
+		palette.popDown();
+		console.log("Want to share");
+		presence = activity.getPresenceObject(function(error, network) {
+			if (error) {
+				console.log("Sharing error");
+				return;
+			}
+			network.createSharedActivity('org.sugarlabs.Pawn', function(groupId) {
+				console.log("Activity shared");
+			});
+			network.onDataReceived(onNetworkDataReceived);
+		});
+	});
 define(["sugar-web/activity/activity", "sugar-web/graphics/radiobuttonsgroup"], function (activity, radioButtonsGroup) {
 	var app;
 
