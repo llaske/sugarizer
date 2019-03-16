b9e22363dceb4760c7254238184fdb256d31b83b
Anshuman <anshu5074@gmail.com>
2019-03-16 11:22:51 +0530

    Add conversion units Data helper

---
+++ activities/UnitConverter.activity/lib/helper.js
@@ -0,0 +1,20 @@
+define(function() {
+  return (helper = {
+    lengths: {
+      meter: 1,
+      centimeter: 100,
+      inches: 39.37,
+      yards: 1.0936,
+      feet: 0.3048,
+      kilometer: 0.0001,
+      mile: 0.000621,
+    },
+    weights: {
+      kilogram: 1,
+      oounces: 35.274,
+      pounds: 2.2046,
+      grams: 1000,
+      stones: 0.1574,
+    },
+  });
+});
