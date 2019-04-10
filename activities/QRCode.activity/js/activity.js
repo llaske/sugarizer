424cdfa6b70f8eb26b8ff67c223a354e7df2fb6d
ashish0910 <ashish09101998@gmail.com>
2019-04-10 18:37:42 +0530

    feed raw value

--- activities/QRCode.activity/js/activity.js
+++ activities/QRCode.activity/js/activity.js
@@ -38,12 +38,13 @@ define(["sugar-web/activity/activity","sugar-web/datastore", "sugar-web/env", "w
 		var generateCode = function(text) {
 			qrCode.clear();
 			qrCode.makeCode(text);
+			addToHistory(text);
+			var text = userText.value.toLowerCase();
 			if (text.indexOf("http://") == 0 || text.indexOf("https://") == 0) {
 				document.getElementById("user-text").classList.add("text-url");
 			} else {
 				document.getElementById("user-text").classList.remove("text-url");
 			}
-			addToHistory(text);
 		}
 
 		// Process Resize events
