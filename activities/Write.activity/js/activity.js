027f1992d97521c3db43cf06df9c6a6dbcbf4ec4
ashish0910 <ashish09101998@gmail.com>
2019-07-20 14:11:07 +0530

    Collaboration stable

--- activities/Write.activity/js/activity.js
+++ activities/Write.activity/js/activity.js
@@ -15,7 +15,7 @@ define([
 ], function (activity,editpalette,formatpalette,listpalette,parapalette,fontPalette,colorpalette,sizepalette , datastore , journalchooser,env,presencepalette,exportpalette) {
 
 	// Manipulate the DOM only when it is ready.
-	requirejs(['domReady!'], function (doc) {
+	requirejs(['domReady!','humane'], function (doc,humane) {
 
 		// Initialize the activity.
 		activity.setup();
@@ -31,12 +31,14 @@ define([
 				resize: true,
 				toolbar: true
 			  },
+			  cursors: true
 			},
 			};
+		Quill.register('modules/cursors', QuillCursors);
 		var container = document.getElementById('editor');
 		var editor = new Quill(container,options);
 		editor.focus();
-		
+		const cursors = editor.getModule('cursors');
 		// Journal Handling (Load)
 		env.getEnvironment(function(err, environment) {
             
@@ -51,10 +53,25 @@ define([
                 activity.getDatastoreObject().loadAsText(function(error, metadata, data) {
                     if (error==null && data!=null) {
 						var delta = JSON.parse(data);
+						console.log(delta);
 						editor.setContents(delta);
                     }
                 });
-            }
+			}
+			// Shared instances
+			if (environment.sharedId) {
+				console.log("Shared instance");
+
+				// Hide GUI of undo and redo for non host users
+                document.getElementById("3").style.display = "none";
+                document.getElementById("4").style.display = "none";
+
+				presence = activity.getPresenceObject(function(error, network) {
+					
+					network.onDataReceived(onNetworkDataReceived);
+					network.onSharedActivityUserChanged(onNetworkUserChanged);
+				});
+			}
 
 		});
 		
@@ -75,6 +92,7 @@ define([
             
         });
 		
+		var changeMadebyUser=false;
 		// Initiating edit-text-palette ( for cut/copy/undo/redo )
 
 		var editButton = document.getElementById("edit-text");
@@ -149,7 +167,6 @@ define([
             {"id": 11, "title":"justify Left" , "cmd":"justifyLeft"},
             {"id": 12, "title":"justify Right" , "cmd":"justifyRight"},
             {"id": 13, "title":"justify Center" , "cmd":"justifyCenter"},
-            {"id": 14, "title":"justify Full" , "cmd":"justifyFull"},
         ];
         parapalette = new parapalette.Parapalette(paraButton, undefined);
         parapalette.setCategories(paraoptions);
@@ -159,17 +176,17 @@ define([
         });
 
         document.getElementById(11).addEventListener("click",function(){
-			editor.format('align','left');
+			changeMadebyUser=true;
+			editor.format('align','justify');
         })
         document.getElementById(12).addEventListener("click",function(){         
+			changeMadebyUser=true;
 			editor.format('align','right');
         });
         document.getElementById(13).addEventListener("click",function(){
-            editor.format('align','center');
+			changeMadebyUser=true;
+			editor.format('align','center');
         });
-        document.getElementById(14).addEventListener("click",function(){
-            editor.format('align','justify');
-		});
 		
 		// Initialise font palette
         var fontButton = document.getElementById("font-button");
@@ -178,7 +195,8 @@ define([
 			var newfont = e.detail.family;
 			if(newfont=="Arial") newfont="arial";
 			if(newfont=="Comic Sans MS") newfont="comic";
-            editor.format('font',newfont);
+			changeMadebyUser=true;
+			editor.format('font',newfont);
 		});
 		
 		// Initiating colour palette for foreground and background
@@ -186,14 +204,16 @@ define([
         var changeForeColorPalette = new colorpalette.ColorPalette(forecolorButton);
         changeForeColorPalette.setColor('rgb(0, 0, 0)');
 		changeForeColorPalette.addEventListener('colorChange', function(e) {
-            editor.format('color',e.detail.color);
+			changeMadebyUser=true;
+			editor.format('color',e.detail.color);
 		});
 		
 		var backcolorButton = document.getElementById("color-button-2");
         var changeBackColorPalette = new colorpalette.ColorPalette(backcolorButton);
         changeBackColorPalette.setColor('rgb(255,255,255)');
 		changeBackColorPalette.addEventListener('colorChange', function(e) {
-            editor.format('background-color',e.detail.color);
+			changeMadebyUser=true;
+			editor.format('background-color',e.detail.color);
 		});
 		
 
@@ -213,15 +233,19 @@ define([
 			editor.focus();
 		});
 		document.getElementById(15).addEventListener("click",function(){
+			changeMadebyUser=true;
 			editor.format('size','24px');
 		});
 		document.getElementById(16).addEventListener("click",function(){
+			changeMadebyUser=true;
 			editor.format('size','48px');
 		});
 		document.getElementById(17).addEventListener("click",function(){
+			changeMadebyUser=true;
 			editor.format('size','75px');
 		});
 		document.getElementById(18).addEventListener("click",function(){
+			changeMadebyUser=true;
 			editor.format('size','100px');
 		});
 
@@ -234,6 +258,7 @@ define([
 				var dataentry = new datastore.DatastoreObject(entry.objectId);
 				dataentry.loadAsText(function (err, metadata, data) {
 					editor.focus();
+					changeMadebyUser=true;
 					var Delta =  Quill.import('delta');
 					editor.updateContents(
 						new Delta()
@@ -321,10 +346,180 @@ define([
                     }, inputData);
                 }
             })
-            });
+			});
+			
+			editor.on('text-change', function(delta, oldDelta, source) {
+				
+				if ((source == 'user' || changeMadebyUser==true) && presence!=null) {
+					var range = editor.getSelection();
+					presence.sendMessage(presence.getSharedInfo().id, {
+						user: presence.getUserInfo(),
+						content: {
+							action: 'typing',
+							data: delta,
+							range: range
+						}
+					});
+					changeMadebyUser=false;
+				}
+				
+			});
 
+			editor.on('selection-change', function(range, oldRange, source) {
+				if (range) {
+				  if (range.length == 0 && presence!=null) {
+					presence.sendMessage(presence.getSharedInfo().id, {
+						user: presence.getUserInfo(),
+						content: {
+							action: 'selection',
+							range: range
+						}
+					});
+				  } 
+				} 
+			  });
+
+		// Presence Palette
+		// Link presence palette
+		var presence = null;
+		var isHost = false;
+		var myid;
+		var nomoreinit = false;
+		var mycursor;
+		var palette = new presencepalette.PresencePalette(document.getElementById("network-button"), undefined);
+        palette.addEventListener('shared', function() {
+            palette.popDown();
+            console.log("Want to share");
+            presence = activity.getPresenceObject(function(error, network) {
+                if (error) {
+                    console.log("Sharing error");
+                    return;
+                }
+                network.createSharedActivity('org.sugarlabs.Write', function(groupId) {
+                    console.log("Activity shared");
+                    isHost = true;
+                });
+                network.onDataReceived(onNetworkDataReceived);
+                network.onSharedActivityUserChanged(onNetworkUserChanged);
+            });
+		});
+		
+		var onNetworkDataReceived = function(msg) {
+			if (presence.getUserInfo().networkId === msg.user.networkId) {
+				return;
+			}
+			if(msg.content.action=='init' && nomoreinit==false){
+				editor.updateContents(msg.content.data);
+				var getallcursors = msg.content.allcursors;
+				for(var i = 0 ; i < getallcursors.length ; i++){
+					console.log(getallcursors[i]);
+					if(getallcursors[i].id!=myid){
+						cursors.createCursor(getallcursors[i].id, getallcursors[i].name,getallcursors[i].color) ;
+						cursors.moveCursor(getallcursors[i].id, getallcursors[i].range) ;
+					}
+				}
+				cursors.update();
+				nomoreinit=true;
+			}
+			if(msg.content.action=='typing'){
+				editor.updateContents(msg.content.data);
+				cursors.moveCursor(msg.user.networkId,msg.content.range);
+				console.log(msg.user.networkId);
+			}
+			if(msg.content.action=='selection'){
+				cursors.moveCursor(msg.user.networkId,msg.content.range);
+			}
+			
+		}; 
+
+		var onNetworkUserChanged = function(msg) {
+			if (isHost) {
+				var data = editor.getContents();
+				var range = editor.getSelection();
+				mycursor.range = range;
+				var allcursors = cursors.cursors();
+				presence.sendMessage(presence.getSharedInfo().id, {
+					user: presence.getUserInfo(),
+					content: {
+						action: 'init',
+						data: data,
+						allcursors: allcursors
+					}
+				});
+				mycursor.range = null;
+			}
+			if(!myid){
+                myid = msg.user.networkId;
+            }
+			// handle user enter/exit Notifications
+            var userName = msg.user.name.replace('<', '&lt;').replace('>', '&gt;');
+			var html = "<img style='height:30px;' src='" + generateXOLogoWithColor(msg.user.colorvalue) + "'>";
+			presence.listSharedActivities(function(activities){
+                for (var i = 0; i < activities.length; i++) {
+                    if (activities[i].id === presence.getSharedInfo().id) {
+                        getConnectedPeople(activities[i].users);
+                    }
+                  }
+            });
+            for (var key in connectedPeople) {
+                console.log(connectedPeople[key].name,key,connectedPeople[key].networkId);
+            }
+			if(msg.move==1){
+				humane.log(html+userName+" Joined");
+				var c = cursors.createCursor(msg.user.networkId, userName, msg.user.colorvalue.stroke);
+				if(myid==msg.user.networkId) {mycursor=c;}
+			}
+			if (msg.move === -1) {
+				cursors.removeCursor(msg.user.networkId);
+				humane.log(html+userName+" Left");
+				if(msg.user.networkId == connectedPeople[0].networkId){
+					if(connectedPeople[1].networkId == myid){
+						document.getElementById(3).style.display = "inline";
+						document.getElementById(4).style.display = "inline";
+						isHost=true;
+					}
+				}
+			}	
+		};
+		
+		
+		var xoLogo = '<?xml version="1.0" ?><!DOCTYPE svg  PUBLIC \'-//W3C//DTD SVG 1.1//EN\'  \'http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\' [<!ENTITY stroke_color "#010101"><!ENTITY fill_color "#FFFFFF">]><svg enable-background="new 0 0 55 55" height="55px" version="1.1" viewBox="0 0 55 55" width="55px" x="0px" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" y="0px"><g display="block" id="stock-xo_1_"><path d="M33.233,35.1l10.102,10.1c0.752,0.75,1.217,1.783,1.217,2.932   c0,2.287-1.855,4.143-4.146,4.143c-1.145,0-2.178-0.463-2.932-1.211L27.372,40.961l-10.1,10.1c-0.75,0.75-1.787,1.211-2.934,1.211   c-2.284,0-4.143-1.854-4.143-4.141c0-1.146,0.465-2.184,1.212-2.934l10.104-10.102L11.409,24.995   c-0.747-0.748-1.212-1.785-1.212-2.93c0-2.289,1.854-4.146,4.146-4.146c1.143,0,2.18,0.465,2.93,1.214l10.099,10.102l10.102-10.103   c0.754-0.749,1.787-1.214,2.934-1.214c2.289,0,4.146,1.856,4.146,4.145c0,1.146-0.467,2.18-1.217,2.932L33.233,35.1z" fill="&fill_color;" stroke="&stroke_color;" stroke-width="3.5"/><circle cx="27.371" cy="10.849" fill="&fill_color;" r="8.122" stroke="&stroke_color;" stroke-width="3.5"/></g></svg>';
+        function generateXOLogoWithColor(color) {
+            var coloredLogo = xoLogo;
+            coloredLogo = coloredLogo.replace("#010101", color.stroke)
+            coloredLogo = coloredLogo.replace("#FFFFFF", color.fill)
+        
+            return "data:image/svg+xml;base64," + btoa(coloredLogo);
+		}
+		
+		var connectedPeople = {};
+        // Maintain a list of connected users
+        function displayConnectedPeople(){
+            var presenceUsersDiv = document.getElementById("presence-users");
+            var html = "<hr><ul style='list-style: none; padding:0;'>";
+            for (var key in connectedPeople) {
+                html += "<li><img style='height:30px;' src='" + generateXOLogoWithColor(connectedPeople[key].colorvalue) + "'>" + connectedPeople[key].name + "</li>"
+            }
+              html += "</ul>"
+              presenceUsersDiv.innerHTML = html
+        }
+
+        function getConnectedPeople(users){
+            var presenceUsersDiv = document.getElementById("presence-users");
+            if (!users || !presenceUsersDiv) {
+            return;
+            }
+            connectedPeople = {};
+            presence.listSharedActivityUsers(presence.getSharedInfo().id , function(usersConnected){
+                connectedPeople = {};
+                for (var i = 0; i < usersConnected.length; i++) {
+                    var userConnected = usersConnected[i];
+                    connectedPeople[i] = userConnected;
+                }
+                displayConnectedPeople();
+            });
+        }
 
 	});
-
-	
+		
 });
