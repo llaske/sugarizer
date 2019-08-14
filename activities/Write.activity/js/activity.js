bbc9ff6a52ad4b4bbf0bd980065572177eeaf283
ashish0910 <ashish09101998@gmail.com>
2019-08-14 14:54:17 +0530

    Remove Blobs for images

--- activities/Write.activity/js/activity.js
+++ activities/Write.activity/js/activity.js
@@ -252,8 +252,6 @@ define([
 				}
 				var dataentry = new datastore.DatastoreObject(entry.objectId);
 				dataentry.loadAsText(function (err, metadata, data) {
-					var blob = base64toBlob(metadata.mimetype, data);
-					var blobUrl = URL.createObjectURL(blob);
 					editor.focus();
 					changeMadebyUser=true;
 					var Delta =  Quill.import('delta');
