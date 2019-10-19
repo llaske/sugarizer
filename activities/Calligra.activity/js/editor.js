c34350e81c00e43e210d43584500540c0d21fa19
Lionel Laské <llaske@c2s.fr>
2019-10-19 22:10:04 +0200

    Transform detail into a Canvas

--- activities/Calligra.activity/js/editor.js
+++ activities/Calligra.activity/js/editor.js
@@ -5,21 +5,28 @@ var Editor = {
 			<div id="back" class="editor-goback" v-on:click="goBack()"></div>
 			<img id="miniletter" class="editor-miniletter" v-bind:src="item.image" v-on:load="onLoad()"></img>
 			<div id="area" class="editor-area">
-				<img id="letter" v-bind:src="item.image"></img>
+				<canvas id="letter"></canvas>
 			</div>
 		</div>`,
 	props: ['item'],
 	methods: {
 		fitSize: function() {
-			var canvas = document.getElementById("canvas") || document.getElementById("body");
-			var canvas_height = canvas.offsetHeight-50;
-			var canvas_width = canvas.offsetWidth-50;
-			var size = { width: canvas_width, height: canvas_height };
+			var body = document.getElementById("canvas") || document.getElementById("body");
+			var body_height = body.offsetHeight-50;
+			var body_width = body.offsetWidth-50;
+			var size = { width: body_width, height: body_height };
 			var lettersize = Math.min(size.width, size.height);
 			var letter = document.getElementById("letter");
-			letter.style.width = lettersize + "px";
+			letter.width = lettersize;
+			letter.height = lettersize;
 			letter.style.marginLeft = (size.width-lettersize)/2-50 + "px";
-			//console.log("ratio="+lettersize/letter.naturalWidth);
+			var context = letter.getContext('2d');
+			var imageObj = new Image();
+			imageObj.onload = function() {
+				context.drawImage(imageObj, 0, 0, lettersize, lettersize);
+			};
+			imageObj.src = this.item.image;
+			//console.log("ratio="+lettersize/document.getElementById("miniletter").naturalWidth);
 		},
 
 		onLoad: function() {
