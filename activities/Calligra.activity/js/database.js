9b34be154f947e35efaa6a49ba3bdfa3da07ea76
Lionel Laské <llaske@c2s.fr>
2019-10-21 22:53:31 +0200

    Put database in a distinct file, add lower a and lower i

---
+++ activities/Calligra.activity/js/database.js
@@ -0,0 +1,67 @@
+
+
+// Default templates
+var defaultTemplates = [
+	{
+		name: "template-lower",
+		images: [
+			{image:"icons/lower-a.svg","starts":[{"x":115,"y":98,"path":[{"x":113,"y":94},{"x":111,"y":92},{"x":109,"y":90},{"x":106,"y":88},{"x":103,"y":87},{"x":100,"y":87},{"x":97,"y":87},{"x":93,"y":88},{"x":90,"y":89},{"x":87,"y":91},{"x":85,"y":94},{"x":83,"y":97},{"x":82,"y":101},{"x":82,"y":105},{"x":83,"y":109},{"x":84,"y":112},{"x":86,"y":115},{"x":88,"y":117},{"x":91,"y":119},{"x":94,"y":120},{"x":98,"y":121},{"x":101,"y":121},{"x":104,"y":120},{"x":107,"y":118},{"x":110,"y":116},{"x":113,"y":113},{"x":115,"y":109},{"x":116,"y":105},{"x":116,"y":101},{"x":116,"y":97},{"x":116,"y":93},{"x":116,"y":90},{"x":116,"y":87}]},{"x":116,"y":87,"path":[{"x":116,"y":90},{"x":116,"y":93},{"x":116,"y":97},{"x":116,"y":101},{"x":116,"y":105},{"x":116,"y":109},{"x":116,"y":112},{"x":116,"y":116},{"x":117,"y":119},{"x":119,"y":121},{"x":122,"y":121},{"x":125,"y":120},{"x":128,"y":118},{"x":131,"y":116},{"x":134,"y":114},{"x":137,"y":111},{"x":139,"y":108}]}]},
+			{image:"icons/lower-b.svg"},
+			{image:"icons/lower-c.svg", starts:[{"x":108,"y":87,"path":[{"x":104,"y":86},{"x":101,"y":86},{"x":98,"y":87},{"x":95,"y":88},{"x":92,"y":90},{"x":89,"y":93},{"x":87,"y":96},{"x":86,"y":99},{"x":86,"y":103},{"x":87,"y":107},{"x":88,"y":110},{"x":90,"y":113},{"x":92,"y":115},{"x":94,"y":117},{"x":97,"y":119},{"x":100,"y":120},{"x":103,"y":121},{"x":106,"y":121},{"x":109,"y":121},{"x":112,"y":120},{"x":115,"y":118},{"x":118,"y":116},{"x":121,"y":114},{"x":123,"y":112},{"x":125,"y":110},{"x":127,"y":108}]}]},
+			{image:"icons/lower-d.svg"},
+			{image:"icons/lower-e.svg"},
+			{image:"icons/lower-f.svg"},
+			{image:"icons/lower-g.svg"},
+			{image:"icons/lower-h.svg"},
+			{image:"icons/lower-i.svg","starts":[{"x":92,"y":108,"path":[{"x":94,"y":104},{"x":96,"y":101},{"x":98,"y":98},{"x":99,"y":95},{"x":100,"y":92},{"x":101,"y":89},{"x":102,"y":86},{"x":102,"y":89},{"x":102,"y":92},{"x":102,"y":95},{"x":102,"y":98},{"x":102,"y":101},{"x":102,"y":104},{"x":102,"y":107},{"x":102,"y":110},{"x":102,"y":113},{"x":102,"y":116},{"x":103,"y":119},{"x":105,"y":121},{"x":108,"y":121},{"x":111,"y":120},{"x":114,"y":118},{"x":117,"y":116},{"x":120,"y":114},{"x":122,"y":112},{"x":124,"y":110},{"x":126,"y":108}]},{"x":103,"y":75,"path":[]}]},
+			{image:"icons/lower-j.svg"},
+			{image:"icons/lower-k.svg"},
+			{image:"icons/lower-l.svg"},
+			{image:"icons/lower-m.svg"},
+			{image:"icons/lower-n.svg"},
+			{image:"icons/lower-o.svg"},
+			{image:"icons/lower-p.svg"},
+			{image:"icons/lower-q.svg"},
+			{image:"icons/lower-r.svg"},
+			{image:"icons/lower-s.svg"},
+			{image:"icons/lower-t.svg"},
+			{image:"icons/lower-u.svg"},
+			{image:"icons/lower-v.svg"},
+			{image:"icons/lower-w.svg"},
+			{image:"icons/lower-x.svg"},
+			{image:"icons/lower-y.svg"},
+			{image:"icons/lower-z.svg"}
+		]
+	},
+	{
+		name: "template-upper",
+		images: [
+			{image:"icons/upper-a.svg"},
+			{image:"icons/upper-b.svg"},
+			{image:"icons/upper-c.svg"},
+			{image:"icons/upper-d.svg"},
+			{image:"icons/upper-e.svg"},
+			{image:"icons/upper-f.svg"},
+			{image:"icons/upper-g.svg"},
+			{image:"icons/upper-h.svg", starts: [{x:97, y:65}]},
+			{image:"icons/upper-i.svg"},
+			{image:"icons/upper-j.svg"},
+			{image:"icons/upper-k.svg"},
+			{image:"icons/upper-l.svg"},
+			{image:"icons/upper-m.svg"},
+			{image:"icons/upper-n.svg"},
+			{image:"icons/upper-o.svg"},
+			{image:"icons/upper-p.svg"},
+			{image:"icons/upper-q.svg"},
+			{image:"icons/upper-r.svg"},
+			{image:"icons/upper-s.svg"},
+			{image:"icons/upper-t.svg"},
+			{image:"icons/upper-u.svg"},
+			{image:"icons/upper-v.svg"},
+			{image:"icons/upper-w.svg"},
+			{image:"icons/upper-x.svg"},
+			{image:"icons/upper-y.svg"},
+			{image:"icons/upper-z.svg"}
+		]
+	}
+];
