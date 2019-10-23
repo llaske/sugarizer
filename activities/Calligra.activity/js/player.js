2004e672c74844899f539c8fcf95a21539601e7f
Lionel Laské <llaske@c2s.fr>
2019-10-23 22:15:40 +0200

    First implementation of drawing

--- activities/Calligra.activity/js/player.js
+++ activities/Calligra.activity/js/player.js
@@ -13,7 +13,7 @@ var Player = {
 		return {
 			size: -1,
 			zoom: -1,
-			current: { start: -1, track: -1, tracks: [] },
+			current: { start: -1, stroke: -1, strokes: [], index: -1 },
 			mode: '',
 			drawing: false
 		}
@@ -62,8 +62,28 @@ var Player = {
 				}
 				var x = Math.floor((e.clientX-letter.getBoundingClientRect().left)/vm.zoom);
 				var y = Math.floor((e.clientY-letter.getBoundingClientRect().top)/vm.zoom);
-				vm.current.tracks.push({x: x, y: y});
-				vm.drawStoke();
+				var target = (vm.item.starts[vm.current.start].path.length?vm.item.starts[vm.current.start].path[vm.current.index]:vm.item.starts[vm.current.start]);
+				var distance = Math.sqrt(Math.pow(x-target.x,2)+Math.pow(y-target.y,2));
+				if (distance < 2)
+				{
+					vm.current.strokes[vm.current.start].push({x: x, y: y});
+					vm.drawStoke();
+					vm.current.index++;
+					if (vm.current.index >= vm.item.starts[vm.current.start].path.length) {
+						vm.current.start++;
+						vm.current.index = 0;
+						if (vm.current.start >= vm.item.starts.length) {
+							console.log("END");
+							vm.mode = 'end';
+						} else {
+							var lines = [];
+							lines.push({x: vm.item.starts[vm.current.start].x, y: vm.item.starts[vm.current.start].y});
+							lines.push({x: vm.item.starts[vm.current.start].x, y: vm.item.starts[vm.current.start].y});
+							vm.current.strokes.push(lines);
+							vm.drawStoke();
+						}
+					}
+				}
 			});
 		},
 
@@ -79,7 +99,7 @@ var Player = {
 				context.drawImage(imageObj, 0, 0, vm.size, vm.size);
 
 				// Draw current drawing
-				if (vm.mode == 'input' && vm.current.tracks.length) {
+				if (vm.mode == 'input' && vm.current.strokes.length) {
 					vm.drawStoke();
 				}
 			};
@@ -91,16 +111,18 @@ var Player = {
 			var vm = this;
 			var letter = document.getElementById("letter");
 			var context = letter.getContext('2d');
-			context.beginPath();
 			context.strokeStyle = app.color.stroke;
 			context.lineWidth = 10;
 			context.lineCap = "round";
 			context.lineJoin = "round";
-			context.moveTo(vm.zoom*vm.current.tracks[0].x, vm.zoom*vm.current.tracks[0].y);
-			for (var i = 1 ; i < vm.current.tracks.length ; i++) {
-				context.lineTo(vm.zoom*vm.current.tracks[i].x, vm.zoom*vm.current.tracks[i].y);
+			for (var i = 0 ; i < vm.current.strokes.length ; i++) {
+				context.beginPath();
+				context.moveTo(vm.zoom*vm.current.strokes[i].x, vm.zoom*vm.current.strokes[i].y);
+				for (var j = 1 ; j < vm.current.strokes[i].length ; j++) {
+					context.lineTo(vm.zoom*vm.current.strokes[i][j].x, vm.zoom*vm.current.strokes[i][j].y);
+				}
+				context.stroke();
 			}
-			context.stroke();
 		},
 
 		onLoad: function() {
@@ -119,7 +141,7 @@ var Player = {
 		vm.mode = 'show';
 		var step = function() {
 			// Draw a segment of path
-			var line = vm.current.tracks[vm.current.start][vm.current.track];
+			var line = vm.current.strokes[vm.current.start][vm.current.stroke];
 			var letter = document.getElementById("letter");
 			if (!letter) {
 				return;
@@ -133,21 +155,25 @@ var Player = {
 			context.moveTo(vm.zoom*line.x0, vm.zoom*line.y0);
 			context.lineTo(vm.zoom*line.x1, vm.zoom*line.y1);
 			context.stroke();
-			vm.current.track++;
-			if (vm.current.track >= vm.current.tracks[vm.current.start].length) {
+			vm.current.stroke++;
+			if (vm.current.stroke >= vm.current.strokes[vm.current.start].length) {
 				vm.current.start++;
-				vm.current.track = 0;
-				if (vm.current.start < vm.current.tracks.length) {
+				vm.current.stroke = 0;
+				if (vm.current.start < vm.current.strokes.length) {
 					setTimeout(step, timeout);
 				} else {
 					setTimeout(function() {
+						// En of draw, start input mode
 						vm.mode = 'input';
 						vm.current.start = 0;
-						vm.current.track = 0;
-						vm.current.tracks = [];
+						vm.current.stroke = 0;
+						vm.current.index = 0;
+						vm.current.strokes = [];
 						if (vm.item.starts && vm.item.starts.length) {
-							vm.current.tracks.push({x: vm.item.starts[0].x, y: vm.item.starts[0].y});
-							vm.current.tracks.push({x: vm.item.starts[0].x, y: vm.item.starts[0].y});
+							var lines = [];
+							lines.push({x: vm.item.starts[0].x, y: vm.item.starts[0].y});
+							lines.push({x: vm.item.starts[0].x, y: vm.item.starts[0].y});
+							vm.current.strokes.push(lines);
 						}
 						vm.draw();
 					}, 500);
@@ -159,8 +185,8 @@ var Player = {
 		if (vm.item.starts) {
 			// Create lines set to draw letter
 			vm.current.start = 0;
-			vm.current.track = 0;
-			vm.current.tracks = [];
+			vm.current.stroke = 0;
+			vm.current.strokes = [];
 			for (var i = 0 ; i < vm.item.starts.length ; i++) {
 				if (!vm.item.starts[i].path) {
 					continue;
@@ -177,7 +203,7 @@ var Player = {
 						lines.push({x0: path[j].x, y0: path[j].y, x1: path[j+1].x, y1: path[j+1].y});
 					}
 				}
-				vm.current.tracks.push(lines);
+				vm.current.strokes.push(lines);
 			}
 			setTimeout(step, timeout);
 		}
