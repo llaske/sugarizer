1d00de21d2530193e212ba0e95bf055f381bc944
Lionel Laské <llaske@c2s.fr>
2019-10-29 16:04:02 +0100

    Add edit mode for template view

--- activities/Calligra.activity/js/template.js
+++ activities/Calligra.activity/js/template.js
@@ -4,12 +4,17 @@ var TemplateItem = {
 	template: `
 		<div class="col-xs-4 col-md-2 col-lg-2 template-col" v-on:click="onClick">
 			<img class="mr3 template-item-image" v-bind:src="image"></img>
+			<div id="remove-item" v-bind:class="computeClass()"></div>
 		</div>
 		`,
-	props: ['image'],
+	props: ['image','editMode'],
 	methods: {
 		onClick: function() {
 			this.$emit('clicked');
+		},
+
+		computeClass: function() {
+			return "template-item-remove" + (this.editMode?" template-item-remove-editable":"");
 		}
 	}
 }
@@ -23,15 +28,21 @@ var TemplateViewer = {
 			<template-item v-for="(item,i) in template.images" :key="item.image"
 				v-bind:ref="'item'+(i++)"
 				v-bind:image="item.image"
+				v-bind:editMode="editMode"
 				v-on:clicked="itemClicked(item)">
 			</template-item>
 		</template>
 		</div>
 	`,
 	props: ['template'],
+	data: function() {
+		return {
+			editMode: false
+		}
+	},
 	methods: {
 		itemClicked: function(item) {
-			this.$emit('selected', item);
+			this.$emit('selected', item, this.editMode);
 		}
 	}
 };
