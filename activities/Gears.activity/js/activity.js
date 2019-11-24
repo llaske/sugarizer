b5f8e4e32b8e2e98ac0d2b55e24e1d4ea35fcb85
Sarthak gupta <sarthakgupta041@gmail.com>
2019-11-24 23:19:33 +0530

    Add updateCanvasSize for future safety

--- activities/Gears.activity/js/activity.js
+++ activities/Gears.activity/js/activity.js
@@ -122,12 +122,14 @@ define(["sugar-web/activity/activity","sugar-web/graphics/radiobuttonsgroup","ge
 			document.getElementById("main-toolbar").style.opacity = 0;
             document.getElementById("canvas").style.top = "0px";
             gearSketch.canvasOffsetY = gearSketch.canvas.getBoundingClientRect().top;
+            gearSketch.updateCanvasSize();
             document.getElementById("unfullscreen-button").style.visibility = "visible";
 		});
 		document.getElementById("unfullscreen-button").addEventListener('click', function() {
 			document.getElementById("main-toolbar").style.opacity = 1;
             document.getElementById("canvas").style.top = "55px";
             gearSketch.canvasOffsetY = gearSketch.canvas.getBoundingClientRect().top;
+            gearSketch.updateCanvasSize();
             document.getElementById("unfullscreen-button").style.visibility = "hidden";
         });
 
