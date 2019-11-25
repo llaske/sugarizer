d85e5891978817ca65aabee9f2f7deb826fd1e8d
Larissa Gilliane Melo De Moura <larissagilliane@A304-08L.imd.local>
2019-11-25 16:22:08 -0300

    Revert "corrections"
    This reverts commit b102f4c3538bcc50c582eaa837b806419ea82039.

--- activities/TamTamMicro.activity/js/activity.js
+++ activities/TamTamMicro.activity/js/activity.js
@@ -8,6 +8,7 @@ define(["sugar-web/activity/activity", "tutorial"], function (activity, tutorial
 		tutorial.start();
 	});
 
+
 	// Manipulate the DOM only when it is ready.
 	requirejs(['domReady!'], function (doc) {
 		// Initialize the activity.
@@ -18,12 +19,15 @@ define(["sugar-web/activity/activity", "tutorial"], function (activity, tutorial
 		sound.renderInto(document.getElementById("audio"));
 
 		// Launch main screen
-		app = new TamTam.App({activity: activity});
+		app = new TamTam.App({ activity: activity });
 		app.renderInto(document.getElementById("keyboard"));
 
 		// Stop sound at end of game to sanitize media environment, specifically on Android
 		document.getElementById("stop-button").addEventListener('click', function (event) {
 			sound.pause();
 		});
+
+
 	});
+
 });
