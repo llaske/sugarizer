e172181944a550c3af610c3f48049a1fa6b03be9
justnoone00 <shikhar100mit@gmail.com>
2019-12-07 21:36:57 +0530

    Fixed change in original identation

--- activities/Fototoon.activity/js/activity.js
+++ activities/Fototoon.activity/js/activity.js
@@ -1,4 +1,4 @@
-define(["sugar-web/activity/activity", "sugar-web/datastore", "sugar-web/env", "textpalette", "sugar-web/graphics/menupalette", "sugar-web/graphics/journalchooser", "lzstring", "webL10n", "toon", "tutorial"], function (activity, datastore, env, textpalette, menupalette, journalchooser, lzstring, l10n, toon, tutorial) {
+define(["sugar-web/activity/activity","sugar-web/datastore","sugar-web/env","textpalette","sugar-web/graphics/menupalette","sugar-web/graphics/journalchooser","lzstring","webL10n","toon","tutorial"], function (activity, datastore, env, textpalette, menupalette, journalchooser, lzstring, l10n, toon, tutorial) {
 
 
     // initialize canvas size
@@ -6,7 +6,7 @@ define(["sugar-web/activity/activity", "sugar-web/datastore", "sugar-web/env", "
     var sugarSubCellSize = 15;
     var language;
 
-    env.getEnvironment(function (err, environment) {
+    env.getEnvironment(function(err, environment) {
         var defaultLanguage = (typeof chrome != 'undefined' && chrome.app && chrome.app.runtime) ? chrome.i18n.getUILanguage() : navigator.language;
         language = environment.user ? environment.user.language : defaultLanguage;
         l10n.language.code = language;
@@ -31,7 +31,7 @@ define(["sugar-web/activity/activity", "sugar-web/datastore", "sugar-web/env", "
         activity.setup();
 
         // HERE GO YOUR CODE
-        var initialData = { "version": "1", "boxs": [{ 'globes': [] }] };
+        var initialData = {"version": "1", "boxs": [{'globes':[]}]};
 
         var mainCanvas = document.getElementById("mainCanvas");
         var sortCanvas = document.getElementById("sortCanvas");
@@ -70,31 +70,21 @@ define(["sugar-web/activity/activity", "sugar-web/datastore", "sugar-web/env", "
 
         var addGlobeButton = document.getElementById("add-globe");
         addGlobeButton.title = _('AddAglobe');
-        var menuData = [{
-            'icon': true, 'id': toon.TYPE_GLOBE,
-            'label': _('Globe')
-        },
-        {
-            'icon': true, 'id': toon.TYPE_EXCLAMATION,
-            'label': _('Exclamation')
-        },
-        {
-            'icon': true, 'id': toon.TYPE_WHISPER,
-            'label': _('Whisper')
-        },
-        {
-            'icon': true, 'id': toon.TYPE_CLOUD,
-            'label': _('Think')
-        },
-        {
-            'icon': true, 'id': toon.TYPE_RECTANGLE,
-            'label': _('Box')
-        },];
+        var menuData = [{'icon': true, 'id': toon.TYPE_GLOBE,
+            'label': _('Globe')},
+        {'icon': true, 'id': toon.TYPE_EXCLAMATION,
+            'label': _('Exclamation')},
+        {'icon': true, 'id': toon.TYPE_WHISPER,
+            'label': _('Whisper')},
+        {'icon': true, 'id': toon.TYPE_CLOUD,
+            'label': _('Think')},
+        {'icon': true, 'id': toon.TYPE_RECTANGLE,
+            'label': _('Box')},];
         var mp = new menupalette.MenuPalette(addGlobeButton,
             _("AddAglobe"), menuData);
 
         for (var i = 0; i < mp.buttons.length; i++) {
-            mp.buttons[i].addEventListener('click', function (e) {
+            mp.buttons[i].addEventListener('click', function(e) {
                 toonModel.addGlobe(this.id);
             });
         };
@@ -102,55 +92,55 @@ define(["sugar-web/activity/activity", "sugar-web/datastore", "sugar-web/env", "
         var addButton = document.getElementById("add-button");
         addButton.title = _("Add");
         addButton.addEventListener('click', function (e) {
-            journalchooser.show(function (entry) {
-                // No selection
-                if (!entry) {
-                    return;
-                }
-                // Get object content
-                var dataentry = new datastore.DatastoreObject(entry.objectId);
-                dataentry.loadAsText(function (err, metadata, text) {
-                    //We load the drawing inside an image element
-                    var element = document.createElement('img');
-                    if (entry.metadata.activity == 'org.olpcfrance.PaintActivity') {
-                        element.src = lzstring.decompressFromUTF16(JSON.parse(text).src);
-                    } else {
-                        element.src = text;
+                    journalchooser.show(function (entry) {
+                    // No selection
+                    if (!entry) {
+                        return;
                     }
-                    element.onload = function () {
-                        toonModel.addImage(element.src);
-                    };
-                });
-            }, { mimetype: 'image/png' }, { mimetype: 'image/jpeg' }, { activity: 'org.olpcfrance.PaintActivity' });
+                    // Get object content
+                    var dataentry = new datastore.DatastoreObject(entry.objectId);
+                    dataentry.loadAsText(function (err, metadata, text) {
+                        //We load the drawing inside an image element
+                        var element = document.createElement('img');
+                        if (entry.metadata.activity == 'org.olpcfrance.PaintActivity') {
+                            element.src = lzstring.decompressFromUTF16(JSON.parse(text).src);
+                        } else {
+                            element.src = text;
+                        }
+                        element.onload = function () {
+                            toonModel.addImage(element.src);
+                        };
+                    });
+                }, { mimetype: 'image/png' }, { mimetype: 'image/jpeg' }, { activity: 'org.olpcfrance.PaintActivity' });
         });
 
         // Load from datatore
-        env.getEnvironment(function (err, environment) {
-            if (environment.objectId) {
-                activity.getDatastoreObject().loadAsText(function (error, metadata, JSONdata) {
-                    if (error == null && JSONdata != null) {
-                        var data = JSON.parse(JSONdata);
-                        var dataWithoutImages = data.dataWithoutImages;
-                        var images = data.dataImages;
-                        dataWithoutImages.images = {};
-                        for (var key in images) {
-                            var imageName = key;
-                            dataWithoutImages.images[imageName] = LZString.decompressFromUTF16(images[imageName]);
+            env.getEnvironment(function(err, environment) {
+                if (environment.objectId) {
+                    activity.getDatastoreObject().loadAsText(function(error, metadata, JSONdata) {
+                        if (error==null && JSONdata!=null) {
+                            var data = JSON.parse(JSONdata);
+                            var dataWithoutImages = data.dataWithoutImages;
+                            var images = data.dataImages;
+                            dataWithoutImages.images = {};
+                            for (var key in images) {
+                                var imageName = key;
+                                dataWithoutImages.images[imageName] = LZString.decompressFromUTF16(images[imageName]);
+                            }
+                            toonModel.setData(dataWithoutImages);
+                            if (!editMode) {
+                                toonModel.changeToEditMode();
+                                editMode = true;
+                            };
                         }
-                        toonModel.setData(dataWithoutImages);
-                        if (!editMode) {
-                            toonModel.changeToEditMode();
-                            editMode = true;
-                        };
-                    }
-                });
-            }
-        });
+                    });
+                }
+            });
 
-        var stopButton = document.getElementById("stop-button");
-        stopButton.addEventListener('click', function (event) {
-            console.log("writing...");
-            toonModel.showWait();
+            var stopButton = document.getElementById("stop-button");
+            stopButton.addEventListener('click', function (event) {
+                console.log("writing...");
+                toonModel.showWait();
 
             if (!editMode) {
                 toonModel.finishSort();
@@ -164,47 +154,47 @@ define(["sugar-web/activity/activity", "sugar-web/datastore", "sugar-web/env", "
             dataWithoutImages['boxs'] = toonModel.getData()['boxs'];
 
             dataImages = {};
-            for (var key in toonModel.getData()['images']) {
+            for(var key in toonModel.getData()['images']) {
                 var imageName = key;
                 console.log('saving image ' + imageName);
-                dataImages[imageName] = LZString.compressToUTF16(toonModel.getData()['images'][imageName]);
+                    dataImages[imageName] = LZString.compressToUTF16(toonModel.getData()['images'][imageName]);
             };
 
-            var fullData = {
-                dataWithoutImages: dataWithoutImages,
-                dataImages: dataImages
-            };
+                var fullData = {
+                    dataWithoutImages: dataWithoutImages,
+                    dataImages: dataImages
+                };
             toonModel.hideWait();
-            var jsonData = JSON.stringify(fullData);
-            activity.getDatastoreObject().setDataAsText(jsonData);
-            activity.getDatastoreObject().save(function (error) {
-                if (error === null) {
-                    console.log("write done.");
-                } else {
-                    console.log("write failed.");
-                }
+                var jsonData = JSON.stringify(fullData);
+                activity.getDatastoreObject().setDataAsText(jsonData);
+                activity.getDatastoreObject().save(function (error) {
+                    if (error === null) {
+                        console.log("write done.");
+                    } else {
+                        console.log("write failed.");
+                    }
+                });
             });
-        });
 
         var saveImageButton = document.getElementById("image-save");
-        saveImageButton.title = _("SaveAsImage");
-        var saveImageMenuData = [{ 'id': '0', 'label': _('OneRow') },
-        { 'id': '1', 'label': _('OneColumn') },
-        { 'id': '2', 'label': _('TwoColumns') }];
+            saveImageButton.title = _("SaveAsImage");
+        var saveImageMenuData = [{'id': '0', 'label': _('OneRow')},
+                {'id': '1', 'label': _('OneColumn')},
+                {'id': '2', 'label': _('TwoColumns')}];
         var simp = new menupalette.MenuPalette(saveImageButton,
             _("SaveAsImage"), saveImageMenuData);
 
         for (var i = 0; i < simp.buttons.length; i++) {
-            simp.buttons[i].addEventListener('click', function (e) {
+            simp.buttons[i].addEventListener('click', function(e) {
                 toonModel.saveAsImage(this.id);
             });
         };
 
         var sortButton = document.getElementById("sort-button");
-        sortButton.title = _('Sort');
+            sortButton.title = _('Sort');
         toonModel.attachSortButton(sortButton);
 
-        sortButton.addEventListener('click', function (e) {
+        sortButton.addEventListener('click', function(e) {
             // verify if there are at least 3 boxes
             // the first box is the title and can't be moved
             // then only have sense sort with more than 2 boxes
@@ -232,7 +222,7 @@ define(["sugar-web/activity/activity", "sugar-web/datastore", "sugar-web/env", "
         });
 
         var cleanAllButton = document.getElementById("clean-all-button");
-        cleanAllButton.title = _("Clean");
+            cleanAllButton.title = _("Clean");
 
         cleanAllButton.addEventListener('click', function (e) {
             toonModel.setData(initialData);
@@ -245,10 +235,10 @@ define(["sugar-web/activity/activity", "sugar-web/datastore", "sugar-web/env", "
         // Launch tutorial
         document.getElementById("help-button").addEventListener('click', function (e) {
             l10n.language.code = language;
-            i = 1;
+            var once = 1;
             window.addEventListener("localized", function () {
-                if (i) {
-                    i = 0;
+                if (once) {
+                    once = 0;
                     tutorial.start(language);
                 }
             });
