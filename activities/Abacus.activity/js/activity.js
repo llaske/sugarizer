37088a66b9690626231fc309d5a34af91809fb2c
Nathan Dimmer <nathandimmer@gmail.com>
2019-12-07 10:41:35 -0500

    fix: board resizes correctly now

--- activities/Abacus.activity/js/activity.js
+++ activities/Abacus.activity/js/activity.js
@@ -1,32 +1,29 @@
-define(["sugar-web/activity/activity", 'easeljs', 'tweenjs', 'activity/game', 'activity/standardabacus', 'activity/standardabacuscolumn', 'activity/abacusbead', 'activity/onecolumnabacus', 'activity/posnegcolumn'], function (act) {
+define(["sugar-web/activity/activity",'easeljs','tweenjs','activity/game','activity/standardabacus','activity/standardabacuscolumn','activity/abacusbead','activity/onecolumnabacus','activity/posnegcolumn'], function (act) {
 
 	// Manipulate the DOM only when it is ready.
 	requirejs(['domReady!'], function (doc) {
 
 		// Initialize the activity.
-		requirejs(["sugar-web/env", "sugar-web/datastore", "fraction", "activity/abacuspalette", "activity/custompalette"], function (env, datastore, fraction, abacuspalette, custompalette) {
+		requirejs(["sugar-web/env","sugar-web/datastore","fraction","activity/abacuspalette","activity/custompalette"], function(env,datastore,fraction,abacuspalette,custompalette) {
 			act.setup();
 			act.getXOColor(function (error, colors) {
-				runactivity(act, doc, colors, env, datastore, fraction, abacuspalette, custompalette);
-				
+				runactivity(act,doc,colors,env,datastore,fraction,abacuspalette,custompalette);
 			});
-
-			
 		});
 	});
 
 });
 
-function runactivity(act, doc, colors, env, datastore, fraction, abacuspalette, custompalette) {
+function runactivity(act,doc,colors,env,datastore,fraction,abacuspalette,custompalette){
 	var canvas;
 	var stage;
 	var g;
 	var e;
 
-	function init() {
+	function init(){
 		canvas = document.getElementById('actualcanvas');
 		canvas.width = window.innerWidth;
-		canvas.height = window.innerHeight - 55;
+		canvas.height = window.innerHeight-55;
 		stage = new createjs.Stage(canvas);
 		stage.update();
 		stage.mouseEventsEnabled = true;
@@ -34,14 +31,11 @@ function runactivity(act, doc, colors, env, datastore, fraction, abacuspalette, 
 
 		createjs.Ticker.setFPS(30);
 		createjs.Ticker.addEventListener("tick", handleTick);
-
 		function handleTick() {
 			stage.update();
 		}
-		var g = new Game(act, stage, colors, fraction, doc, abacuspalette, custompalette, datastore);
-		setTimeout(function () {
-			g.init();
-		}, 500);
+		var g = new Game(act,stage,colors,fraction,doc,abacuspalette,custompalette,datastore);
+		setTimeout(function(){ g.init(); }, 500);
 
 		window.addEventListener('activityStop', function (eve) {
 			eve.preventDefault();
@@ -63,22 +57,26 @@ function runactivity(act, doc, colors, env, datastore, fraction, abacuspalette, 
 		});
 
 		window.addEventListener('resize', resizeCanvas, false);
-
 		function resizeCanvas() {
+			if (document.getElementById("unfullscreen-button").style.visibility === "hidden") {
 			canvas.width = window.innerWidth;
-			canvas.height = window.innerHeight - 55;
+			canvas.height = window.innerHeight-55;
 			g.resize();
+			} else {
+				canvas.width = window.innerWidth;
+				canvas.height = window.innerHeight;
+				g.resize();
+			}
 		}
 
 		var clearButton = doc.getElementById("clear-button");
-		clearButton.addEventListener('click', function (a) {
+			clearButton.addEventListener('click', function (a) {
 			g.clear();
 		});
 		var copyButton = doc.getElementById("copy-button");
-		copyButton.addEventListener('click', function (a) {
+			copyButton.addEventListener('click', function (a) {
 			g.copy();
 		});
-
 	}
 	init();
 }
