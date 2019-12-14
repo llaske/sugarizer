dda58247f9762ab7383a41072a455287b68a1bf9
jontymagan <jontymagansashreek@gmail.com>
2019-12-14 16:12:26 +0530

    Removed buttons on fullscreen.

--- activities/Abecedarium.activity/js/activity.js
+++ activities/Abecedarium.activity/js/activity.js
@@ -35,21 +35,41 @@ define(["sugar-web/activity/activity","sugar-web/datastore"], function (activity
 		document.getElementById("stop-button").addEventListener('click', function (event) {
 			Abcd.sound.pause();
 		});
+		function hidebutton(){
+			var x = document.getElementsByClassName("switchCase");
+			var y = document.getElementsByClassName("switchLang");
+			var i;
+			for (i = 0; i < x.length; i++) {
+  			x[i].style.visibility = "hidden";
+  			}
+  			for (i = 0; i < y.length; i++) {
+  			y[i].style.visibility = "hidden";
+  			}
+  		}
+  		function showbutton(){
+			var x = document.getElementsByClassName("switchCase");
+			var y = document.getElementsByClassName("switchLang");
+			var i;
+			for (i = 0; i < x.length; i++) {
+  			x[i].style.visibility = "visible";
+  			}
+  			for (i = 0; i < y.length; i++) {
+  			y[i].style.visibility = "visible";
+  			}
+  		}
 
 		// Full screen
 		document.getElementById("fullscreen-button").addEventListener('click', function() {
 			document.getElementById("main-toolbar").style.opacity = 0;
 			document.getElementById("canvas").style.top = "0px";
-			document.getElementById("unfullscreen-button").style.visibility = "visible";
-			document.getElementById("learn_caseButton").style.visibility = "hidden";
-			document.getElementById("learn_languageButton").style.visibility = "hidden";
+			document.getElementById("unfullscreen-button").style.visibility = "visible";  
+			hidebutton();
 		});
 		document.getElementById("unfullscreen-button").addEventListener('click', function() {
 			document.getElementById("main-toolbar").style.opacity = 1;
 			document.getElementById("canvas").style.top = "55px";
 			document.getElementById("unfullscreen-button").style.visibility = "hidden";
-			document.getElementById("learn_caseButton").style.visibility = "visible";
-			document.getElementById("learn_languageButton").style.visibility = "visible";
+			showbutton();
 		});
 	});
 
