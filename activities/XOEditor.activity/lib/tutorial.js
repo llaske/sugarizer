96b03c27c066b554bbd5dd95a045eb185063478b
sdziuda <szymondziuda95@gmail.com>
2019-12-16 20:41:24 +0100

    Rearrange tutorial
    Rearranged the tutorial. Also fixed the indentation in tutorial.js and moved Help button to the left.

--- activities/XOEditor.activity/lib/tutorial.js
+++ activities/XOEditor.activity/lib/tutorial.js
@@ -11,6 +11,12 @@ define(["webL10n"], function(l10n) {
 				content: l10n.get("TutoExplainContent")
 			},
 			{
+				element: "#actualcanvas",
+				placement: "top",
+				title: l10n.get("TutoCanvasTitle"),
+				content: l10n.get("TutoCanvasContent")
+			},
+			{
 				element: "#save-button",
 				placement: "bottom",
 				title: l10n.get("TutoSaveTitle"),
@@ -22,51 +28,38 @@ define(["webL10n"], function(l10n) {
 				title: l10n.get("TutoResetTitle"),
 				content: l10n.get("TutoResetContent")
 			},
-			{
-				element: "#actualcanvas",
-				placement: "top",
-				title: l10n.get("TutoCanvasTitle"),
-				content: l10n.get("TutoCanvasContent")
-			}
 		];
 		var tour = new Tour({
-			template:
-				"\
-            <div class='popover tour'>\
-                <div class='arrow'></div>\
-                <h3 class='popover-title tutorial-title'></h3>\
-                <div class='popover-content'></div>\
-                <div class='popover-navigation' style='display: flex; flex-wrap:wrap; justify-content: center; align-items: center'>\
-                    <div class='tutorial-prev-icon icon-button' data-role='prev'>\
-                        <div class='tutorial-prev-icon1 web-activity'>\
-                            <div class='tutorial-prev-icon2 web-activity-icon'></div>\
-                            <div class='tutorial-prev-icon3 web-activity-disable'></div>\
-                        </div>\
-                        <div class='icon-tutorial-text'>" +
-				l10n.get("TutoPrev") +
-				"</div>\
-                    </div>\
-                    <span data-role='separator' style='margin: 4px'>|</span>\
-                    <div class='tutorial-next-icon icon-button' data-role='next'>\
-                        <div class='tutorial-next-icon1 web-activity'>\
-                            <div class='tutorial-next-icon2 web-activity-icon'></div>\
-                            <div class='tutorial-next-icon3 web-activity-disable'></div>\
-                        </div>\
-                        <div class='icon-tutorial-text'>" +
-				l10n.get("TutoNext") +
-				"</div>\
-                    </div>\
-                    <div class='tutorial-end-icon icon-button' data-role='end'>\
-                        <div class='tutorial-end-icon1 web-activity'>\
-                            <div class='tutorial-end-icon2 web-activity-icon'></div>\
-                            <div class='tutorial-end-icon3 web-activity-disable'></div>\
-                        </div>\
-                        <div class='icon-tutorial-text'>" +
-				l10n.get("TutoEnd") +
-				"</div>\
-                    </div>\
-                </div>\
-            </div>",
+			template:"\
+			<div class='popover tour'>\
+				<div class='arrow'></div>\
+				<h3 class='popover-title tutorial-title'></h3>\
+				<div class='popover-content'></div>\
+				<div class='popover-navigation' style='display: flex; flex-wrap:wrap; justify-content: center; align-items: center'>\
+					<div class='tutorial-prev-icon icon-button' data-role='prev'>\
+						<div class='tutorial-prev-icon1 web-activity'>\
+							<div class='tutorial-prev-icon2 web-activity-icon'></div>\
+							<div class='tutorial-prev-icon3 web-activity-disable'></div>\
+						</div>\
+						<div class='icon-tutorial-text'>"+l10n.get("TutoPrev")+"</div>\
+					</div>\
+					<span data-role='separator' style='margin: 4px'>|</span>\
+					<div class='tutorial-next-icon icon-button' data-role='next'>\
+						<div class='tutorial-next-icon1 web-activity'>\
+							<div class='tutorial-next-icon2 web-activity-icon'></div>\
+							<div class='tutorial-next-icon3 web-activity-disable'></div>\
+						</div>\
+						<div class='icon-tutorial-text'>"+l10n.get("TutoNext")+"</div>\
+					</div>\
+					<div class='tutorial-end-icon icon-button' data-role='end'>\
+						<div class='tutorial-end-icon1 web-activity'>\
+							<div class='tutorial-end-icon2 web-activity-icon'></div>\
+							<div class='tutorial-end-icon3 web-activity-disable'></div>\
+						</div>\
+						<div class='icon-tutorial-text'>"+l10n.get("TutoEnd")+"</div>\
+					</div>\
+				</div>\
+			</div>",
 			storage: false,
 			backdrop: true,
 			steps: steps
