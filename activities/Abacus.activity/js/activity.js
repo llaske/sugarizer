a9b13ad4f0ca2a385f432d3c7c04f6b2e2649c4d
sdziuda <szymondziuda95@gmail.com>
2019-12-18 15:46:50 +0100

    Add tutorial for Abacus activity
    Added te tutorial for Abacus activity

--- activities/Abacus.activity/js/activity.js
+++ activities/Abacus.activity/js/activity.js
@@ -4,17 +4,24 @@ define(["sugar-web/activity/activity",'easeljs','tweenjs','activity/game','activ
 	requirejs(['domReady!'], function (doc) {
 
 		// Initialize the activity.
-		requirejs(["sugar-web/env","sugar-web/datastore","fraction","activity/abacuspalette","activity/custompalette"], function(env,datastore,fraction,abacuspalette,custompalette) {
+		requirejs(["sugar-web/env","sugar-web/datastore","fraction","activity/abacuspalette","activity/custompalette","tutorial","webL10n"], function(env,datastore,fraction,abacuspalette,custompalette,tutorial,webL10n) {
 			act.setup();
+			env.getEnvironment(function(err, environment) {
+				currentenv = environment;
+				// Set current language to Sugarizer
+				var defaultLanguage = (typeof chrome != 'undefined' && chrome.app && chrome.app.runtime) ? chrome.i18n.getUILanguage() : navigator.language;
+				var language = environment.user ? environment.user.language : defaultLanguage;
+				webL10n.language.code = language;
+			});
 			act.getXOColor(function (error, colors) {
-				runactivity(act,doc,colors,env,datastore,fraction,abacuspalette,custompalette);
+				runactivity(act,doc,colors,env,datastore,fraction,abacuspalette,custompalette,tutorial);
 			});
 		});
 	});
 
 });
 
-function runactivity(act,doc,colors,env,datastore,fraction,abacuspalette,custompalette){
+function runactivity(act,doc,colors,env,datastore,fraction,abacuspalette,custompalette,tutorial){
 	var canvas;
 	var stage;
 	var g;
@@ -56,6 +63,11 @@ function runactivity(act,doc,colors,env,datastore,fraction,abacuspalette,customp
 		var copyButton = doc.getElementById("copy-button");
 			copyButton.addEventListener('click', function (a) {
 			g.copy();
+		});
+
+		// Launch tutorial
+		document.getElementById("help-button").addEventListener('click', function(e) {
+			tutorial.start();
 		});
 	}
 	init();
