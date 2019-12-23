f52e58a1595b1786d8baaec22a1ccb078c3064bf
jontymagan <jontymagansashreek@gmail.com>
2019-12-23 22:09:33 +0530

    Fixed Issue.

--- activities/GameOfLife.activity/js/activity.js
+++ activities/GameOfLife.activity/js/activity.js
@@ -217,24 +217,18 @@ function main(Board, State, patterns, color, shadeColor, l10n, dataStore, genSpe
     });
   })
 
-  function resize() {
-    var x = document.getElementsByClassName("main")
-    for(var i=0;i<x.length;i++){
-      x[i].style.height="100%";
-    }
-  }
-
   // Full screen
   document.getElementById("fullscreen-button").addEventListener('click', function() {
     document.getElementById("main-toolbar").style.opacity = 0;
     document.getElementById("canvas").style.top = "0px";
     document.getElementById("unfullscreen-button").style.visibility = "visible";
-    resize();
+    //board.handleResize(window.innerWidth, state.state.boardState,state);
   });
   document.getElementById("unfullscreen-button").addEventListener('click', function() {
     document.getElementById("main-toolbar").style.opacity = 1;
     document.getElementById("canvas").style.top = "55px";
     document.getElementById("unfullscreen-button").style.visibility = "hidden";
+    //board.handleResize(window.innerWidth, state.state.boardState,state);
   });
 
   document.querySelector('#random').addEventListener('click', function () {
