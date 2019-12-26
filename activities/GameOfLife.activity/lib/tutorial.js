edae3fc646b599492fbc8fe6a9efc502f52f054c
sdziuda <szymondziuda95@gmail.com>
2019-12-26 12:27:19 +0100

    Add photos for GameOfLife tutorial
    Added the photos to better explain the rules of Game of Life. Also added the popups for Speed and Size button.

--- activities/GameOfLife.activity/lib/tutorial.js
+++ activities/GameOfLife.activity/lib/tutorial.js
@@ -2,7 +2,63 @@ define(["webL10n"], function (l10n) {
 	var tutorial = {};
 
 	tutorial.start = function() {
-		var steps = [
+		var tour = new Tour({
+			template: "\
+			<div class='popover tour'>\
+				<div class='arrow'></div>\
+				<h3 class='popover-title tutorial-title'></h3>\
+				<table><tr><td style='vertical-align:top;'><div id='icon-tutorial' style='visibility:hidden;display:inline-block;'></div>\
+				</td><td><div class='popover-content'></div></td></tr></table>\
+				<div class='popover-navigation' style='display: flex; flex-wrap:wrap; justify-content: center; align-items: center'>\
+					<div class='tutorial-prev-icon icon-button' data-role='prev'>\
+						<div class='tutorial-prev-icon1 web-activity'>\
+							<div class='tutorial-prev-icon2 web-activity-icon'></div>\
+							<div class='tutorial-prev-icon3 web-activity-disable'></div>\
+						</div>\
+						<div class='icon-tutorial-text'>"+l10n.get("TutoPrev")+"</div>\
+					</div>\
+					<span data-role='separator' style='margin: 4px'>|</span>\
+					<div class='tutorial-next-icon icon-button' data-role='next'>\
+						<div class='tutorial-next-icon1 web-activity'>\
+							<div class='tutorial-next-icon2 web-activity-icon'></div>\
+							<div class='tutorial-next-icon3 web-activity-disable'></div>\
+						</div>\
+						<div class='icon-tutorial-text'>"+l10n.get("TutoNext")+"</div>\
+					</div>\
+					<div class='tutorial-end-icon icon-button' data-role='end'>\
+						<div class='tutorial-end-icon1 web-activity'>\
+							<div class='tutorial-end-icon2 web-activity-icon'></div>\
+							<div class='tutorial-end-icon3 web-activity-disable'></div>\
+						</div>\
+						<div class='icon-tutorial-text'>"+l10n.get("TutoEnd")+"</div>\
+					</div>\
+				</div>\
+			</div>",
+			storage: false,
+			backdrop: true,
+			steps: [],
+			onShown: function() {
+				if (tutorial.icons && tutorial.icons.steps && tutorial.icons.steps[tour.getCurrentStep()]) {
+					var icon = tutorial.icons.steps[tour.getCurrentStep()];
+					var iconElement = document.getElementById("icon-tutorial");
+					iconElement.style.visibility = 'visible';
+					iconElement.style.backgroundImage = "url('"+ icon.directory + "/" + icon.icon + "')";
+					iconElement.style.backgroundSize = icon.size + "px";
+					iconElement.style.width = icon.size + "px";
+					iconElement.style.height = icon.size + "px";
+					iconElement.style.marginTop = "15px";
+					iconElement.style.marginLeft = "5px";
+					if (icon.color) {
+						iconLib.colorize(iconElement, icon.color, function(){});
+					}
+				}
+			},
+			onEnd: function() {
+				tutorial.elements = [];
+				tutorial.icons = null;
+			}
+		});
+		tour.addSteps([
 			{
 				element: "",
 				orphan: true,
@@ -53,6 +109,18 @@ define(["webL10n"], function (l10n) {
 				content: l10n.get("TutoRunContent"),
 			},
 			{
+				element: "#speed-button",
+				placement: "bottom",
+				title: l10n.get("TutoSpeedTitle"),
+				content: l10n.get("TutoSpeedContent"),
+			},
+			{
+				element: "#size-button",
+				placement: "bottom",
+				title: l10n.get("TutoSizeTitle"),
+				content: l10n.get("TutoSizeContent"),
+			},
+			{
 				element: "#random",
 				placement: "bottom",
 				title: l10n.get("TutoRandomTitle"),
@@ -76,42 +144,15 @@ define(["webL10n"], function (l10n) {
 				title: l10n.get("TutoClearTitle"),
 				content: l10n.get("TutoClearContent"),
 			},
-		];
-		var tour = new Tour({
-			template: "\
-			<div class='popover tour'>\
-				<div class='arrow'></div>\
-				<h3 class='popover-title tutorial-title'></h3>\
-				<div class='popover-content'></div>\
-				<div class='popover-navigation' style='display: flex; flex-wrap:wrap; justify-content: center; align-items: center'>\
-					<div class='tutorial-prev-icon icon-button' data-role='prev'>\
-						<div class='tutorial-prev-icon1 web-activity'>\
-							<div class='tutorial-prev-icon2 web-activity-icon'></div>\
-							<div class='tutorial-prev-icon3 web-activity-disable'></div>\
-						</div>\
-						<div class='icon-tutorial-text'>"+l10n.get("TutoPrev")+"</div>\
-					</div>\
-					<span data-role='separator' style='margin: 4px'>|</span>\
-					<div class='tutorial-next-icon icon-button' data-role='next'>\
-						<div class='tutorial-next-icon1 web-activity'>\
-							<div class='tutorial-next-icon2 web-activity-icon'></div>\
-							<div class='tutorial-next-icon3 web-activity-disable'></div>\
-						</div>\
-						<div class='icon-tutorial-text'>"+l10n.get("TutoNext")+"</div>\
-					</div>\
-					<div class='tutorial-end-icon icon-button' data-role='end'>\
-						<div class='tutorial-end-icon1 web-activity'>\
-							<div class='tutorial-end-icon2 web-activity-icon'></div>\
-							<div class='tutorial-end-icon3 web-activity-disable'></div>\
-						</div>\
-						<div class='icon-tutorial-text'>"+l10n.get("TutoEnd")+"</div>\
-					</div>\
-				</div>\
-			</div>",
-			storage: false,
-			backdrop: true,
-			steps: steps
-		});
+		]);
+		tutorial.icons = {
+			steps: {
+				2: {icon: 'underpopulation.png', directory: 'photos', size: 100},
+				3: {icon: 'nextgeneration.png', directory: 'photos', size: 100},
+				4: {icon: 'overpopulation.png', directory: 'photos', size: 100},
+				5: {icon: 'reproduction.png', directory: 'photos', size: 100},
+			}
+		};
 		tour.init();
 		tour.start(true);
 
