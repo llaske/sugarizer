00ad3865fd26c6b8f02d534b252398bd563d1cc1
Marcus Chong <marcus.jn.chong@gmail.com>
2020-01-02 15:22:28 -0500

    capture all notes

--- activities/TamTamMicro.activity/whitekeys.js
+++ activities/TamTamMicro.activity/whitekeys.js
@@ -6,27 +6,27 @@ enyo.kind({
                 {tag: "span", content: "1", classes:"number"}
             ]},
 
-            { tag: "li", name: "D", classes:"standard orange", components: [
+            { tag: "li", name: "D", classes:"standard orange", ontap: "handlePlayNote", components: [
                 {tag: "span", content: "2", classes:"number"}
             ]},
 
-            { tag: "li", name: "E", classes:"standard yellow", components: [
+            { tag: "li", name: "E", classes:"standard yellow", ontap: "handlePlayNote", components: [
                 {tag: "span", content: "3", classes:"number"}
             ]}, 
 
-            { tag: "li", name: "F", classes:"standard green", components: [
+            { tag: "li", name: "F", classes:"standard green", ontap: "handlePlayNote", components: [
                 {tag: "span", content: "4", classes:"number"}
             ]},
 
-            { tag: "li", name: "G", classes:"standard aquamarine", components: [
+            { tag: "li", name: "G", classes:"standard aquamarine", ontap: "handlePlayNote", components: [
                 {tag: "span", content: "5", classes:"number"}
             ]},
 
-            { tag: "li", name: "A", classes:"standard blue", components: [
+            { tag: "li", name: "A", classes:"standard blue", ontap: "handlePlayNote", components: [
                 {tag: "span", content: "6", classes:"number"}
             ]},
 
-            { tag: "li", name: "B", classes:"standard purple", components: [
+            { tag: "li", name: "B", classes:"standard purple", ontap: "handlePlayNote", components: [
                 {tag: "span", content: "7", classes:"number"}
             ]},
         ] } 
