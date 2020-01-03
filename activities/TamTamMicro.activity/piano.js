0891b412114406c704209ff7d293b9d192cc385f
Marcus Chong <marcus.jn.chong@gmail.com>
2020-01-02 23:47:23 -0500

    fix positioning of black keys

--- activities/TamTamMicro.activity/piano.js
+++ activities/TamTamMicro.activity/piano.js
@@ -1,44 +1,46 @@
 enyo.kind({
 	name: "TamTam.Piano",
     components: [
-        { tag: "ul", components: [
-            { tag: "li", name: "C", classes:"standard red", ontap: "handlePlayNote", components: [
-                {tag: "span", content: "1", classes:"number"}
+        {tag: "div", classes: "container", components: [
+            { tag: "ul", components: [
+                { tag: "li", name: "C", classes:"standard red", ontap: "handlePlayNote", components: [
+                    {tag: "span", content: "1", classes:"number"}
+                ]},
+    
+                { tag: "li", name: "D", classes:"standard orange", ontap: "handlePlayNote", components: [
+                    {tag: "span", content: "2", classes:"number"}
+                ]},
+    
+                { tag: "li", name: "E", classes:"standard yellow", ontap: "handlePlayNote", components: [
+                    {tag: "span", content: "3", classes:"number"}
+                ]}, 
+    
+                { tag: "li", name: "F", classes:"standard green", ontap: "handlePlayNote", components: [
+                    {tag: "span", content: "4", classes:"number"}
+                ]},
+    
+                { tag: "li", name: "G", classes:"standard aquamarine", ontap: "handlePlayNote", components: [
+                    {tag: "span", content: "5", classes:"number"}
+                ]},
+    
+                { tag: "li", name: "A", classes:"standard blue", ontap: "handlePlayNote", components: [
+                    {tag: "span", content: "6", classes:"number"}
+                ]},
+    
+                { tag: "li", name: "B", classes:"standard purple", ontap: "handlePlayNote", components: [
+                    {tag: "span", content: "7", classes:"number"}
+                ]},
             ]},
-
-            { tag: "li", name: "D", classes:"standard orange", ontap: "handlePlayNote", components: [
-                {tag: "span", content: "2", classes:"number"}
-            ]},
-
-            { tag: "li", name: "E", classes:"standard yellow", ontap: "handlePlayNote", components: [
-                {tag: "span", content: "3", classes:"number"}
-            ]}, 
-
-            { tag: "li", name: "F", classes:"standard green", ontap: "handlePlayNote", components: [
-                {tag: "span", content: "4", classes:"number"}
-            ]},
-
-            { tag: "li", name: "G", classes:"standard aquamarine", ontap: "handlePlayNote", components: [
-                {tag: "span", content: "5", classes:"number"}
-            ]},
-
-            { tag: "li", name: "A", classes:"standard blue", ontap: "handlePlayNote", components: [
-                {tag: "span", content: "6", classes:"number"}
-            ]},
-
-            { tag: "li", name: "B", classes:"standard purple", ontap: "handlePlayNote", components: [
-                {tag: "span", content: "7", classes:"number"}
-            ]},
-        ]},
-        {tag: "ul", classes: "blackkeys", components: [
-                { tag: "li", name: "C#", classes:"black", ontap: "handlePlayNote"},
-                { tag: "li", name: "D#", classes:"black", ontap: "handlePlayNote"},
-                { tag: "li", name: "hidden", classes:"black hidden"},
-                { tag: "li", name: "F#", classes:"black", ontap: "handlePlayNote"},
-                { tag: "li", name: "G#", classes:"black", ontap: "handlePlayNote"},
-                { tag: "li", name: "A#", classes:"black", ontap: "handlePlayNote"},
-            ]
-        } 
+            {tag: "ul", classes: "blackkeys", components: [
+                    { tag: "li", name: "C#", classes:"black", ontap: "handlePlayNote"},
+                    { tag: "li", name: "D#", classes:"black", ontap: "handlePlayNote"},
+                    { tag: "li", name: "hidden", classes:"black hidden"},
+                    { tag: "li", name: "F#", classes:"black", ontap: "handlePlayNote"},
+                    { tag: "li", name: "G#", classes:"black", ontap: "handlePlayNote"},
+                    { tag: "li", name: "A#", classes:"black", ontap: "handlePlayNote"},
+                ]
+            } 
+        ]}
     ],
     
     handlePlayNote: function(s, e) {
@@ -51,13 +53,13 @@ enyo.kind({
             'D': 2,
             'D#': 3,
             'E': 4,
-            'F': 6,
-            'F#': 7,
-            'G': 8,
-            'G#': 9,
-            'A': 10,
-            'A#': 11,
-            'B': 12
+            'F': 5,
+            'F#': 6,
+            'G': 7,
+            'G#': 8,
+            'A': 9,
+            'A#': 10,
+            'B': 11
         }
 
         var player = new Tone.Player('./audio/database/'+currentPianoMode+".mp3");
