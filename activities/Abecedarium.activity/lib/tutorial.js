15b90cb948d2f933f7f102371165533c0b0b36b6
sdziuda <szymondziuda95@gmail.com>
2020-01-04 16:41:38 +0100

    Add popups for export buttons
    Fixed the issue with popups having no popups in the tutorial

--- activities/Abecedarium.activity/lib/tutorial.js
+++ activities/Abecedarium.activity/lib/tutorial.js
@@ -99,81 +99,171 @@ define(["webL10n"], function(l10n) {
 				}
 			]);
 		} else if (Abcd.context.object.id == "learn") {
-			tour.addSteps([
-				{
-					element: "",
-					orphan: true,
-					placement: "bottom",
-					title: l10n.get("TutoLearnExplainTitle"),
-					content: l10n.get("TutoLearnExplainContent")
-				},
-				{
-					element: "#learn_home_home",
-					placement: "bottom",
-					title: l10n.get("TutoHomeTitle"),
-					content: l10n.get("TutoHomeContent")
-				},
-				{
-					element: "#learn_back",
-					placement: "bottom",
-					title: l10n.get("TutoBackTitle"),
-					content: l10n.get("TutoBackContent")
-				},
-				{
-					element: "#learn_prev",
-					placement: "bottom",
-					title: l10n.get("TutoPrevPageTitle"),
-					content: l10n.get("TutoPrevPageContent")
-				},
-				{
-					element: "#learn_startSlideshow",
-					placement: "bottom",
-					title: l10n.get("TutoListenTitle"),
-					content: l10n.get("TutoListenContent")
-				},
-				{
-					element: "#learn_stopSlideshow",
-					placement: "bottom",
-					title: l10n.get("TutoPauseTitle"),
-					content: l10n.get("TutoPauseContent")
-				},
-				{
-					element: "#learn_next",
-					placement: "bottom",
-					title: l10n.get("TutoNextPageTitle"),
-					content: l10n.get("TutoNextPageContent")
-				},
-				{
-					element: ".box-4-theme",
-					placement: "top",
-					title: l10n.get("TutoLearnBoxThemeTitle"),
-					content: l10n.get("TutoLearnBoxThemeContent")
-				},
-				{
-					element: ".box-4-collection",
-					placement: "top",
-					title: l10n.get("TutoLearnBoxCollectionTitle"),
-					content: l10n.get("TutoLearnBoxCollectionContent")
-				},
-				{
-					element: ".box-4-entry",
-					placement: "top",
-					title: l10n.get("TutoLearnBoxEntryTitle"),
-					content: l10n.get("TutoLearnBoxEntryContent")
-				},
-				{
-					element: "#learn_caseButton",
-					placement: "bottom",
-					title: l10n.get("TutoFontTitle"),
-					content: l10n.get("TutoFontContent")
-				},
-				{
-					element: "#learn_languageButton",
-					placement: "left",
-					title: l10n.get("TutoLangTitle"),
-					content: l10n.get("TutoLangContent")
-				}
-			]);
+			if(document.getElementById("png-button").style.visibility=="visible"){
+				tour.addSteps([
+					{
+						element: "",
+						orphan: true,
+						placement: "bottom",
+						title: l10n.get("TutoLearnExplainTitle"),
+						content: l10n.get("TutoLearnExplainContent")
+					},
+					{
+						element: "#png-button",
+						placement: "bottom",
+						title: l10n.get("TutoPngTitle"),
+						content: l10n.get("TutoPngContent")
+					},
+					{
+						element: "#sound-button",
+						placement: "bottom",
+						title: l10n.get("TutoSoundTitle"),
+						content: l10n.get("TutoSoundContent")
+					},
+					{
+						element: "#learn_home_home",
+						placement: "bottom",
+						title: l10n.get("TutoHomeTitle"),
+						content: l10n.get("TutoHomeContent")
+					},
+					{
+						element: "#learn_back",
+						placement: "bottom",
+						title: l10n.get("TutoBackTitle"),
+						content: l10n.get("TutoBackContent")
+					},
+					{
+						element: "#learn_prev",
+						placement: "bottom",
+						title: l10n.get("TutoPrevPageTitle"),
+						content: l10n.get("TutoPrevPageContent")
+					},
+					{
+						element: "#learn_startSlideshow",
+						placement: "bottom",
+						title: l10n.get("TutoListenTitle"),
+						content: l10n.get("TutoListenContent")
+					},
+					{
+						element: "#learn_stopSlideshow",
+						placement: "bottom",
+						title: l10n.get("TutoPauseTitle"),
+						content: l10n.get("TutoPauseContent")
+					},
+					{
+						element: "#learn_next",
+						placement: "bottom",
+						title: l10n.get("TutoNextPageTitle"),
+						content: l10n.get("TutoNextPageContent")
+					},
+					{
+						element: ".box-4-theme",
+						placement: "top",
+						title: l10n.get("TutoLearnBoxThemeTitle"),
+						content: l10n.get("TutoLearnBoxThemeContent")
+					},
+					{
+						element: ".box-4-collection",
+						placement: "top",
+						title: l10n.get("TutoLearnBoxCollectionTitle"),
+						content: l10n.get("TutoLearnBoxCollectionContent")
+					},
+					{
+						element: ".box-4-entry",
+						placement: "top",
+						title: l10n.get("TutoLearnBoxEntryTitle"),
+						content: l10n.get("TutoLearnBoxEntryContent")
+					},
+					{
+						element: "#learn_caseButton",
+						placement: "bottom",
+						title: l10n.get("TutoFontTitle"),
+						content: l10n.get("TutoFontContent")
+					},
+					{
+						element: "#learn_languageButton",
+						placement: "left",
+						title: l10n.get("TutoLangTitle"),
+						content: l10n.get("TutoLangContent")
+					}
+				]);
+			}else{
+				tour.addSteps([
+					{
+						element: "",
+						orphan: true,
+						placement: "bottom",
+						title: l10n.get("TutoLearnExplainTitle"),
+						content: l10n.get("TutoLearnExplainContent")
+					},
+					{
+						element: "#learn_home_home",
+						placement: "bottom",
+						title: l10n.get("TutoHomeTitle"),
+						content: l10n.get("TutoHomeContent")
+					},
+					{
+						element: "#learn_back",
+						placement: "bottom",
+						title: l10n.get("TutoBackTitle"),
+						content: l10n.get("TutoBackContent")
+					},
+					{
+						element: "#learn_prev",
+						placement: "bottom",
+						title: l10n.get("TutoPrevPageTitle"),
+						content: l10n.get("TutoPrevPageContent")
+					},
+					{
+						element: "#learn_startSlideshow",
+						placement: "bottom",
+						title: l10n.get("TutoListenTitle"),
+						content: l10n.get("TutoListenContent")
+					},
+					{
+						element: "#learn_stopSlideshow",
+						placement: "bottom",
+						title: l10n.get("TutoPauseTitle"),
+						content: l10n.get("TutoPauseContent")
+					},
+					{
+						element: "#learn_next",
+						placement: "bottom",
+						title: l10n.get("TutoNextPageTitle"),
+						content: l10n.get("TutoNextPageContent")
+					},
+					{
+						element: ".box-4-theme",
+						placement: "top",
+						title: l10n.get("TutoLearnBoxThemeTitle"),
+						content: l10n.get("TutoLearnBoxThemeContent")
+					},
+					{
+						element: ".box-4-collection",
+						placement: "top",
+						title: l10n.get("TutoLearnBoxCollectionTitle"),
+						content: l10n.get("TutoLearnBoxCollectionContent")
+					},
+					{
+						element: ".box-4-entry",
+						placement: "top",
+						title: l10n.get("TutoLearnBoxEntryTitle"),
+						content: l10n.get("TutoLearnBoxEntryContent")
+					},
+					{
+						element: "#learn_caseButton",
+						placement: "bottom",
+						title: l10n.get("TutoFontTitle"),
+						content: l10n.get("TutoFontContent")
+					},
+					{
+						element: "#learn_languageButton",
+						placement: "left",
+						title: l10n.get("TutoLangTitle"),
+						content: l10n.get("TutoLangContent")
+					}
+				]);
+			}
 		} else if (Abcd.context.object.id == "play") {
 			tour.addSteps([
 				{
