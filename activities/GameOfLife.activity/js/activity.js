b740aea476a011dbf4c7f06bd216bfcc74b3ad50
prakashujjwal1010 <prakashujjwal1010@gmail.com>
2020-01-07 23:18:23 +0530

    changing icon

--- activities/GameOfLife.activity/js/activity.js
+++ activities/GameOfLife.activity/js/activity.js
@@ -40,7 +40,7 @@ function main(Board, State, patterns, color, shadeColor, l10n, dataStore, genSpe
       blankPattern = patterns[3];
 
   var target = document.querySelector('.main canvas');
-  var board = new Board(state.state.boardState, color.fill, '#FBF6F5', '#9C9797', shadeColor(color.stroke, 10), color.stroke, 12, 12, 2, 2, target);
+  var board = new Board(state.state.boardState, color.fill, '#FBF6F5', '#c8cfbc', shadeColor(color.stroke, 10), color.stroke, 12, 12, 2, 2, target);
 
   var genSpeedButton = document.getElementById("speed-button");
   var genSpeedButtonPallete = new genSpeedPalette.ActivityPalette(
