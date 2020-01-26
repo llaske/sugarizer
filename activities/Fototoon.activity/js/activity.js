02761a52ea6679bc5bfb71f5c777be2b044a839d
Anshul Dutt Sharma <45336622+anshuldutt21@users.noreply.github.com>
2020-01-26 18:55:07 +0530

    Update activity.js

--- activities/Fototoon.activity/js/activity.js
+++ activities/Fototoon.activity/js/activity.js
@@ -237,8 +237,7 @@ define(["sugar-web/activity/activity","sugar-web/datastore","sugar-web/env","tex
         window.addEventListener('resize', function(e) {
             var resizeTimeout;            
             clearTimeout(resizeTimeout);
-            resizeTimeout = setTimeout(function(){    
-            console.log("hello");  
+            resizeTimeout = setTimeout(function(){      
             mainCanvas.height = window.innerHeight - sugarCellSize - 5;
             mainCanvas.width = mainCanvas.height * 4 / 3;
             mainCanvas.style.left = ((window.innerWidth - mainCanvas.width) / 2) + "px";
