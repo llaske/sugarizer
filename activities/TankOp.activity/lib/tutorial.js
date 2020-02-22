372b7a10352c1628b5c8606034d46cb4aec6e802
ksraj123 <kumarsaurabhraj.sr@gmail.com>
2020-02-22 19:32:49 +0530

    localization added to tutorials of Tank Operation Activity

--- activities/TankOp.activity/lib/tutorial.js
+++ activities/TankOp.activity/lib/tutorial.js
@@ -1,4 +1,4 @@
-define([], function () {
+define(["webL10n"], function (l10n) {
 	var tutorial = {};
 
 	tutorial.start = function() {
@@ -7,40 +7,41 @@ define([], function () {
 				element: "",
 				orphan: true,
 				placement: "bottom",
-				title: "Tank Operation Activity",
-				content: "Welcome into the Tank Operation activity. Tank Operation is an arcade game that helps kids practice their math facts with different level of difficulty."
+				title: l10n.get("TutoExplainTitle"),
+				content: l10n.get("TutoExplainContent")
 			},
 			{
 				element: "#fullscreen-button",
 				placement: "bottom",
-				title: "FullScreen Button",
-				content: "Press this button to switch to Full Screen mode."
+				title: l10n.get("TutoFullScreenButtonTitle"),
+				content: l10n.get("TutoFullScreenButtonContent")
             },
             {
 				element: "#stop-button",
 				placement: "left",
-				title: "Stop Button",
-				content: "Press this button to switch to Full Screen mode."
+				title: l10n.get("TutoStopButtonTitle"),
+				content: l10n.get("TutoStopButtonContent")
             },
             {
 				element: "#app_image3",
 				placement: "left",
-				title: "Credits",
-				content: "Press this button to know about developers behind this activity."
+				title: l10n.get("TutoCreditsTitle"),
+				content: l10n.get("TutoCreditsContent")
             },
             {
 				element: "#app_control6",
 				placement: "top",
-				title: "Change Mission",
-				content: "Press the left and right buttons to change mission."
+				title: l10n.get("TutoChangeMissionTitle"),
+				content: l10n.get("TutoChangeMissionContent")
             },
 			{
 				element: "#app_image2",
-                title: "Start Activity",
+                title: l10n.get("TutoStartActivityTitle"),
                 placement: "bottom",
-				content: "Press this button to start the activity."
+				content: l10n.get("TutoStartActivityContent")
 			}
-		];
+        ];
+        
         var tour = new Tour({
             template: "\
             <div class='popover tour'>\
@@ -53,7 +54,7 @@ define([], function () {
                             <div class='tutorial-prev-icon2 web-activity-icon'></div>\
                             <div class='tutorial-prev-icon3 web-activity-disable'></div>\
                         </div>\
-                        <div class='icon-tutorial-text'>Prev</div>\
+                        <div class='icon-tutorial-text'>"+l10n.get("TutoPrev")+"</div>\
                     </div>\
                     <span data-role='separator' style='margin: 4px'>|</span>\
                     <div class='tutorial-next-icon icon-button' data-role='next'>\
@@ -61,14 +62,14 @@ define([], function () {
                             <div class='tutorial-next-icon2 web-activity-icon'></div>\
                             <div class='tutorial-next-icon3 web-activity-disable'></div>\
                         </div>\
-                        <div class='icon-tutorial-text'>Next</div>\
+                        <div class='icon-tutorial-text'>"+l10n.get("TutoNext")+"</div>\
                     </div>\
                     <div class='tutorial-end-icon icon-button' data-role='end'>\
                         <div class='tutorial-end-icon1 web-activity'>\
                             <div class='tutorial-end-icon2 web-activity-icon'></div>\
                             <div class='tutorial-end-icon3 web-activity-disable'></div>\
                         </div>\
-                        <div class='icon-tutorial-text'>End</div>\
+                        <div class='icon-tutorial-text'>"+l10n.get("TutoEnd")+"</div>\
                     </div>\
                 </div>\
             </div>",
