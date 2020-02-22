0eba75d6ac8977477f706a8e9a28fad0bb860501
ksraj123 <kumarsaurabhraj.sr@gmail.com>
2020-02-23 02:44:25 +0530

    adopted sugar ui in the tutorial of stopwatch activity

--- activities/Stopwatch.activity/lib/tutorial.js
+++ activities/Stopwatch.activity/lib/tutorial.js
@@ -23,7 +23,41 @@ define([], function () {
 				content: "Press on this button to stop the activity."
 			}
 		];
-		var tour = new Tour({steps: steps});
+        var tour = new Tour({
+            template: "\
+            <div class='popover tour'>\
+                <div class='arrow'></div>\
+                <h3 class='popover-title tutorial-title'></h3>\
+                <div class='popover-content'></div>\
+                <div class='popover-navigation' style='display: flex; flex-wrap:wrap; justify-content: center; align-items: center'>\
+                    <div class='tutorial-prev-icon icon-button' data-role='prev'>\
+                        <div class='tutorial-prev-icon1 web-activity'>\
+                            <div class='tutorial-prev-icon2 web-activity-icon'></div>\
+                            <div class='tutorial-prev-icon3 web-activity-disable'></div>\
+                        </div>\
+                        <div class='icon-tutorial-text'>Prev</div>\
+                    </div>\
+                    <span data-role='separator' style='margin: 4px'>|</span>\
+                    <div class='tutorial-next-icon icon-button' data-role='next'>\
+                        <div class='tutorial-next-icon1 web-activity'>\
+                            <div class='tutorial-next-icon2 web-activity-icon'></div>\
+                            <div class='tutorial-next-icon3 web-activity-disable'></div>\
+                        </div>\
+                        <div class='icon-tutorial-text'>Next</div>\
+                    </div>\
+                    <div class='tutorial-end-icon icon-button' data-role='end'>\
+                        <div class='tutorial-end-icon1 web-activity'>\
+                            <div class='tutorial-end-icon2 web-activity-icon'></div>\
+                            <div class='tutorial-end-icon3 web-activity-disable'></div>\
+                        </div>\
+                        <div class='icon-tutorial-text'>End</div>\
+                    </div>\
+                </div>\
+            </div>",
+            storage: false,
+            backdrop: true,
+            steps: steps
+        });    
 		tour.init();
 		tour.start(true);
 
