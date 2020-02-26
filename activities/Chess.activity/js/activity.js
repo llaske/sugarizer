c908afd51a7bb449f363e4d516bcc818deaff931
Swayam Samyak Debasis <you@example.com>
2020-02-27 02:39:16 +0530

    added layout of chess activity

--- activities/Chess.activity/js/activity.js
+++ activities/Chess.activity/js/activity.js
@@ -3,6 +3,11 @@ define(["sugar-web/activity/activity"], function (activity) {
 	// Manipulate the DOM only when it is ready.
 	requirejs(['domReady!'], function (doc) {
 
+		var config = {
+			position: 'r1bqkbnr/pppp1ppp/2n5/1B2p3/4P3/5N2/PPPP1PPP/RNBQK2R'
+		  }
+		  var board = Chessboard('myBoard', config)
+
 		// Initialize the activity.
 		activity.setup();
 
