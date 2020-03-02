44aaf19571759a4dec0c05dc820dc0c97df19b06
Swayam Samyak Debasis <you@example.com>
2020-03-02 22:34:28 +0530

    updated changes

--- activities/Chess.activity/js/activity.js
+++ activities/Chess.activity/js/activity.js
@@ -1,4 +1,4 @@
-define(["sugar-web/activity/activity"], function (activity) {
+define(["sugar-web/activity/activity","sugar-web/graphics/presencepalette", "sugar-web/env", "sugar-web/graphics/icon", "webL10n"], function (activity,presencepalette,env, icon,webL10n) {
 
 	// Manipulate the DOM only when it is ready.
 	requirejs(['domReady!'], function (doc) {
@@ -138,6 +138,74 @@ define(["sugar-web/activity/activity"], function (activity) {
 		board = Chessboard('myBoard', config)
 
 		updateStatus()
+
+		
+
+	// 	// Link presence palette
+	// var presence = null;
+	// var palette = new presencepalette.PresencePalette(document.getElementById("network-button"), undefined);
+	// palette.addEventListener('shared', function() {
+	// 	palette.popDown();
+	// 	console.log("Want to share");
+	// 	presence = activity.getPresenceObject(function(error, network) {
+	// 		if (error) {
+	// 			console.log("Sharing error");
+	// 			return;
+	// 		}
+	// 		network.createSharedActivity('org.sugarlabs.Chess', function(groupId) {
+	// 			console.log("Activity shared");
+	// 		});
+	// 		network.onDataReceived(onNetworkDataReceived);
+	// 		console.log("hi");
+	// 	});
+	// });
+
+	function shareActivity() {
+		palette.popDown();
+		console.log("Want to share");
+		presence = activity.getPresenceObject(function(error,network){
+			// if no error in getting presence obj means connected to server
+			if (error) {
+				console.log("error in sharing ");
+				return;
+			}
+			userSettings = network.getUserInfo();
+			if(!window.top.sugar.environment.sharedId){
+				network.createSharedActivity('org.sugarlabs.Chess',function(groupId){
+					console.log("Activity Shared");
+					
+					isHost = true;
+				});
+			}
+			if(did_guest_join==1){
+				console.log("guest joined");
+			}
+			var first_time=0;
+				console.log("outif");
+				if (presence){console.log("inif1");
+				if(first_time==0){console.log("inif2");
+				presence.sendMessage(presence.getSharedInfo().id,{
+					// username:currentenv.user.name,
+					user:presence.getUserInfo(),
+					data:"abcd"
+				})
+				first_time=1;
+				}
+			}
+			network.onConnectionClosed(function(event){
+				presence = null;
+				document.getElementById("stop-button").click();
+				
+			});
+			
+			network.onDataReceived(onNetworkDataReceived);
+			// network.onSharedActivityUserChanged(onNetworkUserChanged);
+		});
+	}
+	
+	var palette = new presencepalette.PresencePalette(document.getElementById("network-button"), undefined);
+	palette.addEventListener('shared',shareActivity);
+		
 		  	
 
 	});
