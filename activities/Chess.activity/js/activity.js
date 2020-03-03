b0963f99eb8a6c07bdc24fba65e4ca69bf9b00f6
Swayam Samyak Debasis <you@example.com>
2020-03-04 00:56:56 +0530

    setting up server

--- activities/Chess.activity/js/activity.js
+++ activities/Chess.activity/js/activity.js
@@ -1,213 +1,359 @@
-define(["sugar-web/activity/activity","sugar-web/graphics/presencepalette", "sugar-web/env", "sugar-web/graphics/icon", "webL10n"], function (activity,presencepalette,env, icon,webL10n) {
+define(["sugar-web/activity/activity","sugar-web/env","sugar-web/graphics/presencepalette","sugar-web/graphics/icon"], function (activity,env,presencepalette,icon) {
 
 	// Manipulate the DOM only when it is ready.
 	requirejs(['domReady!'], function (doc) {
-		
-		// //random movement
-		// var board = null
-		// var game = new Chess()
 
-		// function onDragStart (source, piece, position, orientation) {
-		// // do not pick up pieces if the game is over
-		// if (game.game_over()) return false
-
-		// // only pick up pieces for White
-		// if (piece.search(/^b/) !== -1) return false
-		// }
-
-		// function makeRandomMove () {
-		// var possibleMoves = game.moves()
-
-		// // game over
-		// if (possibleMoves.length === 0) return
-
-		// var randomIdx = Math.floor(Math.random() * possibleMoves.length)
-		// game.move(possibleMoves[randomIdx])
-		// board.position(game.fen())
-		// }
-
-		// function onDrop (source, target) {
-		// // see if the move is legal
-		// var move = game.move({
-		// 	from: source,
-		// 	to: target,
-		// 	promotion: 'q' // NOTE: always promote to a queen for example simplicity
-		// })
-
-		// // illegal move
-		// if (move === null) return 'snapback'
-
-		// // make random legal move for black
-		// window.setTimeout(makeRandomMove, 250)
-		// }
-
-		// // update the board position after the piece snap
-		// // for castling, en passant, pawn promotion
-		// function onSnapEnd () {
-		// board.position(game.fen())
-		// }
-
-		// var config = {
-		// draggable: true,
-		// position: 'start',
-		// onDragStart: onDragStart,
-		// onDrop: onDrop,
-		// onSnapEnd: onSnapEnd
-		// }
-		// board = Chessboard('myBoard', config)
+		// Initialize the activity.
+		activity.setup();
 
 
-		
+		var isHost = false;
 		var board = null
+		pos='start'
 		var game = new Chess()
 		var $status = $('#status')
 		var $fen = $('#fen')
 		var $pgn = $('#pgn')
 
-		function onDragStart (source, piece, position, orientation) {
-		// do not pick up pieces if the game is over
-		if (game.game_over()) return false
 
-		// only pick up pieces for the side to move
-		if ((game.turn() === 'w' && piece.search(/^b/) !== -1) ||
-			(game.turn() === 'b' && piece.search(/^w/) !== -1)) {
-			return false
-		}
+		
+  
+  
+		
+		// Welcome user
+		env.getEnvironment(function(err, environment) {
+			currentenv = environment;
+			
+	
+
+		
+		// Shared instances
+		if (environment.sharedId) {
+			console.log("Shared instance");
+			presence = activity.getPresenceObject(function(error, network) {
+				network.onDataReceived(onNetworkDataReceived);
+				network.onSharedActivityUserChanged(onNetworkUserChanged);
+			});
+		
 		}
 
-		function onDrop (source, target) {
+
+
+
+
+		
+		
+		
+
+	
+	
+	
+	
+	
+		
+
+		// Load from datastore
+		if (!environment.objectId) {
+			console.log("New instance");
+		} else {
+			activity.getDatastoreObject().loadAsText(function(error, metadata, data) {
+				console.log(data.configuration);
+				if (error==null && data!=null) {
+					
+					game=new Chess(pos);
+					pos = data.configuration;
+					
+					
+					//console.log(pos);
+					if(game.game_over()===true){
+						pos='start';
+						game=new Chess();
+						
+					}
+					board.orientation(data.alignment);
+					onSnapEnd();
+					
+				}
+				else{
+					alert("Error in Loading! Start new Activity");
+					//console.log("doit");
+				}
+			});
+		}
+
+	});
+	
+		
+
+
+		// Link presence palette
+	
+	var presence = null;
+	var palette = new presencepalette.PresencePalette(document.getElementById("network-button"), undefined);
+	palette.addEventListener('shared', function() {
+		palette.popDown();
+		console.log("Want to share");
+		presence = activity.getPresenceObject(function(error, network) {
+			network.onDataReceived(onNetworkDataReceived);
+			
+			
+			if (error) {
+				console.log("Sharing error");
+				return;
+			}
+			network.createSharedActivity('org.sugarlabs.Chess', function(groupId) {
+				console.log("Activity shared");
+				isHost = true;
+			});
+
+			
+			
+			network.onSharedActivityUserChanged(onNetworkUserChanged);
+			
+		});
+		
+
+	});
+	
+
+	
+	var onNetworkDataReceived = function(msg) {
+		
+		console.log("hi");
+		if (presence.getUserInfo().networkId === msg.user.networkId) {
+			
+			return;
+		}
+		// if(msg.stop){
+		// 	document.getElementById("stop-button").click();
+		// }
+		console.log(msg.content.alignment)
+		game=new Chess(msg.content.configuration);
+		if(msg.content.alignment=="white"){
+			board.orientation("black");
+		}
+		else{
+			board.orientation("white");
+		}
+		//console.log(msg.content);
+		if(game.game_over()===true){
+			game=new Chess();
+		}
+	
+	}; 
+
+
+
+
+	var onNetworkUserChanged = function(msg) {
+		console.log("hello");
+
+		
+		var DataPacket;
+			if(msg.move===1){
+				DataPacket={
+				  	'configuration':game.fen(),
+				  	'alignment':board.orientation(),
+				  	'stop':false
+				};
+			}
+			else{
+				DataPacket={
+				  	'configuration':game.fen(),
+				  	'alignment':board.orientation(),
+				  	'stop':true
+				};
+			}
+
+
+			if (isHost&&(presence!==null)) {
+				presence.sendMessage(presence.getSharedInfo().id, {
+					user: presence.getUserInfo(),
+					content: DataPacket
+				});
+			}
+
+			if(msg.move==-1){
+				presence=null;
+			}
+			//console.log(msg.move);
+	};
+
+
+	
+	function onDragStart (source, piece, position, orientation) {
+		Update();
+		if (game.game_over()){
+			return false;	
+		} 
+		if(presence){
+			
+			if ((game.turn() === 'w' && piece.search(/^b/) !== -1) || (game.turn() === 'b' && piece.search(/^w/) !== -1)) {
+				return false;
+			}
+			if((board.orientation()==='white'&& piece.search(/^b/) !== -1)||(board.orientation()==='black' && piece.search(/^w/) !== -1)){
+				return false;
+			}
+			
+		}
+		
+		  
+	  }
+	  
+	  function onDrop (source, target) {
 		// see if the move is legal
 		var move = game.move({
-			from: source,
-			to: target,
-			promotion: 'q' // NOTE: always promote to a queen for example simplicity
+		  from: source,
+		  to: target,
+		  promotion: 'q' // NOTE: always promote to a queen for example simplicity
 		})
-
+	  
 		// illegal move
 		if (move === null) return 'snapback'
 
+		var DataPacket={
+			'configuration':game.fen(),
+			'alignment':board.orientation(),
+			'stop':false
+	  };
+	  
+	  if (presence) {
+		  presence.sendMessage(presence.getSharedInfo().id, {
+		  user: presence.getUserInfo(),
+		  content: DataPacket
+		  });
+	  }
+	  
 		updateStatus()
-		}
-
-		// update the board position after the piece snap
-		// for castling, en passant, pawn promotion
-		function onSnapEnd () {
+	  }
+	  
+	  // update the board position after the piece snap
+	  // for castling, en passant, pawn promotion
+	  function onSnapEnd () {
 		board.position(game.fen())
-		}
-
-		function updateStatus () {
+	  }
+	  
+	  function updateStatus () {
 		var status = ''
-
+	  
 		var moveColor = 'White'
 		if (game.turn() === 'b') {
-			moveColor = 'Black'
+		  moveColor = 'Black'
 		}
-
+	  
 		// checkmate?
 		if (game.in_checkmate()) {
-			status = 'Game over, ' + moveColor + ' is in checkmate.'
+		  status = 'Game over, ' + moveColor + ' is in checkmate.'
 		}
-
+	  
 		// draw?
 		else if (game.in_draw()) {
-			status = 'Game over, drawn position'
+		  status = 'Game over, drawn position'
 		}
-
+	  
 		// game still on
 		else {
-			status = moveColor + ' to move'
-
-			// check?
-			if (game.in_check()) {
+		  status = moveColor + ' to move'
+	  
+		  // check?
+		  if (game.in_check()) {
 			status += ', ' + moveColor + ' is in check'
-			}
+		  }
 		}
-
+	  
 		$status.html(status)
 		$fen.html(game.fen())
 		$pgn.html(game.pgn())
-		}
-
-		var config = {
+	  }
+	  
+	  var config = {
 		draggable: true,
-		position: 'start',
-		onDragStart: onDragStart,
+		position: pos,
 		onDrop: onDrop,
+		onDragStart: onDragStart,
+		
 		onSnapEnd: onSnapEnd
-		}
-		board = Chessboard('myBoard', config)
+	  }
+	  board = Chessboard('myBoard', config)
+	  
+	  updateStatus()
 
-		updateStatus()
-
-		
-
-	// 	// Link presence palette
-	// var presence = null;
-	// var palette = new presencepalette.PresencePalette(document.getElementById("network-button"), undefined);
-	// palette.addEventListener('shared', function() {
-	// 	palette.popDown();
-	// 	console.log("Want to share");
-	// 	presence = activity.getPresenceObject(function(error, network) {
-	// 		if (error) {
-	// 			console.log("Sharing error");
-	// 			return;
-	// 		}
-	// 		network.createSharedActivity('org.sugarlabs.Chess', function(groupId) {
-	// 			console.log("Activity shared");
-	// 		});
-	// 		network.onDataReceived(onNetworkDataReceived);
-	// 		console.log("hi");
-	// 	});
-	// });
-
-	function shareActivity() {
-		palette.popDown();
-		console.log("Want to share");
-		presence = activity.getPresenceObject(function(error,network){
-			// if no error in getting presence obj means connected to server
-			if (error) {
-				console.log("error in sharing ");
-				return;
+	  var Update=function(){
+		var jsonData = {
+			'configuration':game.fen(),
+			'alignment':board.orientation()
+		};
+		activity.getDatastoreObject().setDataAsText(jsonData);
+		activity.getDatastoreObject().save(function (error) {
+			if (error === null) {
+				//console.log(jsonData);
 			}
-			userSettings = network.getUserInfo();
-			if(!window.top.sugar.environment.sharedId){
-				network.createSharedActivity('org.sugarlabs.Chess',function(groupId){
-					console.log("Activity Shared");
-					
-					isHost = true;
-				});
+			else {
+				//console.log("write failed.");
 			}
-			if(did_guest_join==1){
-				console.log("guest joined");
-			}
-			var first_time=0;
-				console.log("outif");
-				if (presence){console.log("inif1");
-				if(first_time==0){console.log("inif2");
-				presence.sendMessage(presence.getSharedInfo().id,{
-					// username:currentenv.user.name,
-					user:presence.getUserInfo(),
-					data:"abcd"
-				})
-				first_time=1;
-				}
-			}
-			network.onConnectionClosed(function(event){
-				presence = null;
-				document.getElementById("stop-button").click();
-				
-			});
-			
-			network.onDataReceived(onNetworkDataReceived);
-			// network.onSharedActivityUserChanged(onNetworkUserChanged);
 		});
-	}
-	
-	var palette = new presencepalette.PresencePalette(document.getElementById("network-button"), undefined);
-	palette.addEventListener('shared',shareActivity);
-		
-		  	
+	};
+	  
 
+
+	  
+
+
+
+
+	  	document.getElementById("swap-button").addEventListener('click',function (event){
+			  if(presence)
+			  {
+				  game=new Chess();
+			  }
+		  })
+
+
+
+
+	document.getElementById("stop-button").addEventListener('click', function (event) {
+		//console.log("writing...");
+		var jsonData = {
+			'configuration':game.fen(),
+			'level':difficulty,
+			'alignment':board.orientation()
+		};
+		var DataPacket={
+			  'configuration':game.fen(),
+			  'alignment':board.orientation(),
+			  'stop':true
+		};
+		if (presence) {
+			presence.sendMessage(presence.getSharedInfo().id, {
+			user: presence.getUserInfo(),
+			content: DataPacket
+			});
+		}
+		activity.getDatastoreObject().setDataAsText(jsonData);
+		activity.getDatastoreObject().save(function (error) {
+			if (error === null) {
+				//console.log(jsonData);
+			} else {
+				//console.log("write failed.");
+			}
+		});
 	});
+
+
+		
+
+		
+			
+	
+		
+			
+	
+		
+		
+
+
+		});
+
+
 
 });
