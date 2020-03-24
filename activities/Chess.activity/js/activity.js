8ce6d479f9dce152b516cc4ee6af9a4b89be94dc
sandeepguptan <sandeep.guptan@hackerearth.com>
2020-03-24 16:01:52 +0530

    added files only related to chess activity to this branch

---
+++ activities/Chess.activity/js/activity.js
@@ -0,0 +1,75 @@
+define(["sugar-web/activity/activity", "sugar-web/env", "sugar-web/graphics/icon", "webL10n","sugar-web/graphics/presencepalette","sugar-web/datastore","sugar-web/graphics/journalchooser","pawnpalette","tutorial","chess"], function (activity, env, icon, webL10n, presencepalette,datastore, journalchooser,pawnpalette,tutorial,Chess) {
+	// Manipulate the DOM only when it is ready.
+	requirejs(['domReady!'], function (doc) {
+
+		// Initialize the activity.
+		activity.setup();
+	
+
+
+	env.getEnvironment(function(err, environment) {
+
+
+		var board = null
+var game = new Chess()
+
+function onDragStart (source, piece, position, orientation) {
+  // do not pick up pieces if the game is over
+  if (game.game_over()) return false
+
+  // only pick up pieces for White
+  if (piece.search(/^b/) !== -1) return false
+}
+
+function makeRandomMove () {
+  var possibleMoves = game.moves()
+
+  // game over
+  if (possibleMoves.length === 0) return
+
+  var randomIdx = Math.floor(Math.random() * possibleMoves.length)
+  game.move(possibleMoves[randomIdx])
+  board.position(game.fen())
+}
+
+function onDrop (source, target) {
+  // see if the move is legal
+  var move = game.move({
+    from: source,
+    to: target,
+    promotion: 'q' // NOTE: always promote to a queen for example simplicity
+  })
+
+  // illegal move
+  if (move === null) return 'snapback'
+
+  // make random legal move for black
+  window.setTimeout(makeRandomMove, 250)
+}
+
+// update the board position after the piece snap
+// for castling, en passant, pawn promotion
+function onSnapEnd () {
+  board.position(game.fen())
+}
+
+var config = {
+  draggable: true,
+  position: 'start',
+  onDragStart: onDragStart,
+  onDrop: onDrop,
+  onSnapEnd: onSnapEnd
+}
+board = Chessboard('myBoard', config)
+
+
+
+
+	});
+	
+
+	
+
+});
+	
+});
