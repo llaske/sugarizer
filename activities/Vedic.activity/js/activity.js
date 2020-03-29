39cbe9ec9b4a06bd0e5e878bc081d823ee5e1b8e
=abhishektanwar <abhishektanwar084@gmail.com>
2020-03-29 13:43:25 +0530

    curiosity functionality

--- activities/Vedic.activity/js/activity.js
+++ activities/Vedic.activity/js/activity.js
@@ -144,6 +144,99 @@ define(["sugar-web/activity/activity"], function (activity) {
 				'Answer = 2021'
 			]
 		}
+		var all_explanation_steps = {
+			0: [
+				'So let\'s look at the math behind finding \nthe square of a number ending with 5',
+				'We will just recollect our algebraic skills. \n (ax + b)² = a² . x² + 2 * a * b * x + b²',
+				'Now, take 25 for example. In the above form.',
+				'25 is twice of 10 + 5. (10*2 + 5)',
+				'Likewise for any number ending with 5, \nit will be (a * 10 + 5)',
+				'Back to algebra!, \n(10a + 5)² = a² * 10²+ 2 * 10 * a * 5 + 5²',
+				'= a² . 10² + a * 10²+ 5²',
+				'= a * (a + 1) * 10² + 25',
+				'To add a 2 digits number with 100, replace 0s',
+				'So, a * (a + 1) | 25 is the answer!',
+				'For 25, 2 * (2 + 1) | 25.'
+			],
+			1: [
+				'So let’s look at the math behind finding\nthe square of a number just below 100?',
+				'Say for example, we shall take 96.',
+				'Is that OK to write 96 as (100 - 4)?',
+				'To find 96², we will find (100 - 4)²',
+				'Cool, Now recollect your algebraic skills!\n (a-b)² = a² - 2 * a * b + b²',
+				'Now,In the decimal system, \n100 * 100 is 100 shifted to the left two places',
+				'That is 100² + 4²  = 10016 \njust from the place values',
+				'The trick here is to subtract 8 * 100. \nThat accounts a * (b * 2)',
+				'However, by place value this is subtracting \n8 from 100.  (100 - 8) = 92',
+				'So we get (96 - 4) or (100 - 8) | 4²',
+				'Simply is (Number - difference) | difference²'
+			],
+			2: [
+				'Than a proof, we will see a quick intuitive \nway to multiply a number xy with 9.',
+				'Say for example, we will take 68. \nHere x is 6 and y is 8. To find 68 * 9,',
+				'We can find ( 68 * 10 ) - 68',
+				'Since the 6 in 68 represents 60 it \ndoesn\'t change the 1s place of the product.',
+				'It is determined by 9 * y. That is 9 * 8.',
+				'As y increases by 1, the 1s place decreases \nby 1 and so we need to subtract.',
+				'And hence, shortly we see it as,',
+				'(68 - 7) | (10 - 8).'
+				
+			],
+			3: [
+				'So let’s look at the math behind finding \nthe square of a number just above 100?',
+				'Say for example, we shall take 104.',
+				'Is that OK to write 104 as (100 + 4)?',
+				'To find 104², we will find (100 + 4)²',
+				'Cool, Now recollect your algebraic skills! \n(a+b)² = a² + 2 * a * b + b²',
+				'Now, In the decimal system, \n100 * 100 is 100 shifted to the left two places',
+				'That is 100² + 4²  = 10016 \njust from the place values',
+				'The trick here is to add 8 * 100. \nThat accounts a * (b * 2)',
+				'However, by place value this is just \nadding 8 to 100.  (100 + 8) = 108',
+				'So we get (104 + 4) or (100 + 8) | 4²',
+				'Simply is (Number + surplus) | surplus²'
+			],
+			4: [
+				'Does it look intimidating?! It’s so simple.' ,
+				'Recollect your algebraic skills! \n(a+b)² = a² + 2 * a * b + b²',
+				'Now take an example, (504)².',
+				'That is, (5 * 100 + 4 * 1)² \n= 5 * 5 * 100 * 100 + 40 * 100 + 16',
+				'And to our rescue, we just use \nthe position value of the numbers',
+				'Simply, num * 100 is moving to the left, \ntwo digits.',
+				'Likewise, num * 100 * 100 is moving \nto the left, four digits.',
+				'And so we get 254016.'
+			
+			],
+			5: [
+				'This is a good example of Halving and doubling, \nan ancient means of multiplication.', 
+				'For example, 64 * 5 = 32 * 10. \nWhere 32 is half of 64.',
+				'But as odd numbers have their \ndecimals while halved, be careful.',
+				'We just need to include them.',
+				'In terms of the trick, \n63 * 5 = (31 * 10) + 5 = 315',
+			],
+			6: [
+				'Say for example, you take 43 * 11',
+				'We write 11 as 10 + 1 and mutliply 43 with it.',
+				'43 * (10 + 1) = 43 * 10 + 43',
+				'Where, 430 is (4 * 100 + 3 * 10)\nAnd 43 is (4 * 10 + 3)',
+				'= 4 * 100 + 3 * 10 + 4 * 10 + 3',
+				'Surprise!,  4+3 goes in the 10s place\nwhile we group the 10s',
+				'And simply add numbers by place value.',
+				'It is x | x+y | y which is 4 | (7) | 1.'
+			],
+			7: [
+				'We shall take an example, 43 * 47.',
+				'We can write it as (40 + 3) * 47 and on expanding,',
+				'43 * 47 = 40 * 47 + 3 * 47',
+				'= 40 * (40+7) + 3 * (40+7)',
+				'= 40² + 40 * 7 + 40 * 3 + 3 * 7',
+				'= 16 * 100 + 40 * (7+3) + 21',
+				'= 16 * 100 + 4 * 100 + 40 * 10 +21',
+				'= 20 * 100 + 2 * 10 + 1',
+				'So the requirement that the numbers in \nthe 10s place be the same is to create a square.',
+				'And the 7 + 3 = 10 to move the result \nleft one place.'
+			
+			]
+		}
 		document.getElementById("show-all-tricks").addEventListener('click',function(){
 			console.log("clicked show all");
 			document.getElementById("previous").style.display="block";
@@ -247,6 +340,8 @@ define(["sugar-web/activity/activity"], function (activity) {
 			document.getElementById("next-step").style.visibility="visible";
 			document.getElementById("start-over").style.display="block";
 			document.getElementById("start-over").style.visibility="visible";
+			document.getElementById("curiosity-clicks").style.display="block";
+			document.getElementById("curiosity-clicks").style.visibility="visible";
 			document.getElementById("welcome-board").style.backgroundImage = "url(../Vedic.activity/img/board.jpg)"; 
 			document.getElementById("welcome-board").style.width="900px";
 			document.getElementById("welcome-board").style.height="640px"; 
@@ -285,7 +380,58 @@ define(["sugar-web/activity/activity"], function (activity) {
 					document.getElementById("next-step").style.visibility="hidden";
 					document.getElementById("done").style.display="block";
 					document.getElementById("done").style.visibility="visible";
+					document.getElementById("curiosity-clicks").style.display="display";
+			document.getElementById("curiosity-clicks").style.visibility="visible";
 				}
+				
+			});
+		});
+
+		
+		var curiosity_step_counter=0;
+		document.getElementById("curiosity-clicks").addEventListener('click',function(){
+			document.getElementById("start-over").style.display="none";
+			document.getElementById("start-over").style.visibility="hidden";
+			document.getElementById("curiosity-clicks").style.display="none";
+			document.getElementById("curiosity-clicks").style.visibility="hidden";
+			document.getElementById("next-step").style.display="block";
+			document.getElementById("next-step").style.visibility="visible";
+			document.getElementById("welcome-board").style.backgroundImage = "url(../Vedic.activity/img/board.jpg)"; 
+			document.getElementById("welcome-board").style.width="900px";
+			document.getElementById("welcome-board").style.height="640px";
+			chosen_curiosity_steps = all_explanation_steps[trick_show_counter];
+			curiosity_steps = [];
+			curiosity_steps = chosen_curiosity_steps;
+			console.log(curiosity_steps);
+			var trick_green_board = document.getElementById("welcome-board");
+			// var bottom_permanent_message = "<p>To learn a step again, We recommend to start over as it helps you learn it meaningfully!</p>";
+			trick_green_board.innerHTML="<div><ul id='curiosity_step_list'></ul></div>\
+			<p id='curiosity-step'>"+ curiosity_steps[curiosity_step_counter]+"</p>\
+			";
+			curiosity_step_counter++;
+			console.log("main",curiosity_step_counter);
+			console.log("len",curiosity_steps.length)
+			console.log("trick show counter",trick_show_counter);
+			document.getElementById("next-step").addEventListener('click',function(){
+				if(curiosity_step_counter < curiosity_steps.length){
+				document.getElementById("curiosity-step").innerHTML=curiosity_steps[curiosity_step_counter];
+				// document.getElementById("exam-step").innerHTML=example_steps[curiosity_step_counter];
+				var node= document.createElement("LI");
+				var text = curiosity_steps[curiosity_step_counter-1];
+				var textnode = document.createTextNode("Step"+curiosity_step_counter+" :"+ text );
+				node.appendChild(textnode);
+
+				document.getElementById("curiosity_step_list").appendChild(node);
+				curiosity_step_counter++;
+				}
+				else{
+					console.log("tutorial ends");
+					document.getElementById("next-step").style.display="none";
+					document.getElementById("next-step").style.visibility="hidden";
+					document.getElementById("done").style.display="block";
+					document.getElementById("done").style.visibility="visible";
+				}
+				console.log("in",curiosity_step_counter);
 				
 			});
 		});
