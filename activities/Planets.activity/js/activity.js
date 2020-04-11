05c6fe0999120f417d02e593ba5675a93c8b789c
Andrea Gonzales <andrea010403@gmail.com>
2020-04-11 16:01:28 +0800

    Add planet position

--- activities/Planets.activity/js/activity.js
+++ activities/Planets.activity/js/activity.js
@@ -11,15 +11,26 @@ define(["sugar-web/activity/activity", 'sugar-web/datastore'], function (activit
 		var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
 		var renderer = new THREE.WebGLRenderer({preserveDrawingBuffer: true});
 
+		//Allow interaction with planet
+		var controls = new THREE.TrackballControls(camera, renderer.domElement);
+		controls.target.set(0,0,0);
+
 		//Planet Information
 		var infoType = ["Name", "Type", "Year", "Mass", "Temperature", "Moons"];
 		var planet = planets;
 
+		//Containers
 		var homeDisplay = document.getElementById("planets-list");
 		var interactContainer = document.getElementById("planet-container");
 		var planetDisplay = document.getElementById("planet-display");
 		var planetInfo = document.getElementById("planet-info");
+		var planetPos = document.getElementById("planet-pos");
 		var mainCanvas = document.getElementById("canvas");
+
+		//Used for planet position view
+		var distance = -96;
+		var textDistance = 3.5;
+		var frustumSize = 5;
 
 		//Back Button to go back to homepage
 		var backButton = document.createElement("div");
@@ -27,15 +38,15 @@ define(["sugar-web/activity/activity", 'sugar-web/datastore'], function (activit
 		backButton.title = "Back to Planet List"
 		planetInfo.appendChild(backButton);
 
-		//Allow interaction with planet
-		var controls = new THREE.TrackballControls(camera, renderer.domElement);
-		controls.target.set(0,0,0);
-
-
+		//Do not show unnecessary buttons
 		interactContainer.style.display = "none";
 		document.getElementById("rotation-button").style.display = "none";
 		document.getElementById("info-button").style.display = "none";
 		document.getElementById("image-button").style.display = "none";
+		document.getElementById("list-button").style.display = "none";
+		document.getElementById("radius-button").style.display = "none";
+
+		planetPos.style.display="none";
 
 		for (var i = 0; i < planet.length; i ++){
 			var planetList = document.createElement('div');
@@ -55,6 +66,31 @@ define(["sugar-web/activity/activity", 'sugar-web/datastore'], function (activit
 			document.getElementById("planet-" + planet[i].name).appendChild(planetName);
 
 			initPlanet(planet[i].name, planet[i].type, planet[i].year, planet[i].mass, planet[i].temperature, planet[i].moons);
+			initPosition(planet[i].name, planet[i].type, planet[i].radius);
+
+			// Switch to fullscreen mode on click
+			document.getElementById("fullscreen-button").addEventListener('click', function() {
+				document.getElementById("main-toolbar").style.opacity = 0;
+				document.getElementById("canvas").style.top = "0px";
+				document.getElementById("unfullscreen-button").style.visibility = "visible";
+			});
+
+			// Switch to unfullscreen mode
+			document.getElementById("unfullscreen-button").addEventListener('click', function() {
+				document.getElementById("main-toolbar").style.opacity = 1;
+				document.getElementById("canvas").style.top = "55px";
+				document.getElementById("unfullscreen-button").style.visibility = "hidden";
+			});
+
+			document.getElementById("list-button").addEventListener("click", function(){
+				homeDisplay.style.display="block";
+				planetPos.style.display="none";
+				distance = -80;
+				requestAnim = false;
+				textDistance = 3.5;
+				document.getElementById("position-button").style.display="inline";
+				document.getElementById("list-button").style.display = "none";
+			});
 		}
 
 		//Show planet function
@@ -68,7 +104,6 @@ define(["sugar-web/activity/activity", 'sugar-web/datastore'], function (activit
 
 			//Url of planet map files
 			var url = "images/" + name.toLowerCase() + "map.jpg";
-
 
 			//Create Planet
 			var loadTexture = new THREE.TextureLoader().load(url);
@@ -127,15 +162,12 @@ define(["sugar-web/activity/activity", 'sugar-web/datastore'], function (activit
 			}
 
 
-
 			//For planets with terrain, add bumps
 			if (type === "Terrestrial"){
 				var bumpUrl = "images/" + name.toLowerCase() + "bump.png";
 				material.bumpMap = new THREE.TextureLoader().load(bumpUrl);
 				material.bumpScale = 0.1;
 			}
-
-
 
 
 			//Active buttons
@@ -153,9 +185,16 @@ define(["sugar-web/activity/activity", 'sugar-web/datastore'], function (activit
 				homeDisplay.style.display = "none";
 				document.getElementById("planet-info").style.display = "block";
 				planetDisplay.style.width = "60%";
+				document.getElementById("position-button").style.display = "none";
+				document.getElementById("list-button").style.display = "none";
 				document.getElementById("rotation-button").style.display = "inline";
 				document.getElementById("info-button").style.display = "inline";
 				document.getElementById("image-button").style.display = "inline";
+
+				//Remove previous scene
+				while(scene.children.length > 0){
+					 scene.remove(scene.children[0]);
+				};
 
 				renderer.setSize( planetDisplay.clientWidth, window.innerHeight);
 				planetDisplay.appendChild(renderer.domElement);
@@ -308,32 +347,252 @@ define(["sugar-web/activity/activity", 'sugar-web/datastore'], function (activit
 				document.getElementById("rotation-button").style.display = "none";
 				document.getElementById("info-button").style.display = "none";
 				document.getElementById("image-button").style.display = "none";
+				document.getElementById("position-button").style.display = "inline";
+
 
 				//Remove previous scene
 				while(scene.children.length > 0){
 					 scene.remove(scene.children[0]);
 				};
 
+
 			});
-
-			// Switch to fullscreen mode on click
-			document.getElementById("fullscreen-button").addEventListener('click', function() {
-				document.getElementById("main-toolbar").style.opacity = 0;
-				document.getElementById("canvas").style.top = "0px";
-				document.getElementById("unfullscreen-button").style.visibility = "visible";
-			});
-
-			// Switch to unfullscreen mode
-			document.getElementById("unfullscreen-button").addEventListener('click', function() {
-				document.getElementById("main-toolbar").style.opacity = 1;
-				document.getElementById("canvas").style.top = "55px";
-				document.getElementById("unfullscreen-button").style.visibility = "hidden";
-			});
-
-
-
 		}
 
+		function initPosition(name, type, radius){
+
+			var planetSize;
+			var requestAnim;
+			var showRadius;
+
+			//Active Buttons
+			document.getElementById("radius-button").classList.add("active");
+
+			//Url of planet map files
+			var url = "images/" + name.toLowerCase() + "map.jpg";
+			if (name === "Mercury"){
+				planetSize = 0.5;
+			} else if (name === "Venus" || name === "Earth"){
+				planetSize = 2;
+			}
+			else if (name === "Mars"){
+				planetSize = 1;
+			}
+			else if (name === "Jupiter" || name === "Saturn"){
+				planetSize = 10;
+			}
+			else if (name === "Uranus" || name == "Neptune"){
+				planetSize = 5;
+			}
+			//Create Planet
+			var loadTexture = new THREE.TextureLoader().load(url);
+			var geometry = new THREE.SphereGeometry(planetSize, 32, 32);
+			var material = new THREE.MeshBasicMaterial({
+				map: loadTexture,
+			});
+			var light = new THREE.DirectionalLight(0xffffff);
+			var lightHolder = new THREE.Group();
+			var planetMesh = new THREE.Mesh(geometry, material);
+
+			//Create clouds for Earth
+			if(name === "Earth"){
+				var specUrl = "images/" + name.toLowerCase() + "spec.png";
+				var cloudUrl = "images/" + name.toLowerCase() + "cloudmap.jpg";
+				var loadCloudTexture = new THREE.TextureLoader().load(cloudUrl);
+				var cloudGeometry = new THREE.SphereGeometry(planetSize, 32, 32);
+				var cloudMaterial = new THREE.MeshPhongMaterial({
+					map: loadCloudTexture,
+					side: THREE.DoubleSide,
+					opacity: 0.2,
+					transparent: true,
+					depthWrite: false
+				});
+				var cloudMesh = new THREE.Mesh(cloudGeometry, cloudMaterial);
+				material.specularMap = new THREE.TextureLoader().load(specUrl);
+				material.specular  = new THREE.Color('grey');
+				planetMesh.add(cloudMesh);
+			};
+
+			//Create Rings
+			if(name === "Saturn" || name === "Uranus"){
+				var ringUrl = "images/" + name.toLowerCase() + "ringcolor.jpg";
+				var loadRingTexture = new THREE.TextureLoader().load(ringUrl);
+				if (name === "Saturn"){
+					var ringGeometry = new THREE.RingBufferGeometry(13, 22, 40);
+					var position = ringGeometry.attributes.position;
+					var vector = new THREE.Vector3();
+					for (let i = 0; i < position.count; i++){
+						vector.fromBufferAttribute(position, i);
+						ringGeometry.attributes.uv.setXY(i, vector.length() < 14 ? 0 : 1, 1);
+					}
+				}
+				else{
+					var ringGeometry = new THREE.RingBufferGeometry(9, 10, 40);
+				}
+				var ringMaterial = new THREE.MeshPhongMaterial({
+					map: loadRingTexture,
+					side: THREE.DoubleSide,
+					opacity: 0.4,
+					transparent: true,
+					depthWrite: true
+				});
+				var ringMesh = new THREE.Mesh(ringGeometry, ringMaterial);
+			}
+
+
+			//For planets with terrain, add bumps
+			if (type === "Terrestrial"){
+				var bumpUrl = "images/" + name.toLowerCase() + "bump.png";
+				material.bumpMap = new THREE.TextureLoader().load(bumpUrl);
+				material.bumpScale = 0.1;
+			}
+
+			//Add names to planets
+			var planetNewName = document.createElement("div");
+			planetNewName.id = "new-name-" + name;
+			planetNewName.className = "planet-new-name";
+			planetNewName.innerHTML = name;
+			planetPos.appendChild(planetNewName);
+			document.getElementById("new-name-" + name).style.marginLeft = textDistance + "%";
+
+			//Add radius to planets
+			var planetRadius = document.createElement("div");
+			planetRadius.id = "radius-" + name;
+			planetRadius.className = "planet-radius";
+			planetRadius.innerHTML = radius + " km";
+			planetPos.appendChild(planetRadius);
+			document.getElementById("radius-" + name).style.marginLeft = textDistance - 0.5 + "%";
+
+			if (name === "Mercury" || name === "Venus" || name === "Earth" || name === "Mars"){
+				distance +=15;
+				textDistance += 8.5;
+			}
+			else if (name === "Uranus"){
+				distance += 35;
+				textDistance += 13.3;
+			}
+			else if (name === "Neptune"){
+				distance += 25;
+				textDistance += 13.3;
+			}
+			else if (name === "Jupiter"){
+				distance += 17;
+				textDistance += 20;
+			}
+			else{
+				distance += 35;
+				textDistance += 20;
+			}
+
+			planetMesh.position.x = distance;
+
+
+			//Show planet position and radius
+			document.getElementById("position-button").addEventListener("click", function(){
+				var aspect = planetPos.clientHeight/planetPos.clientWidth;
+				var camera = new THREE.OrthographicCamera( frustumSize / - 20, frustumSize / 20, frustumSize * aspect / 20, frustumSize * aspect / - 20, -500, 2000 );
+
+				//Show necessary buttons and hide unused buttons
+				document.getElementById("position-button").style.display="none";
+				homeDisplay.style.display="none";
+				planetPos.style.display="block";
+				document.getElementById("list-button").style.display = "inline";
+				document.getElementById("radius-button").style.display = "inline";
+
+				requestAnim = true;
+
+				scene.add(planetMesh);
+
+				renderer.setSize( planetPos.clientWidth, planetPos.clientHeight);
+				planetPos.appendChild(renderer.domElement);
+				light.position.set( 1, 1, 5 );
+
+				lightHolder.add(light);
+				scene.add(lightHolder);
+
+
+				scene.add(planetMesh);
+				scene.add(camera);
+
+				if (name === "Saturn" || name === "Uranus"){
+					if (name === "Saturn"){
+						ringMesh.rotation.x = 20;
+						ringMesh.position.x = 16;
+					}
+					else{
+						ringMesh.rotation.x = 0;
+						ringMesh.position.x = 51;
+					}
+					scene.add(ringMesh);
+				}
+
+				camera.zoom = 0.0028;
+				camera.updateProjectionMatrix();
+
+				animatePlanet = function() {
+					if (resizePlanet(renderer)){
+						var aspect = planetPos.clientHeight / planetPos.clientWidth;
+					  camera.left = frustumSize / - 20;
+					  camera.right = frustumSize / 20;
+					  camera.top = frustumSize * aspect / 20;
+					  camera.bottom = - frustumSize * aspect / 20;
+
+					  camera.updateProjectionMatrix();
+
+					}
+
+					camera.updateProjectionMatrix();
+					if (requestAnim === true){
+						requestAnimationFrame(animatePlanet)
+					}
+					else{
+						cancelAnimationFrame(animatePlanet);
+					}
+					renderer.render(scene,camera);
+				}
+
+				resizePlanet = function(renderer) {
+
+					var width = planetPos.clientWidth;
+					var height = planetPos.clientHeight;
+					var needResize = planetPos.width !== width || planetPos.height !== height;
+
+					if (needResize) {
+						renderer.setSize( planetPos.clientWidth, planetPos.clientHeight );
+					}
+					return needResize;
+				}
+
+				animatePlanet();
+			});
+
+			//Need to set to false so to cancel animation
+			document.getElementById("list-button").addEventListener("click", function(){
+
+				requestAnim = false;
+				document.getElementById("radius-button").style.display = "none";
+			});
+
+			//Toggle Planet Radius
+			document.getElementById("radius-button").addEventListener("click", function(){
+
+				if (showRadius === false){
+					showRadius = true;
+					for (var i = 0; i < 8; i++){
+						document.getElementsByClassName("planet-radius")[i].style.display = "block";
+					}
+					document.getElementById("radius-button").classList.add("active");
+				} else{
+					showRadius = false;
+					for (var i = 0; i < 8; i++){
+						document.getElementsByClassName("planet-radius")[i].style.display = "none";
+					}
+					document.getElementById("radius-button").classList.remove("active");
+				}
+
+			});
+
+		}
 
 	});
 
