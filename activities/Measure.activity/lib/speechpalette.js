8d4820f0247ff6ae554bf6789d194eecd4a9dbb4
=abhishektanwar <abhishektanwar084@gmail.com>
2020-04-24 09:18:08 +0530

    chrome user gesture graph play fix

--- activities/Measure.activity/lib/speechpalette.js
+++ activities/Measure.activity/lib/speechpalette.js
@@ -1,49 +1,39 @@
 define(["sugar-web/graphics/palette",
-        "text!speechpalette.html"], function (palette, template) {
+		"text!speechpalette.html"], function (palette, template) {
 
-    'use strict';
+	'use strict';
 
-    var activitypalette = {};
+	var activitypalette = {};
 
-    activitypalette.ActivityPalette = function (activityButton) {
+	activitypalette.ActivityPalette = function (activityButton) {
 
-        palette.Palette.call(this, activityButton);
+		palette.Palette.call(this, activityButton);
 
-        var activityTitle;
-        var descriptionLabel;
-        var descriptionBox;
+		var activityTitle;
+		var descriptionLabel;
+		var descriptionBox;
 
-        this.getPalette().id = "activity-palette";
+		this.getPalette().id = "activity-palette";
 
-        var containerElem = document.createElement('div');
-        containerElem.innerHTML = template;
-        this.setContent([containerElem]);
+		var containerElem = document.createElement('div');
+		containerElem.innerHTML = template;
+		this.setContent([containerElem]);
 
-        this.pitchScale = containerElem.querySelector('#timepitchvalue');
-        this.rateScale = containerElem.querySelector('#timeratevalue');
-
-        this.pitchScale.onclick = function() {
-            // console.log("he;;p");
-            console.log(this.value,"pitch");
-            // document.getElementById('pitch').innerHTML = this.value;
-        }
-        this.rateScale.onclick = function() {
-            console.log(this.value,"rate");
-            // document.getElementById('rate').innerHTML = this.value*(300/100) + 10;
-            // console.log("hell");
-        }
+		this.pitchScale = containerElem.querySelector('#timepitchvalue');
+		this.rateScale = containerElem.querySelector('#timeratevalue');
 
 
-    };
 
-    activitypalette.ActivityPalette.prototype =
-        Object.create(palette.Palette.prototype, {
-            setTitleDescription: {
-                value: "Speech Palette:",
-                enumerable: true,
-                configurable: true,
-                writable: true
-            }
-        });
-    return activitypalette;
+	};
+
+	activitypalette.ActivityPalette.prototype =
+		Object.create(palette.Palette.prototype, {
+			setTitleDescription: {
+				value: "Speech Palette:",
+				enumerable: true,
+				configurable: true,
+				writable: true
+			}
+		});
+	return activitypalette;
 });
