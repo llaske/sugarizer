f53f7a0027bd0c7011177f37fc36c9946123b27d
Dhruv <dhruvmisra@live.com>
2020-07-17 12:25:15 +0530

    Removed template title edit button; Using metadata title

--- activities/Curriculum.activity/js/TemplateSelection.js
+++ activities/Curriculum.activity/js/TemplateSelection.js
@@ -8,7 +8,6 @@ var TemplateSelection = {
 				</div>
 				<div class="template-content">
 					<span>{{ template.templateTitle }}</span>
-					<button id="edit-button" @click.stop="editIndex = i"></button>
 				</div>
 			</div>
 			<button class="add-template" id="add-button" @click="showAddPopup = true"></button>
@@ -24,19 +23,6 @@ var TemplateSelection = {
 				<div class="popup-body">
 					<label for="template-url">Template URL</label>
 					<input type="url" id="template-url" v-model="templateURL">
-				</div>
-			</form>
-
-			<form @submit.prevent="editIndex = null" class="template-popup" v-if="editIndex != null">
-				<div class="popup-header">
-					<h2 style="margin: 0">Edit Template</h2>
-					<div class="popup-buttons">
-						<button type="submit" id="confirm-button"></button>
-					</div>
-				</div>
-				<div class="popup-body">
-					<label for="template-title">Template Title</label>
-					<input type="text" id="template-title" v-model="defaultTemplates[editIndex].templateTitle" required>
 				</div>
 			</form>
 		</div>
@@ -60,12 +46,12 @@ var TemplateSelection = {
 					let template = await this.loadTemplate(templateFile);
 					template = JSON.parse(template);
 					if(template.metadata != null) {
-						let temp = JSON.parse(vm.$root.$refs.SugarJournal.LZString.decompressFromUTF16(template.text));
+						let data = JSON.parse(vm.$root.$refs.SugarJournal.LZString.decompressFromUTF16(template.text));
 						template = {
-							templateTitle: temp.templateTitle,
-							templateImage: temp.templateImage,
-							notationLevel: temp.notationLevel,
-							categories: temp.categories
+							templateTitle: template.metadata.title,
+							templateImage: data.templateImage,
+							notationLevel: data.notationLevel,
+							categories: data.categories
 						}
 					} else {
 						template.templateTitle = vm.$root.$refs.SugarL10n.get(template.templateTitle);
@@ -94,9 +80,12 @@ var TemplateSelection = {
 		addTemplate: function() {
 			var vm = this;
 			requirejs([`text!${vm.templateURL}`], function(template) {
-				var data = JSON.parse(vm.$root.$refs.SugarJournal.LZString.decompressFromUTF16(JSON.parse(template).text))
+				template = JSON.parse(template);
+				var data = JSON.parse(vm.$root.$refs.SugarJournal.LZString.decompressFromUTF16(template.text))
 				vm.$emit('template-selected', {
 					fromURL: true,
+					templateTitle: template.metadata.title,
+					templateImage: data.templateImage,
 					categories: data.categories,
 					notationLevel: data.notationLevel
 				});
