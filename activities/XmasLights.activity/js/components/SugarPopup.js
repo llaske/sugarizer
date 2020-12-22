c0e4b3f1d63c06ab998f866c9ee39dce1d424c47
llaske <llaske@c2s.fr>
2020-12-22 14:45:43 +0100

    Initial Xmas Lights commit

---
+++ activities/XmasLights.activity/js/components/SugarPopup.js
@@ -0,0 +1,18 @@
+Vue.component('sugar-popup', {
+	data: function () {
+		return {
+			humane: null
+		}
+	},
+	mounted() {
+		var vm = this;
+		requirejs(["humane"], function (humane) {
+			vm.humane = humane;
+		});
+	},
+	methods: {
+		log(text) {
+			this.humane.log(text);
+		}
+	}
+});
