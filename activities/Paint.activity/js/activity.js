8b3affc15ad6c7db6d3d8ecbb269388ae8f01ebe
Saurabh Gupta <60233336+saurabhhere@users.noreply.github.com>
2020-12-28 13:23:32 +0530

    Updated Indentation in activity.js

--- activities/Paint.activity/js/activity.js
+++ activities/Paint.activity/js/activity.js
@@ -52,22 +52,22 @@ define(["sugar-web/activity/activity","tutorial","webL10n","sugar-web/env","acti
     });
 
     // Export as PNG image 
-document.getElementById("save-image-button").addEventListener('click', function() {
-	var mimetype = 'image/png';
-	var inputData = document.getElementById("paint-canvas").toDataURL(mimetype, 1);
-	var metadata = {
-		mimetype: mimetype,
-		title: webL10n.get("PaintBy",  { name: currentenv.user.name }),
-		activity: "org.olpcfrance.MediaViewerActivity",
-		timestamp: new Date().getTime(),
-		creation_time: new Date().getTime(),
-		file_size: 0
-	};
-	datastore.create(metadata, function() {
-		humane.log(webL10n.get('PaintImageSaved'))
-		console.log("export done.")
-	}, inputData);
-});
+		document.getElementById("save-image-button").addEventListener('click', function() {
+			var mimetype = 'image/png';
+			var inputData = document.getElementById("paint-canvas").toDataURL(mimetype, 1);
+			var metadata = {
+				mimetype: mimetype,
+				title: webL10n.get("PaintBy",  { name: currentenv.user.name }),
+				activity: "org.olpcfrance.MediaViewerActivity",
+				timestamp: new Date().getTime(),
+				creation_time: new Date().getTime(),
+				file_size: 0
+			};
+			datastore.create(metadata, function() {
+				humane.log(webL10n.get('PaintImageSaved'))
+				console.log("export done.")
+			}, inputData);
+		});
 
     document.getElementById("stop-button").addEventListener('click', function(event) {
       var data = {
