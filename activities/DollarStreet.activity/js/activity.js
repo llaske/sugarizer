a3498bcd562bbfe3606d7a54468c25b3abc49975
Lionel Laské <llaske@c2s.fr>
2021-02-14 21:57:23 +0100

    Add tutorial

--- activities/DollarStreet.activity/js/activity.js
+++ activities/DollarStreet.activity/js/activity.js
@@ -37,7 +37,29 @@ var app = new Vue({
 			stringThingsButton: '',
 			stringClose: '',
 			stringImageExported: '',
-			stringNoDescription: ''
+			stringNoDescription: '',
+			stringTutoListWelcomeTitle: '',
+			stringTutoListWelcomeContent: '',
+			stringTutoListIncomeTitle: '',
+			stringTutoListIncomeContent: '',
+			stringTutoListFamilyTitle: '',
+			stringTutoListFamilyContent: '',
+			stringTutoListThingsTitle: '',
+			stringTutoListThingsContent: '',
+			stringTutoListRegionTitle: '',
+			stringTutoListRegionContent: '',
+			stringTutoListPlaceTitle: '',
+			stringTutoListPlaceContent: '',
+			stringTutoDetailViewTitle: '',
+			stringTutoDetailViewContent: '',
+			stringTutoDetailImageTitle: '',
+			stringTutoDetailImageContent: '',
+			stringTutoDetailDescriptionTitle: '',
+			stringTutoDetailDescriptionContent: '',
+			stringTutoDetailThingsTitle: '',
+			stringTutoDetailThingsContent: '',
+			stringTutoDetailBackTitle: '',
+			stringTutoDetailBackContent: ''
 		}
 	},
 	created() {
@@ -187,6 +209,91 @@ var app = new Vue({
 				document.getElementById("spinner").style.visibility = "hidden";
 				document.getElementById("cloudwarning").style.visibility = "visible";
 			});
+		},
+
+		// Display tutorial
+		onHelp: function() {
+			let vm = this;
+			let steps = [];
+			let classplaces = document.getElementsByClassName("place-padding");
+			let placeelement = classplaces && classplaces[0] ? classplaces[0] : "";
+			if (vm.currentView == viewList) {
+				steps = steps.concat([
+					{
+						element: "",
+						orphan: true,
+						placement: "bottom",
+						title: vm.l10n.stringTutoListWelcomeTitle,
+						content: vm.l10n.stringTutoListWelcomeContent
+					},
+					{
+						element: "#street-button",
+						placement: "bottom",
+						title: vm.l10n.stringTutoListIncomeTitle,
+						content: vm.l10n.stringTutoListIncomeContent
+					},
+					{
+						element: "#family-button",
+						placement: "bottom",
+						title: vm.l10n.stringTutoListFamilyTitle,
+						content: vm.l10n.stringTutoListFamilyContent
+					},
+					{
+						element: "#things-button",
+						placement: "bottom",
+						title: vm.l10n.stringTutoListThingsTitle,
+						content: vm.l10n.stringTutoListThingsContent
+					},
+					{
+						element: "#world-button",
+						placement: "bottom",
+						title: vm.l10n.stringTutoListRegionTitle,
+						content: vm.l10n.stringTutoListRegionContent
+					},
+					{
+						element: placeelement,
+						placement: "right",
+						title: vm.l10n.stringTutoListPlaceTitle,
+						content: vm.l10n.stringTutoListPlaceContent
+					},
+				]);
+			} else {
+				steps = steps.concat([
+					{
+						element: "",
+						orphan: true,
+						placement: "bottom",
+						title: vm.l10n.stringTutoDetailViewTitle,
+						content: vm.l10n.stringTutoDetailViewContent
+					},
+					{
+						element: ".family-image-container",
+						placement: "right",
+						title: vm.l10n.stringTutoDetailImageTitle,
+						content: vm.l10n.stringTutoDetailImageContent
+					},
+					{
+						element: "#family-description",
+						placement: "left",
+						title: vm.l10n.stringTutoDetailDescriptionTitle,
+						content: vm.l10n.stringTutoDetailDescriptionContent
+					},
+					{
+						element: placeelement,
+						placement: "top",
+						title: vm.l10n.stringTutoDetailThingsTitle,
+						content: vm.l10n.stringTutoDetailThingsContent
+					},
+					{
+						element: "#family-goback",
+						placement: "right",
+						title: vm.l10n.stringTutoDetailBackTitle,
+						content: vm.l10n.stringTutoDetailBackContent
+					},
+				]);
+			}
+
+			app.$refs.SugarTutorial.show(steps);
 		},
 
 		// Update screen visibility
