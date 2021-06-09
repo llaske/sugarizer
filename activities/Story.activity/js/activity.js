cd72ec4b6f2d51772c6f37fe3a905966aaf6d235
saurabhhere <saurabhguptajpr@yahoo.in>
2021-06-09 23:19:18 +0530

    Load random images from abecedarium db

--- activities/Story.activity/js/activity.js
+++ activities/Story.activity/js/activity.js
@@ -11,7 +11,8 @@ var app = new Vue({
 	el: '#app',
 	data: {
 		grid: true,
-		images: ["https://i.ibb.co/QkBsPS3/Screenshot-from-2021-04-02-01-25-34.png", "https://i.ibb.co/Rv25tbt/sampleimage2.png","https://i.ibb.co/L91Lcz5/sampleimage3.png", "https://i.ibb.co/M8cPxbh/sampleimage4.png", "https://i.ibb.co/0Z73LGZ/sampleimage5.png", "https://i.ibb.co/gvTcPCH/sampleimage6.png", "https://i.ibb.co/98yqR7b/sampleimage7.png", "https://i.ibb.co/mGK22f3/sampleimage8.png", "https://i.ibb.co/YD1WVmp/sampleimage9.png" ],
+		// images: ["https://i.ibb.co/QkBsPS3/Screenshot-from-2021-04-02-01-25-34.png", "https://i.ibb.co/Rv25tbt/sampleimage2.png","https://i.ibb.co/L91Lcz5/sampleimage3.png", "https://i.ibb.co/M8cPxbh/sampleimage4.png", "https://i.ibb.co/0Z73LGZ/sampleimage5.png", "https://i.ibb.co/gvTcPCH/sampleimage6.png", "https://i.ibb.co/98yqR7b/sampleimage7.png", "https://i.ibb.co/mGK22f3/sampleimage8.png", "https://i.ibb.co/YD1WVmp/sampleimage9.png"],
+		images: [],
 		activeImage: "",
 		activeImageIndex: 0,
 		previousBtnId: null,
@@ -38,6 +39,54 @@ var app = new Vue({
 			this.recordIconId = "record";
 			document.getElementById('grid-mode').classList.add("active");
 			this.imageLoaders();
+			this.loadImages();
+		},
+		loadImages: function(){
+			var xhr = new XMLHttpRequest();
+			var imgs = [];
+			var that = this;
+			var source = document.location.href.substr(0, document.location.href.indexOf("/activities/"))+"/activities/Abecedarium.activity/database/db_en.json";
+		
+			function checkFileExists (urlToFile) {
+				try {
+					var req = new XMLHttpRequest();
+					req.open('HEAD', urlToFile, false);
+					req.send();
+					if (req.readyState === 4){
+						return true;
+					} else {
+						return false;
+					}					
+				} catch (error) {
+					return false;
+				}
+			}
+			xhr.onload = function(){
+				if (xhr.status != 0){
+					console.log("Error occured", xhr.statusText);
+				} else {
+					var data;
+					data  = JSON.parse(xhr.response);
+					for (var i=0; i<that.imageCount; i++){
+						var img = data[Math.floor(Math.random() *  data.length)];
+						var imgCheck = checkFileExists(document.location.href.substr(0, document.location.href.indexOf("/activities/"))+"/activities/Abecedarium.activity/images/database/"+img+".png");
+						if (imgs.indexOf(img)===-1 && imgCheck==true){
+							imgs.push(img);
+						} else {
+							i--;
+						}
+					}
+					that.images = imgs;
+				}
+			};
+			xhr.onerror = function(err){
+				console.log("Error: ", xhr.statusText);
+			};
+			xhr.open("GET", source);
+			xhr.send();
+		},
+		getUrlImg: function(img){
+			return '../Abecedarium.activity/images/database/'+ img + '.png';
 		},
 		imageLoaders: function(){
 			function getRandomInt (min, max) {
@@ -52,7 +101,7 @@ var app = new Vue({
 			var quesimgs = document.getElementsByClassName('questionmark');
 			var len = quesimgs.length;
 			for (var i=0; i<len; i++){
-				quesimgs[i].style.background = that.colors[getRandomInt(0,2)];
+				quesimgs[i].style.background = getColor();
 				(function (i){
 					var intervalId = setInterval(function(){quesimgs[i].style.background = getColor()}, 900);
 					that.intervalIds.push(intervalId);
