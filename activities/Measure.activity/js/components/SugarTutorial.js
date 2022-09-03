2593a0b211b740d30f36c2aa7fd7343e232658e7
Eshaan Aggarwal <96648934+EshaanAgg@users.noreply.github.com>
2022-09-04 04:50:42 +0530

    Move Measure Activity to IntroJS

--- activities/Measure.activity/js/components/SugarTutorial.js
+++ activities/Measure.activity/js/components/SugarTutorial.js
@@ -1,11 +1,10 @@
-// Tutorial component based on bootstrap tour
+// Tutorial component based on IntroJs
 Vue.component('sugar-tutorial', {
 	data: function () {
 		return {
 			l10n: {
 				stringTutoPrev: 'Prev',
 				stringTutoNext: 'Next',
-				stringTutoEnd: 'End',
 			}
 		}
 	},
@@ -20,43 +19,16 @@ Vue.component('sugar-tutorial', {
 	methods: {
 		show: function (steps) {
 			var vm = this;
-			var tour = new Tour({
-				template: `
-				<div class='popover tour'>
-					<div class='arrow'></div>
-					<h3 class='popover-title tutorial-title'></h3>
-					<div class='popover-content'></div>
-					<div class='popover-navigation' style='display: flex; flex-wrap:wrap; justify-content: center; align-items: center'>
-						<div class='tutorial-prev-icon tutorial-icon-button' data-role='prev'>
-							<div class='tutorial-prev-icon1 web-activity'></div>
-							<div class='tutorial-icon-text'>${this.l10n.stringTutoPrev}</div>
-						</div>
-						<span data-role='separator' style='margin: 4px'>|</span>
-						<div class='tutorial-next-icon tutorial-icon-button' data-role='next'>
-							<div class='tutorial-next-icon1 web-activity'></div>
-							<div class='tutorial-icon-text'>${this.l10n.stringTutoNext}</div>
-						</div>
-						<div class='tutorial-end-icon tutorial-icon-button' data-role='end'>
-							<div class='tutorial-end-icon1 web-activity'></div>
-							<div class='tutorial-icon-text'>${this.l10n.stringTutoEnd}</div>
-						</div>
-					</div>
-				</div>`,
-				storage: false,
-				backdrop: true,
+			steps = steps.filter((step) =>  !('element' in step) || ((step.element).length && document.querySelector(step.element) && document.querySelector(step.element).style.display != 'none'));
+			introJs().setOptions({
+				tooltipClass: 'customTooltip',
 				steps: steps,
-				onStart: function (tour) {
-					vm.$emit('start', tour);
-				},
-				onShow: function (tour) {
-					vm.$emit('show', tour);
-				},
-				onEnd: function (tour) {
-					vm.$emit('end', tour);
-				},
-			});
-			tour.init();
-			tour.start(true);
+				prevLabel: this.l10n.stringTutoPrev,
+				nextLabel: this.l10n.stringTutoNext,
+				exitOnOverlyClick: false,
+				nextToDone: false,
+				showBullets: false
+			}).start();
 		}
 	}
 });
