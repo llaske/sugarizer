efb04a1bd66458aa85df6c2002932e415429bd3f
Ujjawal Modi <umodi2003@gmail.com>
2022-10-26 14:52:12 +0530

    migrated to IntroJs in abacus tutorial

--- activities/Abacus.activity/lib/tutorial.js
+++ activities/Abacus.activity/lib/tutorial.js
@@ -4,82 +4,92 @@ define(["webL10n"], function (l10n) {
 	tutorial.start = function() {
 		var steps = [
 			{
-				element: "",
-				orphan: true,
-				placement: "bottom",
+				position: "bottom",
 				title: l10n.get("TutoExplainTitle"),
-				content: l10n.get("TutoExplainContent")
+				intro: l10n.get("TutoExplainContent")
 			},
 			{
 				element: "#abacus-button",
-				placement: "bottom",
+				position: "bottom",
 				title: l10n.get("TutoAbacusTitle"),
-				content: l10n.get("TutoAbacusContent")
+				intro: l10n.get("TutoAbacusContent")
 			},
 			{
 				element: "#settings-button",
-				placement: "bottom",
+				position: "bottom",
 				title: l10n.get("TutoCustomTitle"),
-				content: l10n.get("TutoCustomContent")
+				intro: l10n.get("TutoCustomContent")
 			},
 			{
 				element: "#copy-button",
-				placement: "bottom",
+				position: "bottom",
 				title: l10n.get("TutoCopyTitle"),
-				content: l10n.get("TutoCopyContent")
+				intro: l10n.get("TutoCopyContent")
 			},
 			{
 				element: "#clear-button",
-				placement: "bottom",
+				position: "bottom",
 				title: l10n.get("TutoClearTitle"),
-				content: l10n.get("TutoClearContent")
+				intro: l10n.get("TutoClearContent")
 			},
 			{
 				element: "#actualcanvas",
-				placement: "top",
+				position: "top",
 				title: l10n.get("TutoCanvasTitle"),
-				content: l10n.get("TutoCanvasContent")
+				intro: l10n.get("TutoCanvasContent")
 			},
 		];
-		var tour = new Tour({
-			template: "\
-			<div class='popover tour'>\
-				<div class='arrow'></div>\
-				<h3 class='popover-title tutorial-title'></h3>\
-				<div class='popover-content'></div>\
-				<div class='popover-navigation' style='display: flex; flex-wrap:wrap; justify-content: center; align-items: center'>\
-					<div class='tutorial-prev-icon icon-button' data-role='prev'>\
-						<div class='tutorial-prev-icon1 web-activity'>\
-							<div class='tutorial-prev-icon2 web-activity-icon'></div>\
-							<div class='tutorial-prev-icon3 web-activity-disable'></div>\
-						</div>\
-						<div class='icon-tutorial-text'>"+l10n.get("TutoPrev")+"</div>\
-					</div>\
-					<span data-role='separator' style='margin: 4px'>|</span>\
-					<div class='tutorial-next-icon icon-button' data-role='next'>\
-						<div class='tutorial-next-icon1 web-activity'>\
-							<div class='tutorial-next-icon2 web-activity-icon'></div>\
-							<div class='tutorial-next-icon3 web-activity-disable'></div>\
-						</div>\
-						<div class='icon-tutorial-text'>"+l10n.get("TutoNext")+"</div>\
-					</div>\
-					<div class='tutorial-end-icon icon-button' data-role='end'>\
-						<div class='tutorial-end-icon1 web-activity'>\
-							<div class='tutorial-end-icon2 web-activity-icon'></div>\
-							<div class='tutorial-end-icon3 web-activity-disable'></div>\
-						</div>\
-						<div class='icon-tutorial-text'>"+l10n.get("TutoEnd")+"</div>\
-					</div>\
-				</div>\
-			</div>",
-			storage: false,
-			backdrop: true,
-			steps: steps
+	// 	var tour = new Tour({
+	// 		template: "\
+	// 		<div class='popover tour'>\
+	// 			<div class='arrow'></div>\
+	// 			<h3 class='popover-title tutorial-title'></h3>\
+	// 			<div class='popover-content'></div>\
+	// 			<div class='popover-navigation' style='display: flex; flex-wrap:wrap; justify-content: center; align-items: center'>\
+	// 				<div class='tutorial-prev-icon icon-button' data-role='prev'>\
+	// 					<div class='tutorial-prev-icon1 web-activity'>\
+	// 						<div class='tutorial-prev-icon2 web-activity-icon'></div>\
+	// 						<div class='tutorial-prev-icon3 web-activity-disable'></div>\
+	// 					</div>\
+	// 					<div class='icon-tutorial-text'>"+l10n.get("TutoPrev")+"</div>\
+	// 				</div>\
+	// 				<span data-role='separator' style='margin: 4px'>|</span>\
+	// 				<div class='tutorial-next-icon icon-button' data-role='next'>\
+	// 					<div class='tutorial-next-icon1 web-activity'>\
+	// 						<div class='tutorial-next-icon2 web-activity-icon'></div>\
+	// 						<div class='tutorial-next-icon3 web-activity-disable'></div>\
+	// 					</div>\
+	// 					<div class='icon-tutorial-text'>"+l10n.get("TutoNext")+"</div>\
+	// 				</div>\
+	// 				<div class='tutorial-end-icon icon-button' data-role='end'>\
+	// 					<div class='tutorial-end-icon1 web-activity'>\
+	// 						<div class='tutorial-end-icon2 web-activity-icon'></div>\
+	// 						<div class='tutorial-end-icon3 web-activity-disable'></div>\
+	// 					</div>\
+	// 					<div class='icon-tutorial-text'>"+l10n.get("TutoEnd")+"</div>\
+	// 				</div>\
+	// 			</div>\
+	// 		</div>",
+	// 		storage: false,
+	// 		backdrop: true,
+	// 		steps: steps
+	// 	});
+	// 	tour.init();
+	// 	tour.start(true);
+
+	// };
+		steps= steps.filter(function (obj) {
+			return !('element' in obj) || ((obj.element).length && document.querySelector(obj.element) && document.querySelector(obj.element).style.display != 'none');
 		});
-		tour.init();
-		tour.start(true);
-
-	};
-
+		introJs().setOptions({   
+			tooltipClass: 'customTooltip',
+			steps: steps,
+			prevLabel: l10n.get("TutoPrev"),
+			nextLabel: l10n.get("TutoNext"),
+			exitOnOverlayClick: false,
+			nextToDone: false,
+			showBullets: false
+		}).start();
+	}
 	return tutorial;
 });
