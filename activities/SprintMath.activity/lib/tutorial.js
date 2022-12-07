ee3e698183582b124e521b5622af2829370bd00b
Ayesha <shaikayesha95155@gmail.com>
2022-12-07 10:45:58 +0530

    changes made to Sprint Math activity

--- activities/SprintMath.activity/lib/tutorial.js
+++ activities/SprintMath.activity/lib/tutorial.js
@@ -3,100 +3,69 @@ define(["webL10n"], function (l10n) {
 
     tutorial.start = function () {
         var steps = [{
-                element: "",
-                orphan: true,
-                placement: "bottom",
                 title: l10n.get("TutoExplainTitle"),
-                content: l10n.get("TutoExplainContent")
+                intro: l10n.get("TutoExplainContent")
             },
             {
                 element: "#restart-button",
-                placement: "bottom",
+                position: "bottom",
                 title: l10n.get("TutoRestartTitle"),
-                content: l10n.get("TutoRestartContent")
+                intro: l10n.get("TutoRestartContent")
             },
             {
                 element: "#filter-button",
-                placement: "bottom",
+                position: "bottom",
                 title: l10n.get("TutoFilterTitle"),
-                content: l10n.get("TutoFilterContent")
+                intro: l10n.get("TutoFilterContent")
             },
             {
                 element: "#level-button",
-                placement: "bottom",
+                position: "bottom",
                 title: l10n.get("TutoLevelTitle"),
-                content: l10n.get("TutoLevelContent")
+                intro: l10n.get("TutoLevelContent")
             },
             {
                 element: "#time",
-                placement: "bottom",
+                position: "bottom",
                 title: l10n.get("TutoTimeTitle"),
-                content: l10n.get("TutoTimeContent")
+                intro: l10n.get("TutoTimeContent")
             },
             {
                 element: "#score",
-                placement: "bottom",
+                position: "bottom",
                 title: l10n.get("TutoScoreTitle"),
-                content: l10n.get("TutoScoreContent")
+                intro: l10n.get("TutoScoreContent")
             },
             {
                 element: "#question",
-                placement: "bottom",
+                position: "bottom",
                 title: l10n.get("TutoQuestionTitle"),
-                content: l10n.get("TutoQuestionContent")
+                intro: l10n.get("TutoQuestionContent")
             },
             {
                 element: "#choices",
-                placement: "bottom",
+                position: "bottom",
                 title: l10n.get("TutoChoicesTitle"),
-                content: l10n.get("TutoChoicesContent")
+                intro: l10n.get("TutoChoicesContent")
             },
             {
                 element: "#network-button",
-                placement: "bottom",
+                position: "bottom",
                 title: l10n.get("TutoNetworkTitle"),
-                content: l10n.get("TutoNetworkContent")
+                intro: l10n.get("TutoNetworkContent")
             },
         ];
-        var tour = new Tour({
-            template: "\
-            <div class='popover tour'>\
-                <div class='arrow'></div>\
-                <h3 class='popover-title tutorial-title'></h3>\
-                <div class='popover-content'></div>\
-                <div class='popover-navigation' style='display: flex; flex-wrap:wrap; justify-content: center; align-items: center'>\
-                    <div class='tutorial-prev-icon icon-button' data-role='prev'>\
-                        <div class='tutorial-prev-icon1 web-activity'>\
-                            <div class='tutorial-prev-icon2 web-activity-icon'></div>\
-                            <div class='tutorial-prev-icon3 web-activity-disable'></div>\
-                        </div>\
-                        <div class='icon-tutorial-text'>" + l10n.get("TutoPrev") + "</div>\
-                    </div>\
-                    <span data-role='separator' style='margin: 4px'>|</span>\
-                    <div class='tutorial-next-icon icon-button' data-role='next'>\
-                        <div class='tutorial-next-icon1 web-activity'>\
-                            <div class='tutorial-next-icon2 web-activity-icon'></div>\
-                            <div class='tutorial-next-icon3 web-activity-disable'></div>\
-                        </div>\
-                        <div class='icon-tutorial-text'>" + l10n.get("TutoNext") + "</div>\
-                    </div>\
-                    <div class='tutorial-end-icon icon-button' data-role='end'>\
-                        <div class='tutorial-end-icon1 web-activity'>\
-                            <div class='tutorial-end-icon2 web-activity-icon'></div>\
-                            <div class='tutorial-end-icon3 web-activity-disable'></div>\
-                        </div>\
-                        <div class='icon-tutorial-text'>" + l10n.get("TutoEnd") + "</div>\
-                    </div>\
-                </div>\
-            </div>",
-            storage: false,
-            backdrop: true,
-            steps: steps
-        });
-        tour.init();
-        tour.start(true);
-
-    };
+ 				steps = steps.filter((obj) =>  !('element' in obj) || ((obj.element).length && document.querySelector(obj.element) && document.querySelector(obj.element).style.display != 'none'));
+		introJs().setOptions({
+			tooltipClass: 'customTooltip',
+			steps: steps,
+			prevLabel: l10n.get("TutoPrev"),
+			nextLabel: l10n.get("TutoNext"),
+			exitOnOverlayClick: false,
+			nextToDone: false,
+			showBullets: false
+		}).start();
+	}
 
     return tutorial;
 });
