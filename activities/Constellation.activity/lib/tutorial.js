8c37ec9aab5b650f98ac6a3952454a30442309c0
Disha Talreja <dishatalreja1202@gmail.com>
2023-01-06 14:43:23 +0530

    Replaced bootstrap-tour by Introjs in constellation activity

--- activities/Constellation.activity/lib/tutorial.js
+++ activities/Constellation.activity/lib/tutorial.js
@@ -4,106 +4,72 @@ define(["webL10n"], function(l10n) {
 	tutorial.start = function() {
 		var steps = [
 			{
-				element: "",
-				orphan: true,
-				placement: "bottom",
 				title: l10n.get("TutoExplainTitle"),
-				content: l10n.get("TutoExplainContent")
+				intro: l10n.get("TutoExplainContent")
 			},
 			{
 				element: "#add-button",
-				placement: "bottom",
+				position: "bottom",
 				title: l10n.get("TutoAddTitle"),
-				content: l10n.get("TutoAddContent")
+				intro: l10n.get("TutoAddContent")
 			},
 			{
 				element: "#minus-button",
-				placement: "bottom",
+				position: "bottom",
 				title: l10n.get("TutoMinusTitle"),
-				content: l10n.get("TutoMinusContent")
+				intro: l10n.get("TutoMinusContent")
 			},
 			{
 				element: "#const-button",
-				placement: "bottom",
+				position: "bottom",
 				title: l10n.get("TutoConstTitle"),
-				content: l10n.get("TutoConstContent")
+				intro: l10n.get("TutoConstContent")
 			},
 			{
 				element: "#star-button",
-				placement: "bottom",
+				position: "bottom",
 				title: l10n.get("TutoStarTitle"),
-				content: l10n.get("TutoStarContent")
+				intro: l10n.get("TutoStarContent")
 			},
 			{
 				element: "#location-button",
-				placement: "bottom",
+				position: "bottom",
 				title: l10n.get("TutoLocationTitle"),
-				content: l10n.get("TutoLocationContent")
+				intro: l10n.get("TutoLocationContent")
 			},
 			{
 				element: "#world-button",
-				placement: "bottom",
+				position: "bottom",
 				title: l10n.get("TutoWorldTitle"),
-				content: l10n.get("TutoWorldContent")
+				intro: l10n.get("TutoWorldContent")
 			},
 			{
 				element: "#view-button",
-				placement: "bottom",
+				position: "bottom",
 				title: l10n.get("TutoViewTitle"),
-				content: l10n.get("TutoViewContent")
+				intro: l10n.get("TutoViewContent")
 			},
 			{
 				element: ".starmap_clock",
-				placement: "bottom",
+				position: "bottom",
 				title: l10n.get("TutoTimeTitle"),
-				content: l10n.get("TutoTimeContent")
+				intro: l10n.get("TutoTimeContent")
 			}
 		];
-		var tour = new Tour({
-			template:
-				"\
-            <div class='popover tour'>\
-                <div class='arrow'></div>\
-                <h3 class='popover-title tutorial-title'></h3>\
-                <div class='popover-content'></div>\
-                <div class='popover-navigation' style='display: flex; flex-wrap:wrap; justify-content: center; align-items: center'>\
-                    <div class='tutorial-prev-icon icon-button' data-role='prev'>\
-                        <div class='tutorial-prev-icon1 web-activity'>\
-                            <div class='tutorial-prev-icon2 web-activity-icon'></div>\
-                            <div class='tutorial-prev-icon3 web-activity-disable'></div>\
-                        </div>\
-                        <div class='icon-tutorial-text'>" +
-				l10n.get("TutoPrev") +
-				"</div>\
-                    </div>\
-                    <span data-role='separator' style='margin: 4px'>|</span>\
-                    <div class='tutorial-next-icon icon-button' data-role='next'>\
-                        <div class='tutorial-next-icon1 web-activity'>\
-                            <div class='tutorial-next-icon2 web-activity-icon'></div>\
-                            <div class='tutorial-next-icon3 web-activity-disable'></div>\
-                        </div>\
-                        <div class='icon-tutorial-text'>" +
-				l10n.get("TutoNext") +
-				"</div>\
-                    </div>\
-                    <div class='tutorial-end-icon icon-button' data-role='end'>\
-                        <div class='tutorial-end-icon1 web-activity'>\
-                            <div class='tutorial-end-icon2 web-activity-icon'></div>\
-                            <div class='tutorial-end-icon3 web-activity-disable'></div>\
-                        </div>\
-                        <div class='icon-tutorial-text'>" +
-				l10n.get("TutoEnd") +
-				"</div>\
-                    </div>\
-                </div>\
-            </div>",
-			storage: false,
-			backdrop: true,
-			steps: steps
-		});
-		tour.init();
-		tour.start(true);
-	};
+		
+		steps = steps.filter(function (obj) {
+			return !('element' in obj) || ((obj.element).length && document.querySelector(obj.element) && document.querySelector(obj.element).style.display != 'none');
+	    });
+		introJs().setOptions({
+			tooltipClass: 'customTooltip',
+			steps: steps,
+			prevLabel: l10n.get("TutoPrev"),
+			nextLabel: l10n.get("TutoNext"),
+			exitOnOverlayClick: false,
+			nextToDone: false,
+			showBullets: false
+		}).start();
+	}	
 
 	return tutorial;
 });
