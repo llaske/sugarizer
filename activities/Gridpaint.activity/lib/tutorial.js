98190092da744e55bbbc0cb6cbd26683cdcb178a
aniket singh rawat <aniket.rawat@bytelearn.ai>
2023-01-24 23:56:58 +0530

    migrated grid paint activity tutorial from bootsrap tour to intro js

--- activities/Gridpaint.activity/lib/tutorial.js
+++ activities/Gridpaint.activity/lib/tutorial.js
@@ -2,77 +2,54 @@ define(["webL10n"], function (l10n) {
 	var tutorial = {};
 
 	tutorial.start = function() {
-		var tour = new Tour({
-			template: "\
-			<div class='popover tour'>\
-				<div class='arrow'></div>\
-				<h3 class='popover-title tutorial-title'></h3>\
-				<div class='popover-content'></div>\
-				<div class='popover-navigation' style='display: flex; flex-wrap:wrap; justify-content: center; align-items: center'>\
-					<div class='tutorial-prev-icon icon-button' data-role='prev'>\
-						<div class='tutorial-prev-icon1 web-activity'>\
-							<div class='tutorial-prev-icon2 web-activity-icon'></div>\
-							<div class='tutorial-prev-icon3 web-activity-disable'></div>\
-						</div>\
-						<div class='icon-tutorial-text'>"+l10n.get("TutoPrev")+"</div>\
-					</div>\
-					<span data-role='separator' style='margin: 4px'>|</span>\
-					<div class='tutorial-next-icon icon-button' data-role='next'>\
-						<div class='tutorial-next-icon1 web-activity'>\
-							<div class='tutorial-next-icon2 web-activity-icon'></div>\
-							<div class='tutorial-next-icon3 web-activity-disable'></div>\
-						</div>\
-						<div class='icon-tutorial-text'>"+l10n.get("TutoNext")+"</div>\
-					</div>\
-					<div class='tutorial-end-icon icon-button' data-role='end'>\
-						<div class='tutorial-end-icon1 web-activity'>\
-							<div class='tutorial-end-icon2 web-activity-icon'></div>\
-							<div class='tutorial-end-icon3 web-activity-disable'></div>\
-						</div>\
-						<div class='icon-tutorial-text'>"+l10n.get("TutoEnd")+"</div>\
-					</div>\
-				</div>\
-			</div>",
-			storage: false,
-			backdrop: true,
-			steps: [],
-		});
+		var steps = []
 		if(mode=='edit'){
-			tour.addSteps([
+			steps = [
 			{
-				element: "#canvas",
-				placement: "top",
 				title: l10n.get("TutoEditTitle"),
-				content: l10n.get("TutoEditContent")
+				intro: l10n.get("TutoEditContent")
 			},
 			{
 				element: "#clear-button",
-				placement: "bottom",
+				position: "top",
 				title: l10n.get("TutoClearTitle"),
-				content: l10n.get("TutoClearContent")
+				intro: l10n.get("TutoClearContent")
 			}
 			]
-			);
+			
 		}else {
-			tour.addSteps([
+			steps = [
 			{
-				element: "",
-				orphan: true,
-				placement: "bottom",
+				position: "top",
 				title: l10n.get("TutoExplainTitle"),
-				content: l10n.get("TutoExplainContent")
+				intro: l10n.get("TutoExplainContent")
 			},
 			{
-				element: "#canvas",
-				placement: "top",
+				position: "top",
 				title: l10n.get("TutoGridTitle"),
-				content: l10n.get("TutoGridContent")
+				intro: l10n.get("TutoGridContent")
 			}
 			]
-			);
+
 		}
-		tour.init();
-		tour.start(true);
+		steps = steps.filter(
+			(obj) =>
+			  !("element" in obj) ||
+			  (obj.element.length &&
+				document.querySelector(obj.element) &&
+				document.querySelector(obj.element).style.display != "none")
+		  );
+		  introJs()
+			.setOptions({
+			  tooltipClass: "customTooltip",
+			  steps: steps,
+			  prevLabel: l10n.get("TutoPrev"),
+			  nextLabel: l10n.get("TutoNext"),
+			  exitOnOverlayClick: false,
+			  nextToDone: false,
+			  showBullets: false,
+			})
+			.start();
 
 	};
 
