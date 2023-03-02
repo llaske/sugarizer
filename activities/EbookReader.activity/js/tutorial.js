864d3d771e1d05ae2ecea0fe666a1f9e2e17fc39
ABHINAV JHA <75615092+ABHINAV-JHA-27@users.noreply.github.com>
2023-03-02 23:15:24 +0530

    Migrated Ebbok Activity to IntroJS

--- activities/EbookReader.activity/js/tutorial.js
+++ activities/EbookReader.activity/js/tutorial.js
@@ -1,130 +1,109 @@
-
 // Tutorial component based on bootstrap tour
 var Tutorial = {
-	template: '<div/>',
-	data: function() {
-		return {
-			l10n: {
-				stringPrevShort: '',
-				stringNextShort: '',
-				stringEndShort: '',
-				stringTutoExplainTitle: '',
-				stringTutoExplainContent: '',
-				stringTutoBookTitle: '',
-				stringTutoBookContent: '',
-				stringTutoLibraryButtonTitle: '',
-				stringTutoLibraryButtonContent: '',
-				stringTutoContentsButtonTitle: '',
-				stringTutoContentsButtonContent: '',
-				stringTutoSettingsButtonTitle: '',
-				stringTutoSettingsButtonContent: '',
-				stringTutoFullscreenButtonTitle: '',
-				stringTutoFullscreenButtonContent: '',
-				stringTutoNextButtonTitle: '',
-				stringTutoNextButtonContent: '',
-				stringTutoPrevButtonTitle: '',
-				stringTutoPrevButtonContent: '',
-			}
-		}
-	},
-	methods: {
-		localized: function(localization) {
-			var vm = this;
-			Object.keys(this.l10n).forEach(function(key, index) {
-				vm.l10n[key] = localization.get(key.substr(6));
-			});
-		},
+  template: "<div/>",
+  data: function () {
+    return {
+      l10n: {
+        stringPrevShort: "",
+        stringNextShort: "",
+        stringEndShort: "",
+        stringTutoExplainTitle: "",
+        stringTutoExplainContent: "",
+        stringTutoBookTitle: "",
+        stringTutoBookContent: "",
+        stringTutoLibraryButtonTitle: "",
+        stringTutoLibraryButtonContent: "",
+        stringTutoContentsButtonTitle: "",
+        stringTutoContentsButtonContent: "",
+        stringTutoSettingsButtonTitle: "",
+        stringTutoSettingsButtonContent: "",
+        stringTutoFullscreenButtonTitle: "",
+        stringTutoFullscreenButtonContent: "",
+        stringTutoNextButtonTitle: "",
+        stringTutoNextButtonContent: "",
+        stringTutoPrevButtonTitle: "",
+        stringTutoPrevButtonContent: "",
+      },
+    };
+  },
+  methods: {
+    localized: function (localization) {
+      var vm = this;
+      Object.keys(this.l10n).forEach(function (key, index) {
+        vm.l10n[key] = localization.get(key.substr(6));
+      });
+    },
 
-		show: function(options) {
-			options = options || {};
-			var steps = [
-				{
-					element: "",
-					orphan: true,
-					placement: "bottom",
-					title: this.l10n.stringTutoExplainTitle,
-					content: this.l10n.stringTutoExplainContent
-				},
-				{
-					element: options.book,
-					placement: "right",
-					title: this.l10n.stringTutoBookTitle,
-					content: this.l10n.stringTutoBookContent
-				},
-				{
-					element: options.switchbutton,
-					placement: "bottom",
-					title: this.l10n.stringTutoLibraryButtonTitle,
-					content: this.l10n.stringTutoLibraryButtonContent
-				},
-				{
-					element: options.contentsbutton,
-					placement: "bottom",
-					title: this.l10n.stringTutoContentsButtonTitle,
-					content: this.l10n.stringTutoContentsButtonContent
-				},
-				{
-					element: options.settingsbutton,
-					placement: "bottom",
-					title: this.l10n.stringTutoSettingsButtonTitle,
-					content: this.l10n.stringTutoSettingsButtonContent
-				},
-				{
-					element: options.fullscreenbutton,
-					placement: "bottom",
-					title: this.l10n.stringTutoFullscreenButtonTitle,
-					content: this.l10n.stringTutoFullscreenButtonContent
-				},
-				{
-					element: options.prevbutton,
-					placement: "right",
-					title: this.l10n.stringTutoPrevButtonTitle,
-					content: this.l10n.stringTutoPrevButtonContent
-				},
-				{
-					element: options.nextbutton,
-					placement: "left",
-					title: this.l10n.stringTutoNextButtonTitle,
-					content: this.l10n.stringTutoNextButtonContent
-				},
-			];
-			var tour = new Tour({
-				template: "\
-				<div class='popover tour'>\
-					<div class='arrow'></div>\
-					<h3 class='popover-title tutorial-title'></h3>\
-					<div class='popover-content'></div>\
-					<div class='popover-navigation' style='display: flex; flex-wrap:wrap; justify-content: center; align-items: center'>\
-						<div class='tutorial-prev-icon tutorial-icon-button' data-role='prev'>\
-							<div class='tutorial-prev-icon1 web-activity'>\
-								<div class='tutorial-prev-icon2 web-activity-icon'></div>\
-								<div class='tutorial-prev-icon3 web-activity-disable'></div>\
-							</div>\
-							<div class='tutorial-icon-text'>"+this.l10n.stringPrevShort+"</div>\
-						</div>\
-						<span data-role='separator' style='margin: 4px'>|</span>\
-						<div class='tutorial-next-icon tutorial-icon-button' data-role='next'>\
-							<div class='tutorial-next-icon1 web-activity'>\
-								<div class='tutorial-next-icon2 web-activity-icon'></div>\
-								<div class='tutorial-next-icon3 web-activity-disable'></div>\
-							</div>\
-							<div class='tutorial-icon-text'>"+this.l10n.stringNextShort+"</div>\
-						</div>\
-						<div class='tutorial-end-icon tutorial-icon-button' data-role='end'>\
-							<div class='tutorial-end-icon1 web-activity'>\
-								<div class='tutorial-end-icon2 web-activity-icon'></div>\
-								<div class='tutorial-end-icon3 web-activity-disable'></div>\
-							</div>\
-							<div class='tutorial-icon-text'>"+this.l10n.stringEndShort+"</div>\
-						</div>\
-					</div>\
-				</div>",
-				storage: false,
-				backdrop: true,
-				steps: steps
-			});
-			tour.init();
-			tour.start(true);
-		}
-	}
-}
+    show: function (options) {
+      options = options || {};
+      var steps = [
+        {
+          position: "bottom",
+          title: this.l10n.stringTutoExplainTitle,
+          intro: this.l10n.stringTutoExplainContent,
+        },
+        {
+          element: options.book,
+          position: "right",
+          title: this.l10n.stringTutoBookTitle,
+          intro: this.l10n.stringTutoBookContent,
+        },
+        {
+          element: options.switchbutton,
+          position: "bottom",
+          title: this.l10n.stringTutoLibraryButtonTitle,
+          intro: this.l10n.stringTutoLibraryButtonContent,
+        },
+        {
+          element: options.contentsbutton,
+          position: "bottom",
+          title: this.l10n.stringTutoContentsButtonTitle,
+          intro: this.l10n.stringTutoContentsButtonContent,
+        },
+        {
+          element: options.settingsbutton,
+          position: "bottom",
+          title: this.l10n.stringTutoSettingsButtonTitle,
+          intro: this.l10n.stringTutoSettingsButtonContent,
+        },
+        {
+          element: options.fullscreenbutton,
+          position: "bottom",
+          title: this.l10n.stringTutoFullscreenButtonTitle,
+          intro: this.l10n.stringTutoFullscreenButtonContent,
+        },
+        {
+          element: options.prevbutton,
+          position: "right",
+          title: this.l10n.stringTutoPrevButtonTitle,
+          intro: this.l10n.stringTutoPrevButtonContent,
+        },
+        {
+          element: options.nextbutton,
+          position: "left",
+          title: this.l10n.stringTutoNextButtonTitle,
+          intro: this.l10n.stringTutoNextButtonContent,
+        },
+      ];
+
+      steps = steps.filter(
+        (step) =>
+          !("element" in step) ||
+          (step.element &&
+            step.element.style.display != "none" &&
+            step.element.getBoundingClientRect().y != 0)
+      );
+      introJs()
+        .setOptions({
+          tooltipClass: "customTooltip",
+          steps: steps,
+          prevLabel: this.l10n.stringPrevShort,
+          nextLabel: this.l10n.stringNextShort,
+          exitOnOverlayClick: false,
+          nextToDone: false,
+          showBullets: false,
+        })
+        .start();
+    },
+  },
+};
