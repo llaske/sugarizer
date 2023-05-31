0607c5e3f3fdd7b4ee41e4e7902511a874523572
Vinayak Nayar <nayarvinayak03@gmail.com>
2023-05-31 12:17:43 +0530

    chore: Rewrite SugarL10n component using i18next

--- activities/Measure.activity/js/components/SugarL10n.js
+++ activities/Measure.activity/js/components/SugarL10n.js
@@ -32,101 +32,147 @@ Vue.component('sugar-localization', {
 		}
 	},
 	mounted: function () {
-		var vm = this;
+		const vm = this;
+		// var vm = this;
 		if (vm.l10n == null) {
-			requirejs(["sugar-web/env", "webL10n"], function (env, webL10n) {
+			requirejs(["sugar-web/env"], function (env) {
 				env.getEnvironment(function (err, environment) {
-					vm.l10n = webL10n;
-					var defaultLanguage = (typeof chrome != 'undefined' && chrome.app && chrome.app.runtime) ? chrome.i18n.getUILanguage() : navigator.language;
-					var language = environment.user ? environment.user.language : defaultLanguage;
-					webL10n.language.code = language;
-					window.addEventListener("localized", function () {
-						if (!vm.eventReceived) {
-							vm.code = language;
-							vm.dictionary = vm.l10n.dictionary;
-						} else if (webL10n.language.code != language) {
-							webL10n.language.code = language;
-						}
-					});
+					const defaultLanguage = (typeof chrome != 'undefined' && chrome.app && chrome.app.runtime) ? chrome.i18n.getUILanguage() : navigator.language;
+					const language = environment.user ? environment.user.language : defaultLanguage;
+
+					if (vm.l10n == null) {
+						// Initialize i18next with options
+						i18next.init({
+							lng: language,
+							fallbackLng: 'en',
+							debug: true,
+							resources: {
+								en: {
+									translation: null // Placeholder for English translation data
+								},
+								fr: {
+									translation: null // Placeholder for French translation data
+								},
+								es: {
+									translation: null // Placeholder for Spanish translation data
+								},
+								pt: {
+									translation: null // Placeholder for Portuguese translation data
+								}
+							}
+						}, function () {
+							// Fetch all translation files using axios.get() and assign the data to resources
+							const translationPromises = [
+								axios.get('./locales/en.json').then(function (response) {
+									i18next.addResourceBundle('en', 'translation', response.data);
+								}),
+								axios.get('./locales/fr.json').then(function (response) {
+									i18next.addResourceBundle('fr', 'translation', response.data);
+								}),
+								axios.get('./locales/es.json').then(function (response) {
+									i18next.addResourceBundle('es', 'translation', response.data);
+								}),
+								axios.get('./locales/pt.json').then(function (response) {
+									i18next.addResourceBundle('pt', 'translation', response.data);
+								})
+							];
+
+							// Wait for all translation promises to resolve
+							Promise.all(translationPromises).then(function () {
+								vm.l10n = i18next;
+								vm.code = i18next.language;
+								vm.dictionary = i18next.getResourceBundle(i18next.language, 'translation');
+								vm.subscribeLanguageChange();
+							});
+						});
+					}
 				});
 			});
-			//Activity initialization check
-			var SugarActivity = vm.$root.$children.find(function (child) {
-				return child.$options.name == 'SugarActivity';
-			});
-			SugarActivity.$on('initialized', function () {
-				vm.activityInitialized = true;
-			});
 		}
+
+
+
+
+		// Activity initialization check
+		const SugarActivity = vm.$root.$children.find(function (child) {
+			return child.$options.name == 'SugarActivity';
+		});
+		SugarActivity.$on('initialized', function () {
+			vm.activityInitialized = true;
+		});
 	},
 	methods: {
-		// Get a string with parameters
-		get: function (str, params) {
-			var out = '';
-			
-			if (!this.dictionary) {
-				out = str;
-			} else {
-				var item = this.dictionary[str];
-				if (!item || !item.textContent) {
-					out = str;
-				} else {
-					out = item.textContent;
-				}
-			}
-			
-			// Check params
-			if(params) {
-				var paramsInString = out.match(/{{\s*[\w\.]+\s*}}/g);
-				for (var i in paramsInString) {
-					var param = paramsInString[i].match(/[\w\.]+/)[0];
-					if (params[param]) {
-						out = out.replace(paramsInString[i], params[param]);
-					}
-				}
-			}
-			return out;
-		},
+	subscribeLanguageChange: function () {
+		let vm = this;
+		i18next.on('languageChanged', function (lng) {
+			vm.code = lng;
+			vm.dictionary = i18next.getResourceBundle(lng, 'translation');
+			vm.$emit('localized');
+			vm.eventReceived = true;
+		});
+	},
+	// Get a string with parameters
+	get: function (str, params) {
+		let out = '';
 
-		// Get values for a set of strings on the form of {stringKey1: '', stringKey2: '', ...}
-		localize: function (strings) {
-			var vm = this;
-			Object.keys(strings).forEach(function (key, index) {
-				strings[key] = vm.get(key.substr(6));
-			});
-		},
-
-		// Convert a UNIX timestamp to Sugarizer time elapsed string
-		localizeTimestamp: function (timestamp) {
-			var maxlevel = 2;
-			var levels = 0;
-			var time_period = '';
-			var elapsed_seconds = ((new Date().getTime()) - timestamp) / 1000;
-			for (var i = 0; i < this.units.length; i++) {
-				var factor = this.units[i].factor;
-
-				var elapsed_units = Math.floor(elapsed_seconds / factor);
-				if (elapsed_units > 0) {
-					if (levels > 0)
-						time_period += ',';
-
-					time_period += ' ' + elapsed_units + " " + (elapsed_units == 1 ? this.get(this.units[i].name + "_one") : this.get(this.units[i].name + "_other"));
-
-					elapsed_seconds -= elapsed_units * factor;
-				}
-
-				if (time_period != '')
-					levels += 1;
-
-				if (levels == maxlevel)
-					break;
-			}
-
-			if (levels == 0) {
-				return this.get("SecondsAgo");
-			}
-
-			return this.get("Ago", { time: time_period });
+		if (!this.dictionary) {
+			out = str;
+		} else {
+			out = this.dictionary[str] || str;
 		}
+
+		// Check params
+		if (params) {
+			let paramsInString = out.match(/{{\s*[\w\.]+\s*}}/g);
+			for (let i in paramsInString) {
+				let param = paramsInString[i].match(/[\w\.]+/)[0];
+				if (params[param]) {
+					out = out.replace(paramsInString[i], params[param]);
+				}
+			}
+		}
+		return out;
+	},
+
+	// Get values for a set of strings on the form of {stringKey1: '', stringKey2: '', ...}
+	localize: function (strings) {
+		const vm = this;
+		Object.keys(strings).forEach(function (key, index) {
+			strings[key] = vm.get(key.substr(6));
+		});
+	},
+
+	// Convert a UNIX timestamp to Sugarizer time elapsed string
+	localizeTimestamp: function (timestamp) {
+		const maxlevel = 2;
+		const levels = 0;
+		const time_period = '';
+		const elapsed_seconds = (Date.now() - timestamp) / 1000;
+		for (let i = 0; i < this.units.length; i++) {
+			let factor = this.units[i].factor;
+
+			let elapsed_units = Math.floor(elapsed_seconds / factor);
+			if (elapsed_units > 0) {
+				if (levels > 0)
+					time_period += ',';
+
+				time_period += ' ' + elapsed_units + " " + (elapsed_units == 1 ? this.get(this.units[i].name + "_one") : this.get(this.units[i].name + "_other"));
+
+				elapsed_seconds -= elapsed_units * factor;
+			}
+
+			if (time_period != '')
+				levels += 1;
+
+			if (levels == maxlevel)
+				break;
+		}
+
+		if (levels == 0) {
+			return this.get("SecondsAgo");
+		}
+
+		return this.get("Ago", { time: time_period });
 	}
-});
+}
+  });
