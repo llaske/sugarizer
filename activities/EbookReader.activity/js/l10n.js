0c008dfa5b43c3e3fc4f9595837c09922dcab42f
Vishal K <korada.vishal.phe22@itbhu.ac.in>
2023-08-28 19:36:03 +0530

    Replaced code in files to change it to i18

--- activities/EbookReader.activity/js/l10n.js
+++ activities/EbookReader.activity/js/l10n.js
@@ -14,13 +14,12 @@ var Localization = {
 	mounted: function() {
 		var vm = this;
 		if (vm.l10n == null) {
-			requirejs(["sugar-web/env", "webL10n"], function (env, webL10n) {
+			requirejs(["sugar-web/env", "l10n"], function (env, l10n) {
 				env.getEnvironment(function(err, environment) {
-					vm.l10n = webL10n;
+					vm.l10n = l10n;
 					var defaultLanguage = (typeof chrome != 'undefined' && chrome.app && chrome.app.runtime) ? chrome.i18n.getUILanguage() : navigator.language;
 					var language = environment.user ? environment.user.language : defaultLanguage;
-					webL10n.language.code = language;
-					window.addEventListener("localized", function() {
+					l10n.init(language);					window.addEventListener("localized", function() {
 						if (!vm.eventReceived) {
 							vm.code = language;
 							vm.dictionary = vm.l10n.dictionary;
