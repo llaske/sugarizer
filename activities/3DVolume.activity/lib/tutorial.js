67fe91bdf5f3690046d718eb99ebe2baf116ec7d
SamarthBagga <samarthbagga@gmail.com>
2024-07-29 00:32:52 +0530

    localization

--- activities/3DVolume.activity/lib/tutorial.js
+++ activities/3DVolume.activity/lib/tutorial.js
@@ -1,124 +1,124 @@
 define(["l10n"], function (l10n) {
-	var tutorial = {};
+    var tutorial = {};
 
-	tutorial.start = function (ifAdding) {
-		var steps = [
-			{
-				title: l10n.get("VolumeActivity"),
-				intro: "Welcome into the Volume activity. Feel free to zoom in and rotate the board to your preference using touch gestures or simply by clicking and scrolling.",
-			},
-			{
-				element: "#tetra-button",
-				title: "Add Tetrahedrons",
-				intro: "Select this and click anywhere on the board to add tetrahedrons to that position. The default is a solid tetrahedron without numbers.",
-			},
-			{
-				element: "#cube-button",
-				title: "Add Cubes",
-				intro: "Select this and click anywhere on the board to add cubes to that position. The default is a solid cube without numbers.",
-			},
-			{
-				element: "#octa-button",
-				title: "Add Octahedrons",
-				intro: "Select this and click anywhere on the board to add octahedrons to that position. The default is a solid octahedron without numbers.",
-			},
-			{
-				element: "#deca-button",
-				title: "Add Decahedrons",
-				intro: "Select this and click anywhere on the board to add decahedrons to that position. The default is a solid decahedron without numbers.",
-			},
-			{
-				element: "#dodeca-button",
-				title: "Add Dodecahedron",
-				intro: "Select this and click anywhere on the board to add dodecahedron to that position. The default is a solid dodecahedrons without numbers.",
-			},
-			{
-				element: "#icosa-button",
-				title: "Add Icosahedron",
-				intro: "Select this and click anywhere on the board to add icosahedron to that position. The default is a solid icosahedron without numbers.",
-			},
-			{
-				element: "#clear-button",
-				title: "Remove volumes",
-				intro: "Select this and click on any volume on the board to remove it.",
-			},
-			{
-				element: "#volume-button",
-				title: "Select Volume Type",
-				intro: "Select the type of volume you want to add to the board.",
-			},
-			{
-				element: "#color-button-fill",
-				title: "Change Volume Color",
-				intro: "Choose any one of the colors for your volumes. The default is your buddy color.",
-			},
-			{
-				element: "#color-button-text",
-				title: "Change Volume Text Color",
-				intro: "Choose any one of the colors for the text on your volumes. The default is your buddy stroke color.",
-			},
-			{
-				element: "#bg-button",
-				position: "bottom",
-				title: "Change board background",
-				intro: "Choose any one of the three boards backgrounds according to their different frictions.",
-			},
-			{
-				element: "#zoom-button",
-				title: "Zoom In/Out",
-				intro: "Use the zoom pallette to zoom in or out..",
-			},
-			{
-				element: "#throw-button",
-				title: "Throw Button",
-				intro: "Use this button to shake the volumes on the board.",
-			},
-			{
-				element: ".arrow-container",
-				title: "Rotate the board",
-				intro: "Select the arrow pointing in the direction you wish the board to rotate.",
-			},
-		];
+    tutorial.start = function (ifAdding) {
+        var steps = [
+            {
+                title: l10n.get("VolumeTitle"),
+                intro: l10n.get("VolumeIntro"),
+            },
+            {
+                element: "#tetra-button",
+                title: l10n.get("AddTetrahedronsTitle"),
+                intro: l10n.get("AddTetrahedronsIntro"),
+            },
+            {
+                element: "#cube-button",
+                title: l10n.get("AddCubesTitle"),
+                intro: l10n.get("AddCubesIntro"),
+            },
+            {
+                element: "#octa-button",
+                title: l10n.get("AddOctahedronsTitle"),
+                intro: l10n.get("AddOctahedronsIntro"),
+            },
+            {
+                element: "#deca-button",
+                title: l10n.get("AddDecahedronsTitle"),
+                intro: l10n.get("AddDecahedronsIntro"),
+            },
+            {
+                element: "#dodeca-button",
+                title: l10n.get("AddDodecahedronTitle"),
+                intro: l10n.get("AddDodecahedronIntro"),
+            },
+            {
+                element: "#icosa-button",
+                title: l10n.get("AddIcosahedronTitle"),
+                intro: l10n.get("AddIcosahedronIntro"),
+            },
+            {
+                element: "#clear-button",
+                title: l10n.get("RemoveVolumesTitle"),
+                intro: l10n.get("RemoveVolumesIntro"),
+            },
+            {
+                element: "#volume-button",
+                title: l10n.get("SelectVolumeTypeTitle"),
+                intro: l10n.get("SelectVolumeTypeIntro"),
+            },
+            {
+                element: "#color-button-fill",
+                title: l10n.get("ChangeVolumeColorTitle"),
+                intro: l10n.get("ChangeVolumeColorIntro"),
+            },
+            {
+                element: "#color-button-text",
+                title: l10n.get("ChangeVolumeTextColorTitle"),
+                intro: l10n.get("ChangeVolumeTextColorIntro"),
+            },
+            {
+                element: "#bg-button",
+                position: "bottom",
+                title: l10n.get("ChangeBoardBackgroundTitle"),
+                intro: l10n.get("ChangeBoardBackgroundIntro"),
+            },
+            {
+                element: "#zoom-button",
+                title: l10n.get("ZoomInOutTitle"),
+                intro: l10n.get("ZoomInOutIntro"),
+            },
+            {
+                element: "#throw-button",
+                title: l10n.get("ThrowButtonTitle"),
+                intro: l10n.get("ThrowButtonIntro"),
+            },
+            {
+                element: ".arrow-container",
+                title: l10n.get("RotateBoardTitle"),
+                intro: l10n.get("RotateBoardIntro"),
+            },
+        ];
 
-		// Filter out hidden buttons based on screen width
-		var hiddenButtonSelectors = [];
-		var screenWidth = window.innerWidth;
+        // Filter out hidden buttons based on screen width
+        var hiddenButtonSelectors = [];
+        var screenWidth = window.innerWidth;
 
-		if (screenWidth <= 1120) {
-			hiddenButtonSelectors.push("#color-button-text", "#bg-button", "#zoom-button");
-		}
-		if (screenWidth <= 800) {
-			hiddenButtonSelectors.push("#volume-button", "#color-button-fill", "#remove-first");
-		}
-		if (screenWidth <= 690) {
-			hiddenButtonSelectors.push("#sensor-button", "#throw-button");
-		}
+        if (screenWidth <= 1120) {
+            hiddenButtonSelectors.push("#color-button-text", "#bg-button", "#zoom-button");
+        }
+        if (screenWidth <= 800) {
+            hiddenButtonSelectors.push("#volume-button", "#color-button-fill", "#remove-first");
+        }
+        if (screenWidth <= 690) {
+            hiddenButtonSelectors.push("#sensor-button", "#throw-button");
+        }
 
-		steps = steps.filter(function (obj) {
-			return (
-				!("element" in obj) ||
-				(obj.element.length &&
-					document.querySelector(obj.element) &&
-					document.querySelector(obj.element).style.display != "none" &&
-					!hiddenButtonSelectors.includes(obj.element))
-			);
-		});
+        steps = steps.filter(function (obj) {
+            return (
+                !("element" in obj) ||
+                (obj.element.length &&
+                    document.querySelector(obj.element) &&
+                    document.querySelector(obj.element).style.display != "none" &&
+                    !hiddenButtonSelectors.includes(obj.element))
+            );
+        });
 
-		introJs()
-			.setOptions({
-				tooltipClass: "customTooltip",
-				steps: steps,
-				prevLabel: "Prev",
-				nextLabel: "Next",
-				exitOnOverlayClick: false,
-				nextToDone: false,
-				showBullets: false,
-			})
-			.start()
-			.onexit(function() {
-				ifAdding.adding = true;
-			});
-	};
+        introJs()
+            .setOptions({
+                tooltipClass: "customTooltip",
+                steps: steps,
+                prevLabel: "Prev",
+                nextLabel: "Next",
+                exitOnOverlayClick: false,
+                nextToDone: false,
+                showBullets: false,
+            })
+            .start()
+            .onexit(function() {
+                ifAdding.adding = true;
+            });
+    };
 
-	return tutorial;
+    return tutorial;
 });
