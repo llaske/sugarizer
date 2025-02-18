7be762cd71dff5d0e298ac3b61741746ec06267d
Ali Hassan <141296395+AliHassan245@users.noreply.github.com>
2025-02-18 18:12:41 +0500

    Update activity.js

--- activities/FoodChain.activity/js/activity.js
+++ activities/FoodChain.activity/js/activity.js
@@ -49,6 +49,10 @@ define(["sugar-web/activity/activity","l10n","sugar-web/graphics/radiobuttonsgro
 					} else if (language == 'pt') {
 						l10n.language.code = "pt_BR";
 						l10n.init("pt_BR");
+					} else {
+						// Default to English if language is not selected
+						l10n.language.code = "en";
+						l10n.init("en");
 					}
 
 					// Init sound component
