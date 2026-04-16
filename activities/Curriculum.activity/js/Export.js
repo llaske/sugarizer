bc1a66d5676ea5181262af0aa81576aacf55e37c
Karthik-dev56 <prokarthik1889@gmail.com>
2026-04-16 19:37:09 +0530

    Fixes CSV export format to retain multiple columns for Chart activity and update UI labels

--- activities/Curriculum.activity/js/Export.js
+++ activities/Curriculum.activity/js/Export.js
@@ -306,7 +306,7 @@ var Export = {
 					}
 					// Date of aquisition
 					if (skillObj.timestamp) {
-						csvContent += `,"${new Date(skillObj.timestamp).toLocaleDateString()}"`
+						csvContent += `,"${new Date(skillObj.timestamp).toLocaleDateString()}"`;
 					} else {
 						csvContent += `,"-"`;
 					}
@@ -321,9 +321,9 @@ var Export = {
 			}
 
 			var metadata = {
-				mimetype: 'text/plain',
-				title: this.$root.$refs.SugarL10n.get('ExportFileName', { templateTitle: this.templateTitle, userName: this.currentenv.user.name }) + '.txt',
-				activity: "org.olpcfrance.Curriculum",
+				mimetype: 'text/csv',
+				title: this.$root.$refs.SugarL10n.get('ExportFileName', { templateTitle: this.templateTitle, userName: this.currentenv.user.name }) + '.csv',
+				activity: "org.sugarlabs.ChartActivity",
 				timestamp: new Date().getTime(),
 				creation_time: new Date().getTime(),
 				file_size: 0
@@ -331,7 +331,7 @@ var Export = {
 			var vm = this;
 			this.$root.$refs.SugarJournal.createEntry(csvContent, metadata)
 				.then(() => {
-					vm.$root.$refs.SugarPopup.log(this.$root.$refs.SugarL10n.get('ExportTo',{format: 'CSV'}));
+					vm.$root.$refs.SugarPopup.log(this.$root.$refs.SugarL10n.get('ExportTo', { format: 'CSV' }));
 					console.log('Export to CSV complete');
 					vm.$emit('export-completed');
 				});
@@ -354,7 +354,7 @@ var Export = {
 					var vm = this;
 					this.$root.$refs.SugarJournal.createEntry(inputData, metadata)
 						.then(() => {
-							vm.$root.$refs.SugarPopup.log(this.$root.$refs.SugarL10n.get('ExportTo',{format: 'ODT'}));
+							vm.$root.$refs.SugarPopup.log(this.$root.$refs.SugarL10n.get('ExportTo', { format: 'ODT' }));
 							console.log('Export to ODT complete');
 							vm.$emit('export-completed');
 						});
@@ -541,7 +541,7 @@ var Export = {
 				};
 				vm.$root.$refs.SugarJournal.createEntry(inputData, metadata)
 					.then(() => {
-						vm.$root.$refs.SugarPopup.log(this.$root.$refs.SugarL10n.get('ExportTo',{format: 'DOC'}));
+						vm.$root.$refs.SugarPopup.log(this.$root.$refs.SugarL10n.get('ExportTo', { format: 'DOC' }));
 						console.log('Export to DOC complete');
 						vm.$emit('export-completed');
 					});
@@ -571,7 +571,7 @@ var Export = {
 									};
 									vm.$root.$refs.SugarJournal.createEntry(doc.output('dataurlstring'), metadata)
 										.then(() => {
-											vm.$root.$refs.SugarPopup.log(this.$root.$refs.SugarL10n.get('ExportTo',{format: 'PDF'}));
+											vm.$root.$refs.SugarPopup.log(this.$root.$refs.SugarL10n.get('ExportTo', { format: 'PDF' }));
 											console.log('Export to PDF complete');
 											vm.$emit('export-completed');
 										});
